71a4e527e48d6df0c99eb3bfdd0c05c4c11e2392
poffm <matp@live.ca>
2019-02-21 15:00:09 -0500

    In progress Support #14841 Add PCRPrimer component
    https://redmine.biodiversity.agr.gc.ca/issues/14841
    
    -Updated React version for compatibility with "useContext" hook.
    -Updated primer form to get "doOperations" function from context
     instead of the old Operations component.

--- pages/add-pcr-primer.tsx
+++ pages/add-pcr-primer.tsx
@@ -1,10 +1,12 @@
 import { Field, Form, Formik } from "formik";
+import { useContext } from "react";
 import Select from "react-select";
-import { Operations } from "../components/api-client/Operations";
+import { ApiClientContext } from "../components/api-client/ApiClientContext";
 import Head from "../components/head";
 import Nav from "../components/nav";
 import { ResourceSelect } from "../components/resource-select/ResourceSelect";
 import { Group } from "../types/seqdb-api/resources/Group";
+import { PcrPrimer } from "../types/seqdb-api/resources/PcrPrimer";
 
 const PRIMER_TYPE_OPTIONS = [
   {
@@ -30,69 +32,67 @@ const PRIMER_TYPE_OPTIONS = [
 ];
 
 export default function AddPcrPrimerPage() {
+  const { doOperations } = useContext(ApiClientContext);
+
+  function onSubmit({ name, lotNumber, group, type }) {
+    doOperations([
+      {
+        op: "POST",
+        path: "pcrPrimer",
+        value: {
+          attributes: { name, lotNumber, seq: "", type },
+          id: 123,
+          relationships: { group },
+          type: "pcrPrimer"
+        }
+      }
+    ]);
+  }
   return (
     <div>
       <Head title="Add PCR Primer" />
       <Nav />
       <div className="container-fluid">
         <h1>Add PCR Primer</h1>
-        <Operations>
-          {({ doOperations }) => {
-            function onSubmit({ name, lotNumber, group, type }) {
-              doOperations([
-                {
-                  op: "POST",
-                  path: "pcrPrimer",
-                  value: {
-                    attributes: { name, lotNumber, seq: "", type },
-                    id: 123,
-                    relationships: { group },
-                    type: "pcrPrimer"
-                  }
-                }
-              ]);
-            }
-
-            return (
-              <Formik<any> initialValues={{ lotNumber: 1 }} onSubmit={onSubmit}>
-                {({ setFieldValue }) => (
-                  <Form>
-                    <div className="row">
-                      <div className="form-group col-md-2">
-                        <label>Group:</label>
-                        <ResourceSelect<Group>
-                          filter={input => ({ groupName: input })}
-                          model="group"
-                          onChange={val => setFieldValue("group", val)}
-                          optionLabel={group => group.groupName}
-                        />
-                      </div>
-                      <div className="form-group col-md-2">
-                        <label>Primer Type:</label>
-                        <Select<any>
-                          name="type"
-                          options={PRIMER_TYPE_OPTIONS}
-                          onChange={({ value }) => setFieldValue("type", value)}
-                        />
-                      </div>
-                      <div className="form-group col-md-2">
-                        <label>Name:</label>
-                        <Field name="name" className="form-control" />
-                      </div>
-                      <div className="form-group col-md-2">
-                        <label>Lot Number:</label>
-                        <Field name="lotNumber" className="form-control" />
-                      </div>
-                    </div>
-                    <button className="btn btn-primary" type="submit">
-                      Submit
-                    </button>
-                  </Form>
-                )}
-              </Formik>
-            );
-          }}
-        </Operations>
+        <Formik<Partial<PcrPrimer>>
+          initialValues={{ name: "", lotNumber: 1 }}
+          onSubmit={onSubmit}
+        >
+          {({ setFieldValue }) => (
+            <Form>
+              <div className="row">
+                <div className="form-group col-md-2">
+                  <label>Group:</label>
+                  <ResourceSelect<Group>
+                    filter={input => ({ groupName: input })}
+                    model="group"
+                    onChange={val => setFieldValue("group", val)}
+                    optionLabel={group => group.groupName}
+                  />
+                </div>
+                <div className="form-group col-md-2">
+                  <label>Primer Type:</label>
+                  <Select<any>
+                    name="type"
+                    options={PRIMER_TYPE_OPTIONS}
+                    onChange={({ value }) => setFieldValue("type", value)}
+                  />
+                </div>
+                <div className="form-group col-md-2">
+                  <label>Name:</label>
+                  <Field name="name" className="form-control" />
+                </div>
+                <div className="form-group col-md-2">
+                  <label>Lot Number:</label>
+                  <Field name="lotNumber" className="form-control" />
+                </div>
+              </div>
+              <button className="btn btn-primary" type="submit">
+                Submit
+              </button>
+            </Form>
+          )}
+        </Formik>
       </div>
     </div>
   );
