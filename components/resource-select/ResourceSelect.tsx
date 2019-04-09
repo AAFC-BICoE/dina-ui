4d8d78a505847e2813c6864aa6e9258ebe1545a4
poffm <matp@live.ca>
2019-04-09 12:13:18 -0400

    In progress Feature #15647 Resource select dropdown improvements
    https://redmine.biodiversity.agr.gc.ca/issues/15647
    
    -Refactored ResourceSelect as a functinon component.

--- components/resource-select/ResourceSelect.tsx
+++ components/resource-select/ResourceSelect.tsx
@@ -1,13 +1,10 @@
 import { FilterParam, GetParams, KitsuResource } from "kitsu";
 import { debounce, omitBy } from "lodash";
-import React from "react";
+import React, { useContext } from "react";
 import { Async as AsyncSelect } from "react-select";
 import { OptionsType } from "react-select/lib/types";
 import { isUndefined } from "util";
-import {
-  ApiClientContext,
-  ApiClientContextI
-} from "../../components/api-client/ApiClientContext";
+import { ApiClientContext } from "../../components/api-client/ApiClientContext";
 
 /** ResourceSelect component props. */
 export interface ResourceSelectProps<TData> {
@@ -34,43 +31,21 @@ export interface ResourceSelectProps<TData> {
 }
 
 /** Dropdown select input for selecting a resource from the API. */
-export class ResourceSelect<
-  TData extends KitsuResource
-> extends React.Component<ResourceSelectProps<TData>> {
-  public static contextType = ApiClientContext;
-  public context!: ApiClientContextI;
+export function ResourceSelect<TData extends KitsuResource>({
+  filter,
+  include,
+  model,
+  onChange = () => undefined,
+  optionLabel,
+  sort,
+  value
+}: ResourceSelectProps<TData>) {
+  const { apiClient } = useContext(ApiClientContext);
 
-  public render() {
-    const { value, optionLabel } = this.props;
-
-    // Debounces the loadOptions function to avoid sending excessive API requests.
-    const debouncedOptionLoader = debounce((inputValue, callback) => {
-      this.loadOptions(inputValue, callback);
-    }, 250);
-
-    // Set the component's value externally when used as a controlled input.
-    const selectedValue = value
-      ? { label: optionLabel(value), value }
-      : undefined;
-
-    return (
-      <AsyncSelect
-        value={selectedValue}
-        defaultOptions={true}
-        loadOptions={debouncedOptionLoader}
-        onChange={this.onChange}
-      />
-    );
-  }
-
-  /** Loads options by getting a list of resources from the back-end API. */
-  private async loadOptions(
+  async function loadOptions(
     inputValue: string,
     callback: (options: OptionsType<any>) => void
   ) {
-    const { filter, include, optionLabel, model, sort } = this.props;
-    const { apiClient } = this.context;
-
     const filterParam = inputValue ? filter(inputValue) : undefined;
 
     // Omit undefined values from the GET params, which would otherwise cause an invalid request.
@@ -92,13 +67,22 @@ export class ResourceSelect<
     callback(options);
   }
 
-  private onChange = option => {
-    // When there is no onChange prop, do nothing.
-    if (!this.props.onChange) {
-      return;
-    }
+  // Debounces the loadOptions function to avoid sending excessive API requests.
+  const debouncedOptionLoader = debounce((inputValue, callback) => {
+    loadOptions(inputValue, callback);
+  }, 250);
 
-    // Call the provided onChange method with the selected value.
-    this.props.onChange(option.value);
-  };
+  // Set the component's value externally when used as a controlled input.
+  const selectedValue = value
+    ? { label: optionLabel(value), value }
+    : undefined;
+
+  return (
+    <AsyncSelect
+      value={selectedValue as any}
+      defaultOptions={true}
+      loadOptions={debouncedOptionLoader}
+      onChange={({ value }) => onChange(value)}
+    />
+  );
 }
