c288bf1be191843ce9c1fa37d680f17061af5e7c
poffm <matp@live.ca>
2019-04-09 11:07:00 -0400

    In progress Feature #15505 Add date input component
    https://redmine.biodiversity.agr.gc.ca/issues/15505
    
    -Modified DateField to pass the date to/from
     Formik in the correct "yyyy-MM-dd" format.
    -Added tests.

--- components/formik-connected/DateField.tsx
+++ components/formik-connected/DateField.tsx
@@ -5,26 +5,28 @@ import { FieldWrapper, LabelWrapperParams } from "./FieldWrapper";
 
 /** Formik-connected date input. */
 export function DateField(props: LabelWrapperParams) {
-  const { className, name, label } = props;
+  const { name } = props;
 
   return (
-    <FieldWrapper className={className} name={name} label={label}>
+    <FieldWrapper {...props}>
       <Field name={name}>
         {({
           field: { value },
           form: { setFieldValue, setFieldTouched }
         }: FieldProps) => {
           function onChange(date: Date) {
-            setFieldValue(name, date && date.toISOString());
+            setFieldValue(name, date && date.toISOString().slice(0, 10));
             setFieldTouched(name);
           }
 
           return (
             <DatePicker
               className="form-control"
+              dateFormat="yyyy-MM-dd"
               isClearable={true}
               onChange={onChange}
-              selected={value ? new Date(value) : null}
+              selected={value ? new Date(`${value}T12:00:00Z`) : null}
+              showYearDropdown={true}
               todayButton="Today"
             />
           );
