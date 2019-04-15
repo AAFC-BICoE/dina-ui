c20d610679d04051f1afba4bedbc216b378310f3
xuemeigan <xuemei.gan@canada.ca>
2019-04-15 16:28:28 -0400

    Fixed app test with i18n and updated snapshot

--- pages/__tests__/_app.test.tsx
+++ pages/__tests__/_app.test.tsx
@@ -2,28 +2,31 @@ import Kitsu from "kitsu";
 import Router from "next/router";
 import { FunctionComponent } from "react";
 import { create } from "react-test-renderer";
-import { createRenderer } from "react-test-renderer/shallow";
 import { ApiClientContext } from "../../components/api-client/ApiClientContext";
 import SeqdbUiApp from "../_app";
+import { shallow } from "enzyme";
+import toJson from "enzyme-to-json";
 
 jest.mock("next/router", () => ({
-  withRouter: () => ({})
+  withRouter: component => {
+    component.defaultProps = {
+      ...component.defaultProps,
+      router: { pathname: '' }
+    }
+    return component
+  }
 }));
 
 describe("SeqdbUiApp", () => {
   it("Renders the App wrapper.", () => {
     const TestComponent: FunctionComponent = () => <div />;
 
-    const shallowRender = createRenderer().render(
-
-      <SeqdbUiApp
-        router={Router}
-        pageProps={{ exampleProp: "exampleValue" }}
-        Component={TestComponent}
-      />
-    );
-
-    expect(shallowRender).toMatchSnapshot("SeqdbUiApp shallow render.");
+    const wrapper = shallow(<SeqdbUiApp
+      router={Router}
+      pageProps={{ exampleProp: "exampleValue" }}
+      Component={TestComponent}
+    />)
+    expect(toJson(wrapper)).toMatchSnapshot("SeqdbUiApp shallow render");
   });
 
   it("Provides the API context to child components.", done => {
