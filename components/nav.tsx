09de66740d70c2195dc9fc4e9cef646a71d0bca6
xuemeigan <xuemei.gan@canada.ca>
2019-05-16 14:11:32 -0400

    Remove unused dependencies, add version 0.37.0 for nexti18n and use bootstrap classes to emulate nav bar on current seqdb

--- components/nav.tsx
+++ components/nav.tsx
@@ -4,16 +4,19 @@ import { i18n, Trans, Link } from '../i18n'
 export class Nav extends React.Component {
   render() {
     return (
-      <nav className="navbar-expand-lg navbar-light bg-light">
-        <Link prefetch={true} href="/">
-          <a
-            className="navbar-brand"
-            style={{ color: "#1465b7", fontWeight: "bold" }}
-          >
-            <Trans i18nKey='Sequence Database' />
-          </a>
-        </Link>
-        <div style={{ float: 'right' }}>
+      <nav className="navbar-expand-lg navbar-light bg-light navbar navbar-default">
+        <div className="navbar-header">
+          <Link prefetch={true} href="/">
+            <a
+              className="navbar-brand"
+              style={{ color: "#1465b7", fontWeight: "bold" }}
+            >
+              <Trans i18nKey='Sequence Database' />
+            </a>
+          </Link>
+        </div>
+
+        <div className="col-md-10 offset-md-10">
           <a id="en" href="" onClick={() => i18n.changeLanguage('en')} >English |</a>
           <a id="fr" href="" onClick={() => i18n.changeLanguage('fr')} > French </a>
         </div>
