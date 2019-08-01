5115d25419881a610be8bc6d93515121fb170ccc
PoffM <matp@live.ca>
2019-08-01 10:24:23 -0400

    In progress  Feature #16700 Add Docker support
    -Removed http-server dependency.
    -Removed _app.tsx SSR workaround.
    -Removed "next export" build command.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,48 +10,15 @@ import { ApiClientContext, createContextValue } from "../components";
 export default class SeqdbUiApp extends App {
   private contextValue = createContextValue();
 
-  private hasMounted = false;
-
-  public componentDidMount() {
-    this.hasMounted = true;
-
-    // Reload the page after the App is mounted in the browser, because Next.js does not pass in
-    // the URL query string on the initial browser-side render.
-    // https://github.com/zeit/next.js/issues/2910
-    if (!this.isOnErrorPage()) {
-      this.props.router.push(this.props.router.asPath);
-    }
-  }
-
   public render() {
     const { Component, pageProps } = this.props;
 
     return (
-      // Render nothing on the first browser render to avoid passing an empty query string to
-      // the page component.
-      this.canRender() && (
-        <ApiClientContext.Provider value={this.contextValue}>
-          <Container>
-            <Component {...pageProps} />
-          </Container>
-        </ApiClientContext.Provider>
-      )
+      <ApiClientContext.Provider value={this.contextValue}>
+        <Container>
+          <Component {...pageProps} />
+        </Container>
+      </ApiClientContext.Provider>
     );
-  }
-
-  /** Whether the app is currently being rendered on the browser for the first time. */
-  private isFirstBrowserRender() {
-    const isRunningInBrowser = typeof window !== "undefined";
-    return isRunningInBrowser && !this.hasMounted;
-  }
-
-  /** Whether the app is on the 404 page. */
-  private isOnErrorPage() {
-    return this.props.router.route === "/_error";
-  }
-
-  /** Whether the page is ready to be rendered. */
-  private canRender() {
-    return !this.isFirstBrowserRender() || this.isOnErrorPage();
   }
 }
