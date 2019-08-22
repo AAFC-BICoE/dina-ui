33f0fb682f5fc6afef42863b86f0313c1f7c166c
Shemy Gan <xuemei.gan@canada.ca>
2019-08-22 14:23:14 -0400

    Feature 16960, UI - Section for drag and drop multiple files,added more testing code

--- page-tests/media-uploadView/upload.test.tsx
+++ page-tests/media-uploadView/upload.test.tsx
@@ -1,8 +1,15 @@
 /* eslint react/prop-types: 0, jsx-a11y/label-has-for: 0 */
-import { render } from "@testing-library/react";
+import { cleanup, render } from "@testing-library/react";
 import MediaUploadView from "../../pages/media-uploadView/upload";
 
 describe("MediaUploadView should have the styles and accepting multiple files", () => {
+  let files;
+  beforeEach(() => {
+    files = [createFile("file1.pdf", 1111, "application/pdf")];
+  });
+
+  afterEach(cleanup);
+
   describe("behavior", () => {
     it("renders the root and input nodes with the necessary props", () => {
       const { container } = render(<MediaUploadView />);
@@ -12,5 +19,57 @@ describe("MediaUploadView should have the styles and accepting multiple files", 
         "multiple"
       );
     });
+
+    it("When dropped the files, react table get populated with file names", async () => {
+      const event = createDtWithFiles(files);
+      const ui = <MediaUploadView />;
+      const { container } = render(ui);
+      const dropzone = container.querySelector(".container");
+      dispatchEvt(dropzone, "drop", event);
+      await flushPromises(ui, container);
+      expect(container.querySelector(".ReactTable")).toBeTruthy();
+      // expect(container.querySelector('.rt-noData')).toBeNull()
+    });
   });
 });
+
+function createFile(name, size, type) {
+  const file = new File([], name, { type });
+  Object.defineProperty(file, "size", {
+    get() {
+      return size;
+    }
+  });
+  return file;
+}
+
+function createDtWithFiles(files = []) {
+  return {
+    dataTransfer: {
+      files,
+      items: files.map(file => ({
+        getAsFile: () => file,
+        kind: "file",
+        type: file.type
+      })),
+      types: ["Files"]
+    }
+  };
+}
+
+function dispatchEvt(node, type, data) {
+  const event = new Event(type, { bubbles: true });
+  Object.assign(event, data);
+
+  event.preventDefault();
+  node.dispatchEvent(event);
+}
+
+function flushPromises(ui, container) {
+  return new Promise(resolve =>
+    global.setImmediate(() => {
+      render(ui, { container });
+      resolve(container);
+    })
+  );
+}
