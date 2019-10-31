0231a2d5e9c65d874deaebe63662f5f78e2d84fb
poffm <matp@live.ca>
2019-10-31 16:29:23 -0400

    In progress Feature #17109 Pooled Libraries workflow Step
    https://redmine.biodiversity.agr.gc.ca/issues/17109
    
    -Added "Next Step" button to workflow tabs.
    -Put the workflow step tab index into the url so you can link to a
     specific step of a workflow.

--- packages/seqdb-ui/pages/workflow/view.tsx
+++ packages/seqdb-ui/pages/workflow/view.tsx
@@ -1,88 +1,103 @@
-import { FieldView, LoadingSpinner, Query } from "common-ui";
+import { FieldView, LoadingSpinner, useQuery } from "common-ui";
 import { Formik } from "formik";
-import { WithRouterProps } from "next/dist/client/with-router";
 import Link from "next/link";
-import { withRouter } from "next/router";
+import { useRouter } from "next/router";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import { Head, Nav } from "../../components";
 import { StepRenderer } from "../../components/workflow/StepRenderer";
 import { Chain } from "../../types/seqdb-api/resources/workflow/Chain";
 import { ChainStepTemplate } from "../../types/seqdb-api/resources/workflow/ChainStepTemplate";
 
-export function WorkflowDetailsPage({ router }: WithRouterProps) {
-  const { id } = router.query;
+export default function WorkflowDetailsPage() {
+  const {
+    query: { id }
+  } = useRouter();
+
+  const { loading, response } = useQuery<Chain>({
+    include: "group,chainTemplate",
+    path: `chain/${id}`
+  });
 
   return (
-    <div>
+    <>
       <Head title="NGS Workflow" />
       <Nav />
-      <Query<Chain>
-        query={{ include: "group,chainTemplate", path: `chain/${id}` }}
-      >
-        {({ loading, response }) => (
-          <div className="container-fluid">
-            <Link href="/workflow/list">
-              <a>NGS Workflow list</a>
-            </Link>
-            <h1>NGS Workflow Details{response && `: ${response.data.name}`}</h1>
-            <LoadingSpinner loading={loading} />
-            {response && <WorkflowSteps chain={response.data} />}
-          </div>
-        )}
-      </Query>
-    </div>
+      <div className="container-fluid">
+        <Link href="/workflow/list">
+          <a>NGS Workflow list</a>
+        </Link>
+        <h1>NGS Workflow Details{response && `: ${response.data.name}`}</h1>
+        <LoadingSpinner loading={loading} />
+        {response && <WorkflowSteps chain={response.data} />}
+      </div>
+    </>
   );
 }
 
 function WorkflowSteps({ chain }: { chain: Chain }) {
-  return (
-    <Query<ChainStepTemplate[]>
-      query={{
-        filter: { "chainTemplate.id": chain.chainTemplate.id },
-        include: "stepTemplate",
-        path: "chainStepTemplate"
-      }}
-    >
-      {({ loading, response }) => {
-        const steps = response ? response.data : [];
+  const { loading, response } = useQuery<ChainStepTemplate[]>({
+    filter: { "chainTemplate.id": chain.chainTemplate.id },
+    include: "stepTemplate",
+    path: "chainStepTemplate"
+  });
 
-        return (
-          <>
-            <LoadingSpinner loading={loading} />
-            <Tabs>
-              <TabList>
-                <Tab>Details</Tab>
-                {steps.map(step => (
-                  <Tab key={step.id}>
-                    Step {step.stepNumber}: {step.stepTemplate.name}
-                  </Tab>
-                ))}
-              </TabList>
-              <TabPanel>
-                <Formik initialValues={chain} onSubmit={null}>
-                  <div className="col-md-3">
-                    <FieldView label="Template" name="chainTemplate.name" />
-                    <FieldView label="Group" name="group.groupName" />
-                    <FieldView name="name" />
-                    <FieldView name="dateCreated" />
-                  </div>
-                </Formik>
-              </TabPanel>
+  const router = useRouter();
+  const stepNumber = router.query.step ? Number(router.query.step) : 0;
+
+  function goToStep(newIndex: number) {
+    router.push({
+      pathname: router.pathname,
+      query: { ...router.query, step: newIndex }
+    });
+  }
+
+  function goToNextStep() {
+    goToStep(stepNumber + 1);
+  }
+
+  const steps = response ? response.data : [];
+
+  return (
+    <>
+      <LoadingSpinner loading={loading} />
+      <Tabs selectedIndex={stepNumber} onSelect={goToStep}>
+        <div className="list-inline">
+          <div className="list-inline-item">
+            <TabList>
+              <Tab>Details</Tab>
               {steps.map(step => (
-                <TabPanel key={step.id}>
-                  <StepRenderer
-                    chainStepTemplates={steps}
-                    chain={chain}
-                    step={step}
-                  />
-                </TabPanel>
+                <Tab key={step.id}>
+                  Step {step.stepNumber}: {step.stepTemplate.name}
+                </Tab>
               ))}
-            </Tabs>
-          </>
-        );
-      }}
-    </Query>
+            </TabList>
+          </div>
+          <div className="list-inline-item">
+            <button className="btn btn-primary" onClick={goToNextStep}>
+              Next Step
+            </button>
+          </div>
+        </div>
+        <TabPanel>
+          <Formik initialValues={chain} onSubmit={null}>
+            <div className="col-md-3">
+              <FieldView label="Template" name="chainTemplate.name" />
+              <FieldView label="Group" name="group.groupName" />
+              <FieldView name="name" />
+              <FieldView name="dateCreated" />
+            </div>
+          </Formik>
+        </TabPanel>
+        {steps.map(step => (
+          <TabPanel key={step.id}>
+            <StepRenderer
+              chainStepTemplates={steps}
+              chain={chain}
+              step={step}
+            />
+          </TabPanel>
+        ))}
+      </Tabs>
+    </>
   );
 }
-
-export default withRouter(WorkflowDetailsPage);
