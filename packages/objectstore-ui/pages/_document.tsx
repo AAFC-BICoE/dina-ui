3672f70b2c009635cd2832097b358772e212c864
Shemy Gan <xuemei.gan@canada.ca>
2020-01-10 11:32:14 -0500

    Feature 18276 Add Canada Common Look and Feel, Added _document page to override nextjs default and link to GCWeb/WET remotely

---
+++ packages/objectstore-ui/pages/_document.tsx
@@ -0,0 +1,108 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+import React from "react";
+
+/* tslint:disable:max-classes-per-file */
+class WetFoot extends React.Component {
+  public render() {
+    let dangerousInnerHTML;
+
+    if (this.props && this.props.children) {
+      if (typeof this.props.children !== "string") {
+        dangerousInnerHTML = "";
+      } else {
+        dangerousInnerHTML = this.props.children;
+      }
+    }
+
+    return <footer dangerouslySetInnerHTML={{ __html: dangerousInnerHTML }} />;
+  }
+}
+
+class WetHead extends React.Component {
+  public render() {
+    let dangerousInnerHTML;
+
+    if (this.props && this.props.children) {
+      if (typeof this.props.children !== "string") {
+        dangerousInnerHTML = "";
+      } else {
+        dangerousInnerHTML = this.props.children;
+      }
+    }
+
+    return <head dangerouslySetInnerHTML={{ __html: dangerousInnerHTML }} />;
+  }
+}
+
+class WetHtml extends React.Component {
+  public render() {
+    let dangerousInnerHTML;
+
+    if (this.props && this.props.children) {
+      if (typeof this.props.children !== "string") {
+        dangerousInnerHTML = "";
+      } else {
+        dangerousInnerHTML = this.props.children;
+      }
+    }
+
+    return <html dangerouslySetInnerHTML={{ __html: dangerousInnerHTML }} />;
+  }
+}
+class MyDocument extends Document {
+  public static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  public render() {
+    return (
+      <Html>
+        <WetHtml>
+          {`
+                    <html><!--[if lt IE 9]><html class="no-js lt-ie9" lang="en" dir="ltr"><![endif]--><!--[if gt IE 8]><!-->
+                    <html class="no-js" lang="en" dir="ltr">
+                    <!--<![endif]-->                
+                `}
+        </WetHtml>
+
+        <Head />
+        <WetHead>
+          {`
+                        <!--[if gte IE 9 | !IE ]><!-->
+                            <link href="https://wet-boew.github.io/themes-dist/GCWeb/assets/favicon.ico" rel="icon" type="image/x-icon">
+                            <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" integrity="sha384-50oBUHEmvpQ+1lW4y57PTFmhCaXp0ML5d60M1M7uH2+nqUivzIebhndOJK28anvf" crossorigin="anonymous">
+                            <link rel="stylesheet" href="https://wet-boew.github.io/themes-dist/GCWeb/css/theme.min.css">
+                            <!--<![endif]-->
+                            <!--[if lt IE 9]>
+                                    <link href="https://wet-boew.github.io/themes-dist/GCWeb/assets/favicon.ico" rel="shortcut icon" />
+                                    
+                                    <link rel="stylesheet" href="https://wet-boew.github.io/themes-dist/GCWeb/css/ie8-theme.min.css" />
+                                    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.js"></script>
+                                    <script src="https://wet-boew.github.io/themes-dist/wet-boew/js/ie8-wet-boew.min.js"></script>
+                                    <![endif]-->
+                            <!--[if lte IE 9]>		
+		            <![endif]--></link>`}
+        </WetHead>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+
+        <WetFoot>
+          {`
+                    <!--[if gte IE 9 | !IE ]><!-->
+                        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.js"></script>
+                        <script src="https://wet-boew.github.io/themes-dist/GCWeb/wet-boew/js/wet-boew.min.js"></script>
+                    <!--<![endif]-->
+                    <!--[if lt IE 9]>
+		                <script src="https://wet-boew.github.io/themes-dist/GCWeb/wet-boew/js/ie8-wet-boew2.min.js"></script>
+		        	<![endif]-->
+                    <script src="https://wet-boew.github.io/themes-dist/GCWeb/js/theme.min.js"></script>`}
+        </WetFoot>
+      </Html>
+    );
+  }
+}
+export default MyDocument;
