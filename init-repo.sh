6f7c8da50fa18e8b55ca354428e7710264d8f7e1
poffm <matp@live.ca>
2020-02-11 17:40:45 -0500

    Added seqdb-api / seqdb-ui launch configs

--- init-repo.sh
+++ init-repo.sh
@@ -10,7 +10,9 @@ code --install-extension ms-vscode.vscode-typescript-tslint-plugin
 mkdir repos
 git clone --branch dev https://github.com/AAFC-BICoE/dina-ui.git ./repos/dina-ui
 git clone --branch dev https://github.com/AAFC-BICoE/object-store-api.git ./repos/objectstore-api
+git clone --branch dev https://github.com/AAFC-BICoE/seqdb-api.git ./repos/seqdb-api
 
 # Install dependencies:
 mvn -f ./repos/objectstore-api/ -Dmaven.test.skip=true clean install
+mvn -f ./repos/seqdb-api/ -Dmaven.test.skip=true clean install
 yarn --cwd ./repos/dina-ui
