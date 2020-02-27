87b3e10dcd9edb5d56df82e1792a77f0196f481f
PoffM <matp@live.ca>
2020-02-27 17:03:46 -0500

    Feedback Support #18752 Consistent Date formats
    https://redmine.biodiversity.agr.gc.ca/issues/18752
    
    -Mocked the locale string so the test should work everywhere.

--- packages/objectstore-ui/utils/__tests__/dates.test.ts
+++ packages/objectstore-ui/utils/__tests__/dates.test.ts
@@ -1,5 +1,11 @@
 import { readableDate } from "../dates";
 
+// Locale can be different per machine running this, so mock the locale string:
+const MOCK_LOCALE_STRING = "2/27/2020, 4:46:05 PM";
+jest
+  .spyOn(Date.prototype, "toLocaleString")
+  .mockReturnValue(MOCK_LOCALE_STRING);
+
 describe("Dates utils", () => {
   it("Shows a readable date string from ISO format.", () => {
     expect(readableDate("2020-02-27T21:46:05.654Z")).toEqual(
