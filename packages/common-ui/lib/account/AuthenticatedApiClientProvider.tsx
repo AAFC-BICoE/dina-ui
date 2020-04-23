6f5b6b7d9bcadd73fd645538be67949a4b3a0ff6
Mat Poff <matp@live.ca>
2020-04-23 18:20:59 +0000

    In progress Feature 19131
    -Updated authenticated api client provider:
      -  Made all pages render only when authenticated
      - Fixed a bug with axios usage where api requests don't work in the same tab where you logged in.

--- packages/common-ui/lib/account/AuthenticatedApiClientProvider.tsx
+++ packages/common-ui/lib/account/AuthenticatedApiClientProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect } from "react";
+import { ReactNode, useEffect, useRef, useState } from "react";
 import {
   ApiClientContext,
   ApiClientContextConfig,
@@ -17,6 +17,7 @@ export function AuthenticatedApiClientProvider({
   children
 }: AuthenticatedApiClientProviderProps) {
   const { authenticated, initialized, login, token } = useAccount();
+  const authTokenRef = useRef<string>();
 
   // All pages require authentication.
   // Redirect to the login page if not logged in:
@@ -26,17 +27,25 @@ export function AuthenticatedApiClientProvider({
     }
   }, [authenticated, initialized]);
 
-  // Include the bearer token with every API request.
-  // 'Interceptors' is nullable here to support the old tests written before authentication was added,
-  // but in the running app it should always be available.
-  apiContext.apiClient.axios.interceptors?.request.use(config => {
-    config.headers.Authorization = `Bearer ${token}`;
-    return config;
-  });
+  // Include the bearer token with every API request:
+  useEffect(() => {
+    // 'Interceptors' is nullable here to support the old tests written before authentication was added,
+    // but in the running app it should always be available.
+    apiContext.apiClient.axios.interceptors?.request.use(config => {
+      // Get the token from a Ref so the up-to-date one is always used:
+      config.headers.Authorization = `Bearer ${authTokenRef.current}`;
+      return config;
+    });
+  }, [apiContext.apiClient.axios]);
+
+  // Update the token ref when useAccount's token changes:
+  useEffect(() => {
+    authTokenRef.current = token;
+  }, [token]);
 
   return (
     <ApiClientContext.Provider value={apiContext}>
-      {children}
+      {authenticated ? children : null}
     </ApiClientContext.Provider>
   );
 }
