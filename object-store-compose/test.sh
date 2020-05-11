8eac78b0ac059c1aef2ead6458b774a6d6019031
Mat Poff <matp@live.ca>
2020-05-11 17:19:27 +0000

    scaled ui and api to 0 in object store test script.

--- object-store-compose/test.sh
+++ object-store-compose/test.sh
@@ -5,6 +5,6 @@ export GID=$(id -g)
 
 DC_TEST="docker-compose -p dina-object-store-test -f ./docker-compose.base.yml -f ./docker-compose.test.yml"
 
-$DC_TEST up -d --scale mvn-test=0
+$DC_TEST up -d --scale mvn-test=0 --scale ui=0 --scale api=0
 $DC_TEST up mvn-test
 $DC_TEST down
