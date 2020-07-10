ac6618b066bd8d0f01b092bdab2f86fee7759ae5
Mat Poff <matp@live.ca>
2020-07-10 15:36:59 +0000

    In progress Support #19779 Remove excessive/duplicate config
    https://redmine.biodiversity.agr.gc.ca/issues/19779
    
    Reduced polyfills config, because nextjs should handle it out of the box now.

--- packages/dina-ui/jest.config.js
+++ packages/dina-ui/jest.config.js
@@ -8,19 +8,18 @@ module.exports = {
     "jest.config.js",
     "jest.setup.js",
     "next.config.js",
-    "polyfills.js"
   ],
   moduleNameMapper: {
     // Mocks CSS imports to prevent throwing an error during tests.
-    "\\.css$": "identity-obj-proxy"
+    "\\.css$": "identity-obj-proxy",
   },
   setupFiles: ["<rootDir>/jest.setup.js"],
   testPathIgnorePatterns: ["/.next/", "/node_modules/"],
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.(jsx?|js?|tsx?|ts?)$",
   transform: {
-    "^.+\\.tsx?$": "babel-jest"
+    "^.+\\.tsx?$": "babel-jest",
   },
   // Transform our local common-ui package
   transformIgnorePatterns: [`/node_modules/(?!common-ui)`],
-  snapshotSerializers: ["enzyme-to-json/serializer"]
+  snapshotSerializers: ["enzyme-to-json/serializer"],
 };
