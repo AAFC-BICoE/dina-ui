99761cd3498d6738201c443e231288787e13c6d9
xuemeigan <xuemei.gan@canada.ca>
2020-09-17 09:00:06 -0400

    Support 20236 Main page doesn't load in IE or Edge - Add browser detection for IE

--- packages/dina-ui/pages/index.tsx
+++ packages/dina-ui/pages/index.tsx
@@ -4,69 +4,73 @@ import Link from "next/link";
 import React from "react";
 import { Footer, Head, Nav } from "../components";
 import { DinaMessage } from "../intl/dina-ui-intl";
+import { isIE } from "react-device-detect";
 
-const Home: React.FunctionComponent = () => (
-  <div>
-    <Head title="Home" />
-    <Nav />
+const Home: React.FunctionComponent = () =>
+  isIE ? (
+    <div> IE is not supported. Download Chrome/Opera/Firefox </div>
+  ) : (
+    <div>
+      <Head title="Home" />
+      <Nav />
 
-    <div className="container">
-      <h2>
-        <DinaMessage id="objectStoreTitle" />
-      </h2>
-      <ul>
-        <li>
-          <Link href="/object-store/upload">
-            <a>
-              <DinaMessage id="uploadPageTitle" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/object-store/object/list">
-            <a>
-              <DinaMessage id="objectListTitle" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/object-store/managedAttributesView/listView">
-            <a>
-              <DinaMessage id="managedAttributeListTitle" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/object-store/object-subtype/list">
-            <a>
-              <DinaMessage id="objectSubtypeListTitle" />
-            </a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/object-store/revisions-by-user">
-            <a>
-              <DinaMessage id="revisionsByUserPageTitle" />
-            </a>
-          </Link>
-        </li>
-      </ul>
-      <h2>
-        <DinaMessage id="agentsSectionTitle" />
-      </h2>
-      <ul>
-        <li>
-          <Link href="/person/list">
-            <a>
-              <DinaMessage id="personListTitle" />
-            </a>
-          </Link>
-        </li>
-      </ul>
+      <div className="container">
+        <h2>
+          <DinaMessage id="objectStoreTitle" />
+        </h2>
+        <ul>
+          <li>
+            <Link href="/object-store/upload">
+              <a>
+                <DinaMessage id="uploadPageTitle" />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/object-store/object/list">
+              <a>
+                <DinaMessage id="objectListTitle" />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/object-store/managedAttributesView/listView">
+              <a>
+                <DinaMessage id="managedAttributeListTitle" />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/object-store/object-subtype/list">
+              <a>
+                <DinaMessage id="objectSubtypeListTitle" />
+              </a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/object-store/revisions-by-user">
+              <a>
+                <DinaMessage id="revisionsByUserPageTitle" />
+              </a>
+            </Link>
+          </li>
+        </ul>
+        <h2>
+          <DinaMessage id="agentsSectionTitle" />
+        </h2>
+        <ul>
+          <li>
+            <Link href="/person/list">
+              <a>
+                <DinaMessage id="personListTitle" />
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+
+      <Footer />
     </div>
-
-    <Footer />
-  </div>
-);
+  );
 
 export default Home;
