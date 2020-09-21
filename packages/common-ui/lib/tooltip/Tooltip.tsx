dccb3665a18e9ba9cbf937ac85d0c50cd0f63808
Mat Poff <matp@live.ca>
2020-09-21 22:06:06 +0000

    Feedback 20363
    -Changed Tooltip component so it can use a different initial visible element.
    -Added tooltip to the QueryTable to explain multi-sort.

--- packages/common-ui/lib/tooltip/Tooltip.tsx
+++ packages/common-ui/lib/tooltip/Tooltip.tsx
@@ -1,20 +1,28 @@
-import ReactTooltip from "react-tooltip";
+import { ReactNode } from "react";
 import { FormattedMessage } from "react-intl";
+import ReactTooltip from "react-tooltip";
 
 export interface TooltipProps {
+  /** The ID of the message to show in the tooltip. */
   id: string;
+
+  /**
+   * The element shown that you hover on to see the tooltip.
+   * Default is a small "i" image.
+   */
+  visibleElement?: ReactNode;
 }
 
-export function Tooltip({ id }: TooltipProps) {
+export function Tooltip({ id, visibleElement }: TooltipProps) {
   return (
     <span className="m-2">
-      <img
-        src="/static/images/iconInformation.gif"
-        data-tip={true}
-        data-for={id}
-      />
+      <span data-tip={true} data-for={id}>
+        {visibleElement ?? <img src="/static/images/iconInformation.gif" />}
+      </span>
       <ReactTooltip id={id}>
-        <FormattedMessage id={id} />
+        <div style={{ maxWidth: "15rem" }}>
+          <FormattedMessage id={id} />
+        </div>
       </ReactTooltip>
     </span>
   );
