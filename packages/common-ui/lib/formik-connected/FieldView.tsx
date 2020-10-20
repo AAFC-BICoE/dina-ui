8addf7b354f350ba3871541f35fb1f074c799b2d
xuemeigan <xuemei.gan@canada.ca>
2020-10-20 08:18:09 -0400

    Support 20582 Create orgnization view - Update fieldview to show organizations' names

--- packages/common-ui/lib/formik-connected/FieldView.tsx
+++ packages/common-ui/lib/formik-connected/FieldView.tsx
@@ -32,6 +32,8 @@ export function FieldView(props: FieldViewProps) {
                 .map(val =>
                   val.name
                     ? val.name
+                    : val.names
+                    ? val.names[0].name
                     : typeof val === "string"
                     ? val
                     : JSON.stringify(val)
