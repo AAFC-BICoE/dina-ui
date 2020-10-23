1bdb5abfc083040042da5e05bac22eadf18f5ae8
Mat Poff <matp@live.ca>
2020-10-23 20:38:38 +0000

    -Added docker-compose config for dina-user-api -Updated keycloak starter json for user-api related changes from dina-local-deployment.

--- init-repo.sh
+++ init-repo.sh
@@ -4,3 +4,4 @@ git clone --branch dev https://github.com/AAFC-BICoE/dina-ui.git ./repos/dina-ui
 git clone --branch dev https://github.com/AAFC-BICoE/object-store-api.git ./repos/objectstore-api
 git clone --branch dev https://github.com/AAFC-BICoE/seqdb-api.git ./repos/seqdb-api
 git clone --branch dev https://github.com/AAFC-BICoE/agent-api.git ./repos/agent-api
+git clone --branch dev https://github.com/AAFC-BICoE/dina-user-api.git ./repos/dina-user-api
--- object-store-compose/initdb.sql
+++ object-store-compose/initdb.sql
@@ -1,3 +1,4 @@
 CREATE SCHEMA object_store;
 CREATE SCHEMA keycloak;
 CREATE SCHEMA agent;
+CREATE SCHEMA userapi;
