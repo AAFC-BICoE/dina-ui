74ac0e915c9511833288f8234224640e37351ffc
Mat Poff <matp@live.ca>
2020-11-09 21:18:00 +0000

    20860
    Removed xmpRightsWebStatement edit column.

--- packages/dina-ui/pages/object-store/metadata/edit.tsx
+++ packages/dina-ui/pages/object-store/metadata/edit.tsx
@@ -112,10 +112,6 @@ export default function EditMetadatasPage() {
       data: "metadata.dcRights",
       title: formatMessage("field_dcRights")
     },
-    {
-      data: "metadata.xmpRightsWebStatement",
-      title: formatMessage("field_xmpRightsWebStatement")
-    },
     resourceSelectCell<License>(
       {
         label: license => license.titles[locale] ?? license.url,
