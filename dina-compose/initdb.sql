a5afacb8daeb8a0c80be1436c6ce261c16422517
Mat Poff <matp@live.ca>
2020-11-27 00:23:01 +0000

    move seqdb-ui into dina-ui.
    -Renamed object-store-compose to dina-compose.
    -Renamed keycloak clientId from objectstore to dina.
    -Added debug configs for all 4 java APIs.
    -Changed database name to dina.
    -Added seqdb api to dina-compose configs.
    -Removed old object-store-api testing script.

--- dina-compose/initdb.sql
+++ dina-compose/initdb.sql
@@ -1,4 +1,8 @@
 CREATE SCHEMA object_store;
+CREATE SCHEMA seqdb;
 CREATE SCHEMA keycloak;
 CREATE SCHEMA agent;
 CREATE SCHEMA userapi;
+
+set schema 'seqdb';
+create extension if not exists "pgcrypto";
