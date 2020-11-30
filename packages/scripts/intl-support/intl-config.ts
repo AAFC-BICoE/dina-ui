a9516cce46bf810818be93ff16a24d6f582d7d9e
Mat Poff <matp@live.ca>
2020-11-30 14:45:18 +0000

    21020
    Fixed intl config file paths.

--- packages/scripts/intl-support/intl-config.ts
+++ packages/scripts/intl-support/intl-config.ts
@@ -9,8 +9,8 @@ const {
 } = require("../../common-ui/lib/intl/common-ui-fr");
 const { DINAUI_MESSAGES_ENGLISH } = require("../../dina-ui/intl/dina-ui-en");
 const { DINAUI_MESSAGES_FRENCH } = require("../../dina-ui/intl/dina-ui-fr");
-const { SEQDB_MESSAGES_ENGLISH } = require("../../seqdb-ui/intl/seqdb-en");
-const { SEQDB_MESSAGES_FRENCH } = require("../../seqdb-ui/intl/seqdb-fr");
+const { SEQDB_MESSAGES_ENGLISH } = require("../../dina-ui/intl/seqdb-en");
+const { SEQDB_MESSAGES_FRENCH } = require("../../dina-ui/intl/seqdb-fr");
 
 // Type definitions and message file locations for exporting/importing app messages using CSV spreadsheets:
 
@@ -63,11 +63,11 @@ export const MESSAGE_GROUPS: MessageDictionary = {
   },
   seqdb: {
     english: {
-      file: join(__dirname, "../../../packages/seqdb-ui/intl/seqdb-en.ts"),
+      file: join(__dirname, "../../../packages/dina-ui/intl/seqdb-en.ts"),
       messages: SEQDB_MESSAGES_ENGLISH
     },
     french: {
-      file: join(__dirname, "../../../packages/seqdb-ui/intl/seqdb-fr.ts"),
+      file: join(__dirname, "../../../packages/dina-ui/intl/seqdb-fr.ts"),
       messages: SEQDB_MESSAGES_FRENCH
     }
   }
