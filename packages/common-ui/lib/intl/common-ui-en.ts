81641e2a6a580c5d45883d5cb0ad0e1399617668
Mat Poff <matp@live.ca>
2020-12-09 22:43:23 +0000

    Feedback 21136
    -Added error message and disabed the submit button when there is a validation error.
    -Added numbered rows to the table so the error tells which row has the error.

--- packages/common-ui/lib/intl/common-ui-en.ts
+++ packages/common-ui/lib/intl/common-ui-en.ts
@@ -8,6 +8,7 @@ export const COMMON_UI_MESSAGES_ENGLISH = {
   OR: "OR",
   areYouSure: "Are you sure?",
   backToListButtonText: "Back to List",
+  bulkEditorValidationError: "Invalid value on row {row}: {value}",
   cancelButtonText: "Cancel",
   checkAllTooltipMessage:
     "Check this header box to check all visible items in this page.",
