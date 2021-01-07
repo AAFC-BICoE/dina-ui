8fe71d179e38ef0a0ae9ac7f5e5ba18e47ec961f
Mat Poff <matp@live.ca>
2021-01-07 19:28:31 +0000

    In progress 21328
    -Started writing single record test.

---
+++ packages/dina-ui/page-tests/object-store/metadata/__tests__/single-record-edit.test.tsx
@@ -0,0 +1,103 @@
+import { PersistedResource } from "kitsu";
+import { License, Metadata, Person } from "../../../../types/objectstore-api";
+import MetadataEditPage from "../../../../pages/object-store/metadata/single-record-edit";
+import { mountWithAppContext } from "../../../../test-util/mock-app-context";
+
+const mockGet = jest.fn(async path => {
+  if (
+    path === "objectstore-api/metadata/25f81de5-bbee-430c-b5fa-71986b70e612"
+  ) {
+    return { data: TEST_METADATA };
+  } else if (path === "objectstore-api/license") {
+    return { data: TEST_LICENSES };
+  } else {
+    return { data: [] };
+  }
+});
+
+const mockBulkGet = jest.fn(async paths => {
+  if ((paths[0] as string).startsWith("/metadata/")) {
+    return TEST_METADATA;
+  }
+  if ((paths[0] as string).startsWith("/managed-attribute/")) {
+    return paths.map(() => ({
+      id: "a360a695-bbff-4d58-9a07-b6d6c134b208",
+      name: "existing-attribute"
+    }));
+  }
+  if ((paths[0] as string).startsWith("/object-upload/")) {
+    return paths.map(() => ({
+      id: "b4c8d6a6-0332-4f2a-a7b9-68b7898b6486",
+      dateTimeDigitized: "2020-12-17T23:37:45.932Z",
+      originalFilename: "test-file.png"
+    }));
+  }
+});
+
+const TEST_LICENSES: PersistedResource<License>[] = [
+  {
+    id: "open-government-license-canada",
+    type: "license",
+    url: "https://open.canada.ca/en/open-government-licence-canada",
+    titles: {
+      en: "Open Government Licence - Canada",
+      fr: "Licence du gouvernement ouvert – Canada"
+    }
+  }
+];
+
+const TEST_METADATA: PersistedResource<Metadata> = {
+  dcCreator: {
+    displayName: "Mat Poff",
+    id: "6e80e42a-bcf6-4062-9db3-946e0f26458f",
+    type: "person"
+  } as Person,
+  acTags: ["tag1", "tag2", "tag3"],
+  bucket: "testbucket",
+  dcType: "Image",
+  fileExtension: ".png",
+  fileIdentifier: "9a85b858-f8f0-4a97-99a8-07b2cb759766",
+  xmpRightsWebStatement:
+    "https://open.canada.ca/en/open-government-licence-canada",
+  id: "25f81de5-bbee-430c-b5fa-71986b70e612",
+  type: "metadata"
+};
+
+const mockSave = jest.fn();
+
+const apiContext: any = {
+  apiClient: { get: mockGet },
+  bulkGet: mockBulkGet,
+  save: mockSave
+};
+
+const mockUseRouter = jest.fn();
+
+// Pretend the metadata ids were passed in the URL:
+jest.mock("next/router", () => ({
+  useRouter: () => mockUseRouter()
+}));
+
+describe("Metadata single record edit page.", () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
+    mockSave.mockImplementation(args => args.map(({ resource }) => resource));
+    mockUseRouter.mockReturnValue({
+      query: {
+        id: "25f81de5-bbee-430c-b5fa-71986b70e612"
+      }
+    });
+  });
+
+  it("Lets you edit the Metadata.", async () => {
+    const wrapper = mountWithAppContext(<MetadataEditPage />, { apiContext });
+
+    await new Promise(setImmediate);
+    wrapper.update();
+
+    expect(wrapper.find(".originalFilename-field input").prop("value")).toEqual(
+      ""
+    );
+    expect(wrapper.find(".acTags-field textarea").prop("value")).toEqual("");
+  });
+});
