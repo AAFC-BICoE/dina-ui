5e7ca2915722adc5d607728c5197d115383f1393
xuemeigan <xuemei.gan@canada.ca>
2021-01-14 09:43:54 -0500

    Support 21257 Create Collecting event details page collectors section - Expose collection section in nav and index page which includes collecting event and collector group

--- packages/dina-ui/components/button-bar/nav/nav.tsx
+++ packages/dina-ui/components/button-bar/nav/nav.tsx
@@ -79,7 +79,7 @@ export function Nav() {
               <NavSeqDBDropdown />
             </li>
             <li className="list-inline-item my-auto">
-              <NavCollectingEventDropdown />
+              <NavCollectionDropdown />
             </li>
           </ul>
         </div>
@@ -216,7 +216,7 @@ function NavSeqDBDropdown() {
 }
 
 /** Collecting event links. */
-function NavCollectingEventDropdown() {
+function NavCollectionDropdown() {
   return (
     <div className="dropdown">
       <a className="nav-link dropdown-toggle" href="#">
@@ -226,6 +226,11 @@ function NavCollectingEventDropdown() {
         <Link href="/collecting-event/list">
           <a className="dropdown-item">
             <DinaMessage id="collectingEventListTitle" />
+          </a>
+        </Link>
+        <Link href="/collector-group/list">
+          <a className="dropdown-item">
+            <DinaMessage id="collectorGroupListTitle" />
           </a>
         </Link>
       </div>
