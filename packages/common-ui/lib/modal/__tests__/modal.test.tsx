00dcfd31adb771eaf1e0348653977272425ebf5c
Mat Poff <matp@live.ca>
2021-02-05 23:23:49 +0000

    Feedback 21650
    -Changed modal state from one element to an array.
    -Added test for multiple modals.
    -Fixed some styling when multiple modals are open.

--- packages/common-ui/lib/modal/__tests__/modal.test.tsx
+++ packages/common-ui/lib/modal/__tests__/modal.test.tsx
@@ -15,6 +15,20 @@ function TestComponent() {
   );
 }
 
+function TestComponentNestedModals() {
+  const { closeModal, openModal } = useModal();
+
+  return (
+    <>
+      <button
+        className="open-modal"
+        onClick={() => openModal(<TestComponentNestedModals />)}
+      />
+      <button className="close-modal" onClick={closeModal} />
+    </>
+  );
+}
+
 describe("Modal", () => {
   it("Lets you open a modal.", () => {
     const wrapper = mount(
@@ -37,5 +51,42 @@ describe("Modal", () => {
     wrapper.update();
 
     expect(wrapper.find(".test-modal-content").exists()).toEqual(false);
+  });
+
+  it("Lets you open multiple modals in first-in-last-out order.", async () => {
+    const wrapper = mount(
+      <ModalProvider appElement={document.querySelector("body")}>
+        <TestComponentNestedModals />
+      </ModalProvider>
+    );
+
+    // Open the modal:
+    wrapper.find("button.open-modal").simulate("click");
+    wrapper.update();
+
+    // Open the inner modal:
+    wrapper.find("button.open-modal").at(1).simulate("click");
+    wrapper.update();
+
+    expect(wrapper.find(".dina-modal-wrapper").length).toEqual(2);
+
+    // Original modal is hidden:
+    expect(wrapper.find(".dina-modal-wrapper").at(0).prop("style")).toEqual({
+      display: "none"
+    });
+
+    // Second modal is visible:
+    expect(wrapper.find(".dina-modal-wrapper").at(1).prop("style")).toEqual({});
+
+    // Close second modal:
+    wrapper.find("button.close-modal").at(1).simulate("click");
+    wrapper.update();
+
+    // Close first modal:
+    wrapper.find("button.close-modal").at(0).simulate("click");
+    wrapper.update();
+
+    // No modals left:
+    expect(wrapper.find(".dina-modal-wrapper").length).toEqual(0);
   });
 });
--- packages/common-ui/lib/modal/modal.tsx
+++ packages/common-ui/lib/modal/modal.tsx
@@ -1,4 +1,11 @@
-import { createContext, ReactNode, useContext, useState } from "react";
+import {
+  createContext,
+  Fragment,
+  ReactNode,
+  useContext,
+  useRef,
+  useState
+} from "react";
 import ReactModal from "react-modal";
 
 interface ModalContextI {
@@ -20,26 +27,43 @@ const ModalContext = createContext<ModalContextI>(
   undefined as any
 );
 
-/**  */
+/** Allows opening modals using the "useModal" hook: */
 export function ModalProvider({ appElement, children }: ModalProviderProps) {
-  const [Component, setComponent] = useState<JSX.Element | null>(null);
+  const [modals, setModals] = useState<JSX.Element[]>([]);
+
+  function openModal(newModal: JSX.Element) {
+    // Add the new modal at the end of the array:
+    setModals([...modals, newModal]);
+  }
+
+  function closeModal() {
+    // Remove the outer-most modal:
+    setModals(current => current.slice(0, -1));
+  }
 
   return (
-    <ModalContext.Provider
-      value={{
-        closeModal: () => setComponent(null),
-        openModal: setComponent
-      }}
-    >
+    <ModalContext.Provider value={{ closeModal, openModal }}>
       {children}
+      <style>{`.modal-content { margin: auto }`}</style>
       <ReactModal
         appElement={appElement ?? undefined}
-        isOpen={!!Component}
+        isOpen={!!modals.length}
         className="Modal__Bootstrap modal-dialog"
         // Make sure the modal is in front of the Bootstrap nav bar:
         style={{ overlay: { zIndex: 1040 } }}
       >
-        {Component}
+        {modals.map((modal, index) => (
+          <div
+            className="dina-modal-wrapper"
+            key={index}
+            style={{
+              display: index === modals.length - 1 ? undefined : "none"
+            }}
+          >
+            <style>{`.modal-dialog { max-width: 500px }`}</style>
+            {modal}
+          </div>
+        ))}
       </ReactModal>
     </ModalContext.Provider>
   );
