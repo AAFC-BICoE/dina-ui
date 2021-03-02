d345acb9aa7685cbcc19197d465d4dc8ea642b7e
xuemeigan <xuemei.gan@canada.ca>
2021-03-02 14:31:29 -0500

    Support 21901 Add fields to GeoreferenceAssertion - Update FieldView to display number - Layout adjustment on edit and view pages

--- packages/dina-ui/components/collection/GeoReferenceAssertionRow.tsx
+++ packages/dina-ui/components/collection/GeoReferenceAssertionRow.tsx
@@ -18,118 +18,106 @@ export function GeoReferenceAssertionRow({
 }: GeoReferenceAssertionRowProps) {
   const { formatMessage } = useDinaIntl();
   return (
-    <div className="row">
-      <div className="col-md-6">
-        {viewOnly ? (
-          <FieldView
-            name={`geoReferenceAssertions[${index}].dwcDecimalLatitude`}
-            label={formatMessage("decimalLatitude")}
-            className={"dwcDecimalLatitude"}
-          />
-        ) : (
-          <NumberField
-            name={`geoReferenceAssertions[${index}].dwcDecimalLatitude`}
-            label={formatMessage("decimalLatitude")}
-            className={"dwcDecimalLatitude"}
-          />
-        )}
-        <NumberField
-          name={`geoReferenceAssertions[${index}].dwcDecimalLongitude`}
-          label={formatMessage("decimalLongitude")}
-          readOnly={viewOnly}
-          className={"dwcDecimalLongitude"}
-        />
-        <NumberField
-          name={`geoReferenceAssertions[${index}].dwcCoordinateUncertaintyInMeters`}
-          label={formatMessage("coordinateUncertaintyInMeters")}
-          readOnly={viewOnly}
-          className={"dwcCoordinateUncertaintyInMeters"}
-        />
-        {viewOnly ? (
-          <FieldView
-            name={`geoReferenceAssertions[${index}].dwcGeoreferencedDate`}
-            className={"dwcGeoreferencedDate"}
-            label={formatMessage("georeferencedDateLabel")}
-          />
-        ) : (
-          <DateField
-            name={`geoReferenceAssertions[${index}].dwcGeoreferencedDate`}
-            className={"dwcGeoreferencedDate"}
-            label={formatMessage("georeferencedDateLabel")}
-            withZone={true}
-          />
-        )}
-      </div>
+    <>
       {viewOnly ? (
-        <div>
-          <FieldView
-            name={`geoReferenceAssertions[${index}].literalGeoreferencedBy`}
-            className={"literalGeoreferencedBy"}
-            label={formatMessage("literalGeoreferencedByLabel")}
-          />
-          <FieldView
-            name={`geoReferenceAssertions[${index}].dwcGeoreferenceProtocol`}
-            className={"dwcGeoreferenceProtocol"}
-            label={formatMessage("georeferenceProtocolLabel")}
-          />
-          <FieldView
-            name={`geoReferenceAssertions[${index}].dwcGeoreferenceSources`}
-            className={"dwcGeoreferenceSources"}
-            label={formatMessage("georeferenceSourcesLabel")}
-          />
-          <FieldView
-            name={`geoReferenceAssertions[${index}].dwcGeoreferenceRemarks`}
-            className={"dwcGeoreferenceRemarks"}
-            label={formatMessage("georeferenceRemarksLabel")}
-          />
+        <div className="row">
+          <div className="col-md-5">
+            <FieldView
+              name={`geoReferenceAssertions[${index}].dwcDecimalLatitude`}
+              label={formatMessage("decimalLatitude")}
+              className={"dwcDecimalLatitude"}
+            />
+            <FieldView
+              name={`geoReferenceAssertions[${index}].dwcDecimalLongitude`}
+              label={formatMessage("decimalLongitude")}
+              className={"dwcDecimalLongitude"}
+            />
+            <FieldView
+              name={`geoReferenceAssertions[${index}].dwcCoordinateUncertaintyInMeters`}
+              label={formatMessage("coordinateUncertaintyInMeters")}
+              className={"dwcCoordinateUncertaintyInMeters"}
+            />
+            <FieldView
+              name={`geoReferenceAssertions[${index}].dwcGeoreferencedDate`}
+              className={"dwcGeoreferencedDate"}
+              label={formatMessage("georeferencedDateLabel")}
+            />
+          </div>
+          <div className="col-md-5">
+            <FieldView
+              name={`geoReferenceAssertions[${index}].literalGeoreferencedBy`}
+              className={"literalGeoreferencedBy"}
+              label={formatMessage("literalGeoreferencedByLabel")}
+            />
+            <FieldView
+              name={`geoReferenceAssertions[${index}].dwcGeoreferenceProtocol`}
+              className={"dwcGeoreferenceProtocol"}
+              label={formatMessage("georeferenceProtocolLabel")}
+            />
+            <FieldView
+              name={`geoReferenceAssertions[${index}].dwcGeoreferenceSources`}
+              className={"dwcGeoreferenceSources"}
+              label={formatMessage("georeferenceSourcesLabel")}
+            />
+            <FieldView
+              name={`geoReferenceAssertions[${index}].dwcGeoreferenceRemarks`}
+              className={"dwcGeoreferenceRemarks"}
+              label={formatMessage("georeferenceRemarksLabel")}
+            />
+          </div>
         </div>
       ) : (
-        <div className="col-md-5">
-          <TextField
-            name={`geoReferenceAssertions[${index}].literalGeoreferencedBy`}
-            className={"literalGeoreferencedBy"}
-            label={formatMessage("literalGeoreferencedByLabel")}
-          />
-          <TextField
-            name={`geoReferenceAssertions[${index}].dwcGeoreferenceProtocol`}
-            className={"dwcGeoreferenceProtocol"}
-            label={formatMessage("georeferenceProtocolLabel")}
-          />
-          <TextField
-            name={`geoReferenceAssertions[${index}].dwcGeoreferenceSources`}
-            className={"dwcGeoreferenceSources"}
-            label={formatMessage("georeferenceSourcesLabel")}
-          />
-          <TextField
-            name={`geoReferenceAssertions[${index}].dwcGeoreferenceRemarks`}
-            className={"dwcGeoreferenceRemarks"}
-            label={formatMessage("georeferenceRemarksLabel")}
-          />
-        </div>
-      )}
-
-      {!viewOnly && (
-        <div className="d-none">
-          <div className="list-inline-item">
-            <button
-              className="btn btn-primary add-assertion-button"
-              type="button"
-              onClick={onAddClick}
-            >
-              +
-            </button>
+        <div className="row">
+          <div className="col-md-5">
+            <NumberField
+              name={`geoReferenceAssertions[${index}].dwcDecimalLatitude`}
+              label={formatMessage("decimalLatitude")}
+              className={"dwcDecimalLatitude"}
+            />
+            <NumberField
+              name={`geoReferenceAssertions[${index}].dwcDecimalLongitude`}
+              label={formatMessage("decimalLongitude")}
+              readOnly={viewOnly}
+              className={"dwcDecimalLongitude"}
+            />
+            <NumberField
+              name={`geoReferenceAssertions[${index}].dwcCoordinateUncertaintyInMeters`}
+              label={formatMessage("coordinateUncertaintyInMeters")}
+              readOnly={viewOnly}
+              className={"dwcCoordinateUncertaintyInMeters"}
+            />
+            <DateField
+              name={`geoReferenceAssertions[${index}].dwcGeoreferencedDate`}
+              className={"dwcGeoreferencedDate"}
+              label={formatMessage("georeferencedDateLabel")}
+              withZone={true}
+            />
           </div>
-          <div className="list-inline-item">
-            <button
-              className="btn btn-primary"
-              type="button"
-              onClick={onRemoveClick}
-            >
-              -
-            </button>
-          </div>{" "}
+
+          <div className="col-md-5">
+            <TextField
+              name={`geoReferenceAssertions[${index}].literalGeoreferencedBy`}
+              className={"literalGeoreferencedBy"}
+              label={formatMessage("literalGeoreferencedByLabel")}
+            />
+            <TextField
+              name={`geoReferenceAssertions[${index}].dwcGeoreferenceProtocol`}
+              className={"dwcGeoreferenceProtocol"}
+              label={formatMessage("georeferenceProtocolLabel")}
+            />
+            <TextField
+              name={`geoReferenceAssertions[${index}].dwcGeoreferenceSources`}
+              className={"dwcGeoreferenceSources"}
+              label={formatMessage("georeferenceSourcesLabel")}
+            />
+            <TextField
+              name={`geoReferenceAssertions[${index}].dwcGeoreferenceRemarks`}
+              className={"dwcGeoreferenceRemarks"}
+              label={formatMessage("georeferenceRemarksLabel")}
+            />
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 }
