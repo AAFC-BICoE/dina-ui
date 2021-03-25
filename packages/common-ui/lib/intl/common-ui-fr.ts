6e734f551f2fd612745888d2ea71a4c4ac7bcc66
xuemeigan <xuemei.gan@canada.ca>
2021-03-25 14:19:52 -0400

    Support 22145 Collecting Agents should be clickable - Check in yarn lint: fixes

--- packages/common-ui/lib/intl/common-ui-fr.ts
+++ packages/common-ui/lib/intl/common-ui-fr.ts
@@ -1,6 +1,8 @@
 import { COMMON_UI_MESSAGES_ENGLISH } from "./common-ui-en";
 
-export const COMMON_UI_MESSAGES_FR: Partial<typeof COMMON_UI_MESSAGES_ENGLISH> = {
+export const COMMON_UI_MESSAGES_FR: Partial<
+  typeof COMMON_UI_MESSAGES_ENGLISH
+> = {
   AND: "ET",
   IS: "EST",
   ISNOT: "N'EST PAS",
