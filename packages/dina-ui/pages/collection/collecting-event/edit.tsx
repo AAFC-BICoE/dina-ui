7ae98fd04b96c51b44ce26c5515888ed59cfb456
Mat Poff <matp@live.ca>
2021-04-08 17:34:44 +0000

    Feedback 22260
    -Set the Collecting Event ID after saving to prevent duplicate POST requests after an error.

--- packages/dina-ui/pages/collection/collecting-event/edit.tsx
+++ packages/dina-ui/pages/collection/collecting-event/edit.tsx
@@ -218,7 +218,7 @@ function CollectingEventForm({
     }
   }
 
-  const onSubmit: DinaFormOnSubmit = async ({ submittedValues }) => {
+  const onSubmit: DinaFormOnSubmit = async ({ submittedValues, formik }) => {
     // Init relationships object for one-to-many relations:
     submittedValues.relationships = {};
 
@@ -312,6 +312,10 @@ function CollectingEventForm({
         apiBaseUrl: "/collection-api"
       }
     );
+
+    // Set the Collecting Event ID so if there is an error after this,
+    // then subsequent submissions use PATCH instea of POST:
+    formik.setFieldValue("id", savedCollectingEvent.id);
 
     // save georeference assertions:
     await saveGeoReferenceAssertion(
