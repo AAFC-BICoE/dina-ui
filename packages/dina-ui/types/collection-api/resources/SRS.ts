8d0297858ed51a173e96d3f2098734f618b0ef32
Shemy <shemy.gan@gmail.com>
2021-04-14 16:10:04 -0400

    Support 22286 use verbatim coordinates system dropdown to pick verbatim lat long entry - Add default value for cooridnate system and src

--- packages/dina-ui/types/collection-api/resources/SRS.ts
+++ packages/dina-ui/types/collection-api/resources/SRS.ts
@@ -4,4 +4,9 @@ interface SRSAttributes {
   srs: string[];
 }
 
+export enum SRSEnum {
+  WGS84 = "WGS84 (EPSG:4326)",
+  NAD27 = "NAD27 (EPSG:4276)"
+}
+
 export type SRS = KitsuResource & SRSAttributes;
