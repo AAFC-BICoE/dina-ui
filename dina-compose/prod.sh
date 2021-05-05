5eead6369f844f4ccd97d2dfc0d18da6aa1e470b
Mat Poff <matp@live.ca>
2021-05-05 14:31:39 +0000

    -Moved prod-specific compose config into separate file.

--- dina-compose/prod.sh
+++ dina-compose/prod.sh
@@ -1,3 +1,3 @@
 # Short-hand way of running docker-compose with the prod launch config.
 
-docker-compose -p dina-object-store-prod -f ./docker-compose.base.yml $@
+docker-compose -p dina-object-store-prod -f ./docker-compose.base.yml -f ./docker-compose.prod.yml $@
