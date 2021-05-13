c966ede7760c2b3777fe56bf3b0ae803b3b99c63
Mat Poff <matp@live.ca>
2021-05-13 21:28:04 +0000

    Feedback 22664
    Call scrollIntoView when a new error appears.

--- packages/common-ui/lib/formik-connected/ErrorViewer.tsx
+++ packages/common-ui/lib/formik-connected/ErrorViewer.tsx
@@ -1,15 +1,29 @@
 import { connect } from "formik";
+import { useEffect, useRef } from "react";
 
 /** Renders the Formik status as an error message. */
 export const ErrorViewer = connect(function ErrorViewerInternal({
   formik: { status }
 }) {
-  return status ? (
-    <div
-      className="alert alert-danger"
-      style={{ whiteSpace: "pre-wrap", wordWrap: "break-word" }}
-    >
-      {status}
+  const wrapperRef = useRef<HTMLDivElement>(null);
+
+  // When there is a new error, scroll to it:
+  useEffect(() => {
+    if (status) {
+      wrapperRef.current?.scrollIntoView?.();
+    }
+  }, [status]);
+
+  return (
+    <div ref={wrapperRef} style={{ scrollMargin: "20px" }}>
+      {status ? (
+        <div
+          className="alert alert-danger"
+          style={{ whiteSpace: "pre-wrap", wordWrap: "break-word" }}
+        >
+          {status}
+        </div>
+      ) : null}
     </div>
-  ) : null;
+  );
 });
