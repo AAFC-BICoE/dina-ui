ffab5a90b03ab5ce6502c5a65713a9cef89341c2
Shemy <shemy.gan@gmail.com>
2021-05-13 14:25:12 -0400

    Support 22648 WCAG issue object store managed attribute edit view page - Fix heading issue and clean up code

--- packages/dina-ui/pages/object-store/managedAttributesView/detailsView.tsx
+++ packages/dina-ui/pages/object-store/managedAttributesView/detailsView.tsx
@@ -179,23 +179,12 @@ function ManagedAttributeForm({ profile, router }: ManagedAttributeFormProps) {
       )}
       {id && (
         <div style={{ width: "25rem" }}>
-          <h2>
-            <DinaMessage id="field_managedAttributeCreatedOn" />
-          </h2>
-          <DateField
-            showTime={true}
-            name="createdOn"
-            disabled={true}
-            hideLabel={true}
-          />
+          <DateField showTime={true} name="createdOn" disabled={true} />
         </div>
       )}
       {id && (
         <div style={{ width: "25rem" }}>
-          <h2>
-            <DinaMessage id="field_managedAttributeCreatedBy" />
-          </h2>
-          <TextField name="createdBy" hideLabel={true} readOnly={true} />
+          <TextField name="createdBy" readOnly={true} />
         </div>
       )}
     </DinaForm>
