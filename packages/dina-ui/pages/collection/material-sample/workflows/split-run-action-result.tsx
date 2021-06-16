00666f49e68b37b059ab6a141a9e8da481316f8e
Shemy <shemy.gan@gmail.com>
2021-06-16 09:14:40 -0400

    Support-22663-workflow-part-2-running-through-a-split-workflow-actiob-as-a-user - Remove Parent name not found message as it might cause confusion than being helpful

--- packages/dina-ui/pages/collection/material-sample/workflows/split-run-action-result.tsx
+++ packages/dina-ui/pages/collection/material-sample/workflows/split-run-action-result.tsx
@@ -77,11 +77,6 @@ export default function SplitRunActionResult() {
                     <DinaMessage id="destroyedLabel" />{" "}
                   </span>
                 </>
-              ) : !splitChildSampleRunActionResult?.parentSampleId ? (
-                <span className="text-danger">
-                  (
-                  <DinaMessage id="parentSampleNotFoundLabel" /> )
-                </span>
               ) : null}
             </span>
             <span className="fw-bold">
