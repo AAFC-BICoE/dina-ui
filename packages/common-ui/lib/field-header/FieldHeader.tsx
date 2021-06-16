fc9ded41372be8a1a5f2bf4747b8587a9994dd09
Brandon Andre <andr0284@algonquinlive.com>
2021-06-16 10:48:49 -0400

    Feature #23062 - Ability to add images or links to tooltips
    https://redmine.biodiversity.agr.gc.ca/issues/23062
    
    - Started adding optional image and link fields, can now be added directly to the FieldHeader.

--- packages/common-ui/lib/field-header/FieldHeader.tsx
+++ packages/common-ui/lib/field-header/FieldHeader.tsx
@@ -7,16 +7,28 @@ export interface FieldNameProps {
 
   /** Override the default "name" prop used to get labels and tooltips from the intl messages. */
   customName?: string;
+
+  /** Optional image source to display an image in a tooltip. */
+  tooltipImage?: string,
+
+  /** Optional image alt text for image accessability. */
+  tooltipImageAlt?: string,
+
+  /** Optional link. */
+  tooltipLink?: string,
 }
 
 /** Get the field label and tooltip given the camelCase field key. */
 export function useFieldLabels() {
   const { formatMessage, messages } = useIntl();
 
-  function getFieldLabel(name: string) {
+  function getFieldLabel(name: string, tooltipImage?: string, tooltipImageAlt?: string, tooltipLink?: string) {
     const messageKey = `field_${name}`;
     const tooltipKey = `${messageKey}_tooltip`;
-    const tooltip = messages[tooltipKey] ? <Tooltip id={tooltipKey} /> : null;
+    const tooltip =
+      messages[tooltipKey] || tooltipImage || tooltipLink ? (
+        <Tooltip id={tooltipKey} image={tooltipImage} altImage={tooltipImageAlt} link={tooltipLink} />
+      ) : null;
 
     const fieldLabel = messages[messageKey]
       ? formatMessage({ id: messageKey as any })
@@ -33,9 +45,9 @@ export function useFieldLabels() {
  * The tooltip is found from the intl messages file using the key "field_{fieldName}_tooltip".
  * e.g. field_acMetadataCreator.displayName_tooltip
  */
-export function FieldHeader({ name, customName }: FieldNameProps) {
+export function FieldHeader({ name, customName, tooltipImage, tooltipImageAlt, tooltipLink }: FieldNameProps) {
   const { getFieldLabel } = useFieldLabels();
-  const { fieldLabel, tooltip } = getFieldLabel(customName ?? name);
+  const { fieldLabel, tooltip } = getFieldLabel((customName ?? name), tooltipImage, tooltipImageAlt, tooltipLink);
 
   return (
     <div className={`${customName ?? name}-field-header`}>
--- packages/common-ui/lib/tooltip/Tooltip.tsx
+++ packages/common-ui/lib/tooltip/Tooltip.tsx
@@ -2,9 +2,9 @@ import RcTooltip from "rc-tooltip";
 import { ReactNode } from "react";
 import { FormattedMessage } from "react-intl";
 
-export interface TooltipProps {
+interface TooltipProps {
   /** The ID of the message to show in the tooltip. */
-  id: string;
+  id?: string;
 
   /** Intl message arguments */
   intlValues?: Record<string, any>;
@@ -14,16 +14,52 @@ export interface TooltipProps {
    * Default is a small "i" image.
    */
   visibleElement?: ReactNode;
+
+  /** Link attachment, links will always be opened in a new tab. */
+  link?: string,
+
+  /** Image attachment, will display it under the tooltip message. */
+  image?: string,
+
+  /** Image accessability text. */
+  altImage?: string;
 }
 
-export function Tooltip({ id, intlValues, visibleElement }: TooltipProps) {
+export function Tooltip({ id, intlValues, visibleElement, link, image, altImage }: TooltipProps) {
+
+  let tooltipMessage;
+  let tooltipImage;
+  let tooltipLink;
+
+  // Determine if a tooltip message needs to be displayed.
+  if (id != null) {
+    tooltipMessage = <FormattedMessage id={id} values={intlValues} />;
+  }
+
+  // Determine if an image should be displayed.
+  if (image != null) {
+    tooltipImage = (
+      <div>
+        <img src={image} alt={altImage} />
+      </div>
+    );
+  }
+
+  // Determine if a link should be displayed.
+  if (link != null) {
+    tooltipLink = <a href={link} target="_blank"></a>;
+  }
+
   return (
     <span className="m-2">
       <RcTooltip
         id={id}
         overlay={
+          
           <div style={{ maxWidth: "15rem", whiteSpace: "pre-wrap" }}>
-            <FormattedMessage id={id} values={intlValues} />
+            {tooltipMessage}
+            {tooltipImage}
+            {tooltipLink}
           </div>
         }
         placement="top"
