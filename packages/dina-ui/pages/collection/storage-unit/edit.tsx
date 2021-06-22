2379f3a34a3ed9941c8c3013bc9b5866dbd65716
Mat Poff <matp@live.ca>
2021-06-22 21:13:02 +0000

    In progress 23171
    -Added nav / home page links.
    -Added simple working list / edit / view pages for Storage Unit.

---
+++ packages/dina-ui/pages/collection/storage-unit/edit.tsx
@@ -0,0 +1,142 @@
+import {
+  BackButton,
+  ButtonBar,
+  DateField,
+  DinaForm,
+  DinaFormSubmitParams,
+  filterBy,
+  ResourceSelectField,
+  SubmitButton,
+  TextField,
+  useDinaFormContext,
+  useQuery,
+  withResponse
+} from "common-ui";
+import { PersistedResource } from "kitsu";
+import { useRouter } from "next/router";
+import { GroupSelectField, Head, Nav } from "../../../components";
+import { DinaMessage, useDinaIntl } from "../../../intl/dina-ui-intl";
+import { StorageUnit } from "../../../types/collection-api";
+
+export function useStorageUnit(id?: string) {
+  return useQuery<StorageUnit>(
+    { path: `collection-api/storage-unit/${id}`, include: "parentStorageUnit" },
+    { disabled: !id }
+  );
+}
+
+export default function StorageUnitEditPage() {
+  const router = useRouter();
+  const { formatMessage } = useDinaIntl();
+
+  const id = router.query.id?.toString();
+
+  const storageUnitQuery = useStorageUnit(id);
+
+  const title = id ? "editStorageUnitTitle" : "addStorageUnitTitle";
+
+  async function goToViewPage(resource: PersistedResource<StorageUnit>) {
+    await router.push(`/collection/storage-unit/view?id=${resource.id}`);
+  }
+
+  return (
+    <div>
+      <Head title={formatMessage(title)} />
+      <Nav />
+      <div className="container">
+        <h1>
+          <DinaMessage id={title} />
+        </h1>
+        {id ? (
+          withResponse(storageUnitQuery, ({ data }) => (
+            <StorageUnitForm storageUnit={data} onSaved={goToViewPage} />
+          ))
+        ) : (
+          <StorageUnitForm onSaved={goToViewPage} />
+        )}
+      </div>
+    </div>
+  );
+}
+
+export interface StorageUnitFormProps {
+  storageUnit?: PersistedResource<StorageUnit>;
+  onSaved: (storageUnit: PersistedResource<StorageUnit>) => Promise<void>;
+}
+
+export function StorageUnitForm({
+  storageUnit,
+  onSaved
+}: StorageUnitFormProps) {
+  const initialValues = storageUnit || { type: "storage-unit" };
+
+  async function onSubmit({
+    submittedValues,
+    api: { save }
+  }: DinaFormSubmitParams<StorageUnit>) {
+    const [savedStorage] = await save<StorageUnit>(
+      [
+        {
+          resource: submittedValues,
+          type: "storage-unit"
+        }
+      ],
+      { apiBaseUrl: "/collection-api" }
+    );
+
+    await onSaved(savedStorage);
+  }
+
+  const buttonBar = (
+    <ButtonBar>
+      <BackButton
+        entityId={storageUnit?.id}
+        entityLink="/collection/storage-unit"
+      />
+      <SubmitButton className="ms-auto" />
+    </ButtonBar>
+  );
+
+  return (
+    <DinaForm<Partial<StorageUnit>>
+      initialValues={initialValues}
+      onSubmit={onSubmit}
+    >
+      {buttonBar}
+      <StorageUnitFormFields />
+    </DinaForm>
+  );
+}
+
+/** Re-usable field layout between edit and view pages. */
+export function StorageUnitFormFields() {
+  const { readOnly } = useDinaFormContext();
+
+  return (
+    <div>
+      <div className="row">
+        <GroupSelectField
+          name="group"
+          enableStoredDefaultGroup={true}
+          className="col-sm-6"
+        />
+      </div>
+      <div className="row">
+        <TextField className="col-sm-6" name="name" />
+        <ResourceSelectField<StorageUnit>
+          name="parentStorageUnit"
+          filter={filterBy(["name"])}
+          model="collection-api/storage-unit"
+          className="col-sm-6"
+          optionLabel={unit => unit.name}
+        />
+      </div>
+      {readOnly && (
+        <div className="row">
+          <DateField className="col-sm-6" name="createdOn" />
+          <TextField className="col-sm-6" name="createdBy" />
+        </div>
+      )}
+    </div>
+  );
+}
---
+++ packages/dina-ui/pages/collection/storage-unit/list.tsx
@@ -0,0 +1,66 @@
+import { ButtonBar, CreateButton, ListPageLayout, dateCell } from "common-ui";
+import Link from "next/link";
+import { Footer, GroupSelectField, Head, Nav } from "../../../components";
+import { DinaMessage, useDinaIntl } from "../../../intl/dina-ui-intl";
+
+const STORAGE_UNIT_FILTER_ATTRIBUTES = [
+  "name",
+  "parentStorageUnit.name",
+  "createdBy"
+];
+const STORAGE_UNIT_TABLE_COLUMNS = [
+  {
+    Cell: ({ original: { id, name } }) => (
+      <Link href={`/collection/storage-unit/view?id=${id}`}>{name}</Link>
+    ),
+    accessor: "name"
+  },
+  "parentStorageUnit.name",
+  "group",
+  "createdBy",
+  dateCell("createdOn")
+];
+
+export default function storageUnitListPage() {
+  const { formatMessage } = useDinaIntl();
+
+  return (
+    <div>
+      <Head title={formatMessage("storageUnitListTitle")} />
+      <Nav />
+      <main className="container-fluid">
+        <h1>
+          <DinaMessage id="storageUnitListTitle" />
+        </h1>
+        <ButtonBar>
+          <CreateButton entityLink="/collection/storage-unit" />
+        </ButtonBar>
+        <ListPageLayout
+          additionalFilters={filterForm => ({
+            // Apply group filter:
+            ...(filterForm.group && { rsql: `group==${filterForm.group}` })
+          })}
+          filterAttributes={STORAGE_UNIT_FILTER_ATTRIBUTES}
+          id="storage-unit-list"
+          queryTableProps={{
+            columns: STORAGE_UNIT_TABLE_COLUMNS,
+            path: "collection-api/storage-unit",
+            include: "parentStorageUnit"
+          }}
+          filterFormchildren={({ submitForm }) => (
+            <div className="mb-3">
+              <div style={{ width: "300px" }}>
+                <GroupSelectField
+                  onChange={() => setImmediate(submitForm)}
+                  name="group"
+                  showAnyOption={true}
+                />
+              </div>
+            </div>
+          )}
+        />
+      </main>
+      <Footer />
+    </div>
+  );
+}
