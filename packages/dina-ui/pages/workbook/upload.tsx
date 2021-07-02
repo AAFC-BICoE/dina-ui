155a2849a64a19fcd6f75d4f71b5e63bf9f47e23
Brandon Andre <andr0284@algonquinlive.com>
2021-07-02 14:27:28 -0400

    Support #23091 - Add an experimental workbook sandbox
    https://redmine.biodiversity.agr.gc.ca/issues/23091
    
    - Added a dropzone that only accepts one excel sheet.

--- packages/dina-ui/pages/workbook/upload.tsx
+++ packages/dina-ui/pages/workbook/upload.tsx
@@ -1,21 +1,42 @@
+import { DinaForm } from "common-ui";
 import { Footer, Head, Nav } from "../../components";
 import { DinaMessage, useDinaIntl } from "../../intl/dina-ui-intl";
-// import { FileUploader } from "../../components/object-store";
+import Dropzone from "react-dropzone-uploader";
 
 export default function UploadWorkbookPage() {
   const { formatMessage } = useDinaIntl();
+
+  // specify upload params and url for your files
+  const getUploadParams = ({}) => {
+    return { url: "" };
+  };
+
+  // receives array of files that are done uploading when submit button is clicked
+  const handleSubmit = allFiles => {
+    allFiles.forEach(f => f.remove());
+  };
 
   return (
     <div>
       <Head title={formatMessage("workbookGroupUploadTitle")} />
       <Nav />
-      <main role="main">
-        <div className="container">
-          <h1>
-            <DinaMessage id="workbookGroupUploadTitle" />
-          </h1>
-        </div>
-      </main>
+      <DinaForm initialValues={{ defaultValuesConfig: null }}>
+        <main role="main">
+          <div className="container">
+            <h1>
+              <DinaMessage id="workbookGroupUploadTitle" />
+            </h1>
+
+            <Dropzone
+              getUploadParams={getUploadParams}
+              multiple={false}
+              maxFiles={1}
+              onSubmit={handleSubmit}
+              accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            />
+          </div>
+        </main>
+      </DinaForm>
       <Footer />
     </div>
   );
