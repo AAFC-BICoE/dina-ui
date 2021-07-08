25791143531cce9befb151872daa70f6ffc5ee2f
Mat Poff <matp@live.ca>
2021-07-08 17:24:27 +0000

    23178
    -Got rid of the childStorageUnits inclusion from the query to avoid infinite recursion, replaced it with a separate query.
    -Got the Move All button working.

--- packages/dina-ui/pages/collection/storage-unit/view.tsx
+++ packages/dina-ui/pages/collection/storage-unit/view.tsx
@@ -1,81 +1,67 @@
 import {
+  AreYouSureModal,
   BackButton,
   ButtonBar,
   DeleteButton,
   DinaForm,
   EditButton,
-  useQuery,
-  withResponse,
-  useModal,
-  AreYouSureModal,
-  ResourceSelectField,
   filterBy,
-  useApiClient
+  ResourceSelectField,
+  useApiClient,
+  useModal,
+  useQuery,
+  withResponse
 } from "common-ui";
 import { WithRouterProps } from "next/dist/client/with-router";
 import { withRouter } from "next/router";
 import { Head, Nav } from "../../../components";
-import { useDinaIntl, DinaMessage } from "../../../intl/dina-ui-intl";
+import { DinaMessage, useDinaIntl } from "../../../intl/dina-ui-intl";
 import { StorageUnit } from "../../../types/collection-api";
-import { StorageUnitFormFields } from "./edit";
-import { useState } from "react";
+import { StorageUnitFormFields, useStorageUnit } from "./edit";
 
 export function StorageUnitDetailsPage({ router }: WithRouterProps) {
-  const id = String(router.query.id);
+  const id = router.query.id?.toString();
   const { formatMessage } = useDinaIntl();
-  const [parent, setParent] = useState<StorageUnit>();
 
   const { save } = useApiClient();
 
   const { openModal } = useModal();
 
-  const StorageUnitQuery = useQuery<StorageUnit>(
+  const storageUnitQuery = useStorageUnit(id);
+  const childrenQuery = useQuery<StorageUnit[]>(
     {
-      path: `collection-api/storage-unit/${id}`,
-      include: "storageUnitChildren,parentStorageUnit,storageUnitType"
+      path: `collection-api/storage-unit/${id}/storageUnitChildren`
     },
-    { deps: [parent?.id] }
+    { disabled: !id }
   );
+  const children = childrenQuery.response?.data;
 
-  const storageUnit = StorageUnitQuery.response?.data;
+  const storageUnit = storageUnitQuery.response?.data;
 
   async function moveAllContentToNewContainer(submittedValues) {
     const parentUnit = submittedValues.parentStorageUnit;
-    const children = storageUnit?.storageUnitChildren;
     // Set first level children to new parent
     if (children) {
       await save(
-        children?.map(child => {
-          (child as any).relationships = {};
-          (child as any).relationships.storageUnitChildren = {
-            data: child?.storageUnitChildren?.map(it => ({
-              id: it.id,
-              type: it.type
-            }))
-          };
-          delete child.storageUnitChildren;
-          return {
-            resource: {
-              type: child.type,
-              id: child.id,
-              parentStorageUnit: parentUnit
-            },
-            type: "storage-unit"
-          };
-        }) as any,
+        children.map(child => ({
+          resource: {
+            type: child.type,
+            id: child.id,
+            parentStorageUnit: parentUnit
+          },
+          type: "storage-unit"
+        })),
         { apiBaseUrl: "/collection-api" }
       );
     }
-    setParent(parentUnit);
+    router.push(`/collection/storage-unit/view?id=${id}`);
   }
 
   function onMoveAllContentClick() {
     const exclusionNames: string[] = [];
     function setExclusionContainerNames(container: StorageUnit) {
       exclusionNames.push(container.name);
-      container?.storageUnitChildren?.map(child =>
-        setExclusionContainerNames(child)
-      );
+      children?.map(child => setExclusionContainerNames(child));
     }
 
     setExclusionContainerNames(storageUnit as any);
@@ -108,49 +94,49 @@ export function StorageUnitDetailsPage({ router }: WithRouterProps) {
     );
   }
 
-  const buttonBar = (strgUnit: StorageUnit) => (
-    <ButtonBar>
-      <BackButton
-        entityId={id}
-        entityLink="/collection/storage-unit"
-        byPassView={true}
-      />
-      {!strgUnit.storageUnitChildren?.length && (
-        <EditButton
-          className="ms-auto"
-          entityId={id}
-          entityLink="collection/storage-unit"
-        />
-      )}
-      {!strgUnit.storageUnitChildren?.length && (
-        <DeleteButton
-          className="ms-5"
-          id={id}
-          options={{ apiBaseUrl: "/collection-api" }}
-          postDeleteRedirect="/collection/storage-unit/list"
-          type="storage-unit"
-        />
-      )}
-      {!!strgUnit.storageUnitChildren?.length && (
-        <button
-          className="btn btn-info moveAllContent ms-auto"
-          onClick={onMoveAllContentClick}
-        >
-          <DinaMessage id="moveAllContent" />
-        </button>
-      )}
-    </ButtonBar>
-  );
-
   return (
     <div>
       <Head title={formatMessage("storageUnitViewTitle")} />
       <Nav />
       <main className="container">
-        {withResponse(StorageUnitQuery, ({ data: strgUnit }) => {
+        {withResponse(storageUnitQuery, ({ data: strgUnit }) => {
+          const buttonBar = (
+            <ButtonBar>
+              <BackButton
+                entityId={strgUnit.id}
+                entityLink="/collection/storage-unit"
+                byPassView={true}
+              />
+              {!children?.length && (
+                <EditButton
+                  className="ms-auto"
+                  entityId={strgUnit.id}
+                  entityLink="collection/storage-unit"
+                />
+              )}
+              {!children?.length && (
+                <DeleteButton
+                  className="ms-5"
+                  id={strgUnit.id}
+                  options={{ apiBaseUrl: "/collection-api" }}
+                  postDeleteRedirect="/collection/storage-unit/list"
+                  type="storage-unit"
+                />
+              )}
+              {!!children?.length && (
+                <button
+                  className="btn btn-info moveAllContent ms-auto"
+                  onClick={onMoveAllContentClick}
+                >
+                  <DinaMessage id="moveAllContent" />
+                </button>
+              )}
+            </ButtonBar>
+          );
+
           return (
             <>
-              {buttonBar(strgUnit)}
+              {buttonBar}
               <DinaForm<StorageUnit> initialValues={strgUnit} readOnly={true}>
                 <StorageUnitFormFields />
               </DinaForm>
