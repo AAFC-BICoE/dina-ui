e112656983cb3da8d5e982b564ccbc479da9f18d
Shemy <shemy.gan@gmail.com>
2021-08-19 10:39:07 -0400

    24183 - Add MaterialSampleBreadCrumb and a test

---
+++ packages/dina-ui/components/collection/MaterialSampleBreadCrumb.tsx
@@ -0,0 +1,45 @@
+import Link from "next/link";
+import { MaterialSample } from "../../types/collection-api";
+
+export interface MaterialSampleBreadCrumbProps {
+  disableLastLink?: boolean;
+  materialSample: MaterialSample;
+}
+
+export function MaterialSampleBreadCrumb({
+  disableLastLink,
+  materialSample
+}: MaterialSampleBreadCrumbProps) {
+  const parentPath = [...(materialSample.hierarchy?.slice(1) ?? [])].reverse();
+
+  const displayName =
+    materialSample.materialSampleName ?? materialSample.dwcCatalogNumber;
+
+  return (
+    <ol
+      className="breadcrumb mb-0"
+      style={{ "--bs-breadcrumb-divider": "'>'" } as any}
+    >
+      {parentPath.map(node => (
+        <li className="breadcrumb-item" key={node.uuid}>
+          <Link href={`/collection/material-sample/view?id=${node.uuid}`}>
+            <a target="_blank">{node.name}</a>
+          </Link>
+        </li>
+      ))}
+      <li className="breadcrumb-item">
+        <strong>
+          {!disableLastLink ? (
+            <Link
+              href={`/collection/material-sample/view?id=${materialSample.id}`}
+            >
+              <a target="_blank">{displayName}</a>
+            </Link>
+          ) : (
+            displayName
+          )}
+        </strong>
+      </li>
+    </ol>
+  );
+}
