81869639c8b8fe4a2c37d6590c0979d2af435940
demayk <kirk.demay@agr.gc.ca>
2021-10-20 11:46:34 -0400

    Corrected build error in /object-store/object/view.txs by adding missing import reference and const declaration

--- packages/dina-ui/pages/object-store/object/view.tsx
+++ packages/dina-ui/pages/object-store/object/view.tsx
@@ -20,7 +20,7 @@ import {
   useMetadataQuery
 } from "../../../components/object-store";
 import { MetadataFileView } from "../../../components/object-store/metadata/MetadataFileView";
-import { DinaMessage } from "../../../intl/dina-ui-intl";
+import { DinaMessage, useDinaIntl } from "../../../intl/dina-ui-intl";
 
 const OBJECT_DETAILS_PAGE_CSS = `
   .file-viewer-wrapper img {
@@ -31,6 +31,7 @@ const OBJECT_DETAILS_PAGE_CSS = `
 
 export default function MetadataViewPage() {
   const router = useRouter();
+  const { formatMessage } = useDinaIntl();
 
   const id = String(router.query.id);
 
