3ff50cecc791dda9f8234098afb5faba700d1755
Brandon Andre <andr0284@algonquinlive.com>
2021-10-26 09:31:01 -0400

    Support #25049 - View collection method description on list page
    https://redmine.biodiversity.agr.gc.ca/issues/25049
    
    - Started working on the test for descriptionCell.
    - Currently selected language is now provided from the intlSupport.
    - Logic has been cleaned up for determining how the description should be displayed.

---
+++ packages/common-ui/lib/table/__tests__/descriptionCell.test.tsx
@@ -0,0 +1,75 @@
+import { descriptionCell } from "../description-cell";
+import { mountWithAppContext } from "../../test-util/mock-app-context";
+import { MultilingualDescription } from "packages/dina-ui/types/collection-api/resources/PreparationType";
+import { getIntlSupport } from "../..";
+
+const { useIntl } = getIntlSupport({
+  defaultMessages: {},
+  translations: { en: {}, fr: {} }
+});
+
+describe("descriptionCell", () => {
+  const { setLocale } = useIntl();
+
+  const fieldName = "myDescriptionField";
+  const englishDescription = "English description";
+  const frenchDescription = "French description";
+
+  const descriptionDataBoth: MultilingualDescription = {
+    descriptions: [
+      {
+        lang: "en",
+        desc: englishDescription
+      },
+      {
+        lang: "fr",
+        desc: frenchDescription
+      }
+    ]
+  };
+
+  const descriptionDataEn: MultilingualDescription = {
+    descriptions: [
+      {
+        lang: "en",
+        desc: englishDescription
+      }
+    ]
+  };
+
+  it("Both description languages provided, english as selected language.", () => {
+    setLocale("en");
+
+    const cell = descriptionCell(fieldName);
+
+    const wrapper = mountWithAppContext(
+      <cell.Cell original={{ myDescriptionField: descriptionDataBoth }} />
+    );
+
+    expect(cell.accessor).toEqual(fieldName);
+    expect(wrapper).toEqual(englishDescription);
+  });
+
+  it("Both description languages provided, french as the selected language.", () => {
+    setLocale("fr");
+
+    const cell = descriptionCell(fieldName);
+
+    const wrapper = mountWithAppContext(
+      <cell.Cell original={{ myDescriptionField: descriptionDataBoth }} />
+    );
+
+    expect(cell.accessor).toEqual(fieldName);
+    expect(wrapper).toEqual(frenchDescription);
+  });
+
+  it("English description provided, language selected is french.", () => {
+    setLocale("fr");
+
+    const cell = descriptionCell(fieldName);
+
+    const wrapper = mountWithAppContext(
+      <cell.Cell original={{ myDescriptionField: descriptionDataEn }} />
+    );
+  });
+});
--- packages/common-ui/lib/table/description-cell.tsx
+++ packages/common-ui/lib/table/description-cell.tsx
@@ -3,6 +3,8 @@ import {
   MultilingualDescription,
   MultilingualPair
 } from "../../../dina-ui/types/collection-api/resources/PreparationType";
+import { useContext } from "react";
+import { intlContext } from "../intl/IntlSupport";
 
 /**
  * Used for multilingual descriptions which contain an English and French version of the
@@ -11,44 +13,37 @@ import {
 export function descriptionCell(accessor: string) {
   return {
     Cell: ({ original }) => {
+      const { locale } = useContext(intlContext);
+
       // Retrieve the current language being used.
-      const language = "fr"; // TODO: retrieve the current language.
+      const language = locale;
 
       // Get the descriptions provided.
       const multilingualDescription: MultilingualDescription = get(
         original,
         accessor
       );
-      const descriptionPairs: MultilingualPair[] | null | undefined =
-        multilingualDescription.descriptions;
 
       // If no descriptions are provided, just leave the cell blank.
-      if (descriptionPairs == null || descriptionPairs.length === 0) {
-        return null;
+      if (
+        multilingualDescription.descriptions == null ||
+        multilingualDescription.descriptions.length === 0
+      ) {
+        return <div />;
       }
 
-      // Multiple languages provided.
-      if (descriptionPairs.length > 1) {
-        // Loop through all of the descriptions provided.
-        descriptionPairs.forEach((description: MultilingualPair) => {
-          // If current language is detected, use that one.
-          if (description.lang === language) {
-            return description.desc;
-          }
-        });
+      const descriptionPairs: MultilingualPair[] =
+        multilingualDescription.descriptions;
 
-        // If the current language could not be found, just use one of them.
-        return descriptionPairs.at(0)?.desc;
-      } else {
-        const description = descriptionPairs.at(0);
-        // Check if the only description is the currently used language.
-        if (description?.lang === language) {
-          // Display the description without indicating the language.
-          return description.desc;
-        } else {
-          return description?.lang;
+      // Loop through all of the descriptions provided, the preferred one is always the currently used language.
+      for (const description of descriptionPairs) {
+        if (description.lang === language) {
+          return <div>description.desc</div>;
         }
       }
+
+      // Preferred language could not be found above. Use another language and make sure it's indicated.
+      return <div>{descriptionPairs[0] ? descriptionPairs[0].desc : ""}</div>;
     },
     accessor
   };
