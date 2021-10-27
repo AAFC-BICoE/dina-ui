83850dc40ed5edb7f93378e62cbcd715346d3bf5
Shemy <shemy.gan@gmail.com>
2021-10-27 10:39:48 -0400

    24956 - Add missing file for commit

--- packages/dina-ui/types/collection-api/resources/MaterialSample.ts
+++ packages/dina-ui/types/collection-api/resources/MaterialSample.ts
@@ -46,6 +46,8 @@ export interface MaterialSampleAttributes {
 
   hostOrganism?: HostOrganism;
   associations?: MaterialSampleAssociation[];
+
+  association?: MaterialSampleAssociation;
 }
 
 export interface HostOrganism {
