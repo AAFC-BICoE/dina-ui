7afbe37b2858a23d83155e8efcd5e1fe363503c1
Shemy <shemy.gan@gmail.com>
2021-10-29 11:30:52 -0400

    Added more postgis extension in initdb.sql to match local deployment

--- dina-compose/initdb.sql
+++ dina-compose/initdb.sql
@@ -5,7 +5,11 @@ CREATE SCHEMA agent;
 CREATE SCHEMA userapi;
 CREATE SCHEMA collection;
 
-CREATE EXTENSION if not exists postgis SCHEMA collection;
+CREATE EXTENSION IF NOT EXISTS postgis CASCADE;
+CREATE EXTENSION IF NOT EXISTS postgis_topology CASCADE;
+CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder CASCADE;
+UPDATE pg_extension SET extrelocatable = TRUE WHERE extname = 'postgis';
+ALTER EXTENSION postgis SET SCHEMA collection;
 
 set schema 'seqdb';
 create extension if not exists "pgcrypto";
