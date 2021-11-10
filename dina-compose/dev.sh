e8ca15ebc3280562784444fdfbc0b0348acb5e6b
Mathew Poff <matp@live.ca>
2021-11-10 18:18:49 -0500

    Added comment

--- dina-compose/dev.sh
+++ dina-compose/dev.sh
@@ -3,5 +3,7 @@
 export UID=$(id -u)
 export GID=$(id -g)
 
+# Avoids the docker-compose error for too many connections:
 export COMPOSE_PARALLEL_LIMIT=1000
+
 docker-compose -p dina-dev -f ./docker-compose.base.yml -f ./docker-compose.dev.yml $@
