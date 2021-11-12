8b1ae63658e5553a0a1183f2337b194a01c24bd0
Mathew Poff <matp@live.ca>
2021-11-12 15:17:58 -0500

    Added docker compose's connection fix to the prod.sh script.
    Dev and prod configs work locally for me now.

--- dina-compose/prod.sh
+++ dina-compose/prod.sh
@@ -1,3 +1,6 @@
 # Short-hand way of running docker-compose with the prod launch config.
 
+# Avoids the docker-compose error for too many connections:
+export COMPOSE_PARALLEL_LIMIT=1000
+
 docker-compose -p dina-object-store-prod -f ./docker-compose.base.yml -f ./docker-compose.prod.yml $@
