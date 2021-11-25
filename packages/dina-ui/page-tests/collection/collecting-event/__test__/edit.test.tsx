12e107187d42937f9fa3b7399b04eaf46c7432d8
Mathew Poff <matp@live.ca>
2021-11-25 12:32:13 -0500

    Feedback 25681
    Made the coord systemj null for new collecting events with no coordinates set.

--- packages/dina-ui/page-tests/collection/collecting-event/__test__/edit.test.tsx
+++ packages/dina-ui/page-tests/collection/collecting-event/__test__/edit.test.tsx
@@ -146,7 +146,7 @@ describe("collecting-event edit page", () => {
           path: "collecting-event",
           value: {
             attributes: {
-              dwcVerbatimCoordinateSystem: "decimal degrees",
+              dwcVerbatimCoordinateSystem: null,
               dwcVerbatimSRS: "WGS84 (EPSG:4326)",
               managedAttributes: {},
               publiclyReleasable: true, // Default value
@@ -408,6 +408,67 @@ describe("collecting-event edit page", () => {
     expect(assertionTabs.length).toEqual(2);
     expect(assertionTabs.at(0).text()).toEqual("1");
     expect(assertionTabs.at(1).text()).toEqual("2 (Primary)");
+  });
+
+  it("Removes the coordinate system if there are no coordinates set.", async () => {
+    mockPatch.mockReturnValueOnce({
+      data: [
+        {
+          data: {
+            attributes: {
+              startEventDateTime: "12/21/2019T16:00",
+              endEventDateTime: "12/22/2019T16:00",
+              verbatimEventDateTime: "From 2019,12,21 4pm to 2019,12,22 4pm"
+            },
+            id: "1",
+            type: "collecting-event"
+          },
+          status: 201
+        }
+      ] as OperationsResponse
+    });
+
+    mockQuery = {};
+
+    const wrapper = mountWithAppContext(<CollectingEventEditPage />, {
+      apiContext
+    });
+
+    await new Promise(setImmediate);
+    wrapper.update();
+
+    // Default value:
+    expect(
+      wrapper.find(".dwcVerbatimCoordinateSystem-field input").prop("value")
+    ).toEqual("decimal degrees");
+
+    wrapper.find("form").simulate("submit");
+
+    await new Promise(setImmediate);
+    wrapper.update();
+
+    expect(mockPatch).lastCalledWith(
+      "/collection-api/operations",
+      [
+        {
+          op: "POST",
+          path: "collecting-event",
+          value: {
+            attributes: expect.objectContaining({
+              dwcVerbatimCoordinateSystem: null
+            }),
+            relationships: {
+              attachment: {
+                data: []
+              }
+            },
+            id: "00000000-0000-0000-0000-000000000000",
+            type: "collecting-event"
+          }
+        }
+      ],
+      expect.anything()
+    );
   });
 });
 
