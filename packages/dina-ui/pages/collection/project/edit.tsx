931ee05f8002fa981cc0636aeaa2cc9efcb4ebd6
Shemy(Xuemei) Gan <xuemei.gan@agr.gc.ca>
2021-11-25 16:05:56 -0500

    25690 - Fix the project status field to autocomplete

--- packages/dina-ui/pages/collection/project/edit.tsx
+++ packages/dina-ui/pages/collection/project/edit.tsx
@@ -170,10 +170,18 @@ export function ProjectFormLayout() {
         />
       </div>
       <div className="row">
-        <AutoSuggestTextField
+        <AutoSuggestTextField<Project>
           className="col-md-6 status"
           name="status"
           label={formatMessage("field_projectStatus")}
+          query={(searchValue, ctx) => ({
+            path: "collection-api/project",
+            filter: {
+              ...(ctx.values.group && { group: { EQ: ctx.values.group } }),
+              rsql: `status==${searchValue}*`
+            }
+          })}
+          suggestion={project => project.status ?? ""}          
         />
         <DateField
           className="col-md-6 endDate"
