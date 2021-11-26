6a95a4aaff9894fb148a44dec21b416fab472441
Shemy(Xuemei) Gan <xuemei.gan@agr.gc.ca>
2021-11-26 15:37:09 -0500

    25528 - Fix test to use record instead of string

--- packages/dina-ui/page-tests/object-store/metadata/__tests__/single-record-edit.test.tsx
+++ packages/dina-ui/page-tests/object-store/metadata/__tests__/single-record-edit.test.tsx
@@ -139,7 +139,7 @@ describe("Metadata single record edit page.", () => {
     ]);
     expect(
       wrapper.find(".managed-attributes-editor input").last().prop("value")
-    ).toEqual({"a360a695-bbff-4d58-9a07-b6d6c134b208": "test-managed-attribute-value"});
+    ).toEqual({"a360a695-bbff-4d58-9a07-b6d6c134b208": "test-managed-attribute-value"});    
 
     // Set new values:
     wrapper.find(".acTags-field").find(CreatableSelect).prop<any>("onChange")([
@@ -151,10 +151,13 @@ describe("Metadata single record edit page.", () => {
       .last()
       .simulate("change", {
         target: {
-          name: "managedAttributeMap.values.39558b3c-02e9-476e-a9c8-7946f8bbff63.value",
-          value: "new-managed-attribute-value"
+          name: "managedAttributeValues", 
+          value: {"a360a695-bbff-4d58-9a07-b6d6c134b208" :"new-managed-attribute-value"}
         }
       });
+
+    await new Promise(setImmediate);
+      wrapper.update();      
 
     wrapper.find("form").simulate("submit");
 
@@ -178,7 +181,7 @@ describe("Metadata single record edit page.", () => {
             fileExtension: ".png",
             fileIdentifier: "9a85b858-f8f0-4a97-99a8-07b2cb759766",
             id: "25f81de5-bbee-430c-b5fa-71986b70e612",
-            managedAttributeValues: "new-managed-attribute-value",
+            managedAttributeValues: {"a360a695-bbff-4d58-9a07-b6d6c134b208": "new-managed-attribute-value"},
             originalFilename: "test-file.png",
             type: "metadata",
             xmpRightsUsageTerms: "",
