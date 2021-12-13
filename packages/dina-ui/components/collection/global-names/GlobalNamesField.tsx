cda6fee8347c7a139ab5418eb9f6a984dd046c0d
Shemy(Xuemei) Gan <xuemei.gan@agr.gc.ca>
2021-12-13 13:49:00 -0500

    25936 - Add data definition for global names search result type - Add GlobalNamesSearchBox to query global names endpoint - Add GlobalNamesField to wrap the GlobalNamesSearchBox

---
+++ packages/dina-ui/components/collection/global-names/GlobalNamesField.tsx
@@ -0,0 +1,162 @@
+import { FieldWrapper, FieldWrapperProps } from "common-ui";
+import { FormikProps } from "formik";
+import { DinaMessage } from "../../../intl/dina-ui-intl";
+import { useState } from "react";
+import { isArray } from "lodash";
+import DOMPurify from "dompurify";
+import { GlobalNamesSearchBox } from "../global-names/GlobalNamesSearchBox";
+export interface GlobalNamesFieldProps extends FieldWrapperProps {
+  scientificNameSourceField?: string;
+  onChange?: (selection: string | null, formik: FormikProps<any>) => void;
+  /** Overridable for mocking in tests. */
+  fetchJson?: (url: string) => Promise<any>;
+  index?: number;
+  isDetermination?: boolean;
+  scientificNameDetailsSrcUrlField?: string;
+  scientificNameDetailsLabelHtmlField?: string;
+
+  /** Mock this out in tests so it gives a predictable value. */
+  dateSupplier?: () => string;
+}
+
+export function GlobalNamesField({
+  onChange,
+  scientificNameSourceField,
+  fetchJson,
+  index,
+  isDetermination,
+  scientificNameDetailsSrcUrlField,
+  scientificNameDetailsLabelHtmlField,
+  dateSupplier,
+  ...fieldWrapperProps
+}: GlobalNamesFieldProps) {
+  const [searchInitiated, setSearchInitiated] = useState(false);
+
+  const getFieldValue = (form, fieldName) => {
+    return form?.getFieldValue
+      ? form?.getFieldValue(fieldName as any)
+      : form?.getFieldMeta
+      ? form?.getFieldMeta(fieldName as any).value
+      : null;
+  };
+
+  const RenderAsReadonly = ({ value, form }) => {
+    const scientificNameDetailSrcUrl = getFieldValue(
+      form,
+      scientificNameDetailsSrcUrlField
+    );
+    const scientificNameDetailLabelHtml = getFieldValue(
+      form,
+      scientificNameDetailsLabelHtmlField
+    );
+    const scientificNameVal = getFieldValue(form, scientificNameSourceField);
+
+    const link = document.createElement("a");
+    link.setAttribute("href", scientificNameDetailSrcUrl);
+    link.setAttribute("target", "_blank");
+    link.setAttribute("rel", "noopener");
+
+    link.innerHTML = scientificNameDetailLabelHtml;
+
+    const safeHtmlLink: string = DOMPurify.sanitize(link.outerHTML, {
+      ADD_ATTR: ["target", "rel"]
+    });
+
+    const isFromSrcDetailsUrl = scientificNameDetailSrcUrl?.length > 0;
+
+    return (
+      <GlobalNamesReadOnly
+        value={isFromSrcDetailsUrl ? safeHtmlLink : value}
+        scientificNameSource={scientificNameSourceField && scientificNameVal}
+        isFromSrcDetailsUrl={isFromSrcDetailsUrl}
+      />
+    );
+  };
+  return (
+    <FieldWrapper
+      {...fieldWrapperProps}
+      disableLabelClick={true}
+      readOnlyRender={(value, form) => (
+        <div className="card card-body">
+          <RenderAsReadonly value={value} form={form} />
+        </div>
+      )}
+    >
+      {({ formik, setValue, value }) => {
+        const scientificNameSrceDetailUrlVal = formik.getFieldMeta(
+          scientificNameDetailsSrcUrlField as any
+        ).value as string;
+        return value &&
+          (searchInitiated || scientificNameSrceDetailUrlVal?.length > 0) ? (
+          // When the field has a value of previous or current search result
+          <div
+            className="d-flex flex-row"
+            style={{ border: "1px solid #F5F5F5" }}
+          >
+            <div className="col-md-6 mt-2 ">
+              <RenderAsReadonly value={value} form={formik} />
+            </div>
+            <div className="col-md-4 d-flex align-items-center">
+              <button
+                type="button"
+                className="btn btn-danger remove-button"
+                onClick={() => {
+                  onChange?.(null, formik);
+                  setValue(null);
+                  setSearchInitiated(false);
+                }}
+              >
+                <DinaMessage id="remove" />
+              </button>
+            </div>
+          </div>
+        ) : (
+          <GlobalNamesSearchBox
+            fetchJson={fetchJson}
+            onSelect={newValue => {
+              const val = isArray(newValue) ? newValue?.[1] : newValue;
+              onChange?.(newValue as any, formik);
+              setValue(val);
+              setSearchInitiated(true);
+            }}
+            index={index}
+            setValue={setValue}
+            initSearchValue={value ?? ""}
+            formik={formik}
+            onChange={onChange}
+            isDetermination={isDetermination}
+            dateSupplier={dateSupplier}
+          />
+        );
+      }}
+    </FieldWrapper>
+  );
+}
+
+function GlobalNamesReadOnly({
+  value,
+  scientificNameSource,
+  isFromSrcDetailsUrl
+}) {
+  const sanitizedHtml = DOMPurify.sanitize(value, {
+    ADD_ATTR: ["target", "rel"]
+  });
+
+  return (
+    <div style={{ whiteSpace: "pre-wrap" }}>
+      {isFromSrcDetailsUrl ? (
+        <p dangerouslySetInnerHTML={{ __html: sanitizedHtml }} />
+      ) : (
+        <p> {value} </p>
+      )}
+      {scientificNameSource && (
+        <p>
+          <strong>
+            <DinaMessage id="source" />:{" "}
+          </strong>
+          {scientificNameSource}
+        </p>
+      )}
+    </div>
+  );
+}
---
+++ packages/dina-ui/components/collection/global-names/GlobalNamesSearchBox.tsx
@@ -0,0 +1,280 @@
+import {
+  FormikButton,
+  LoadingSpinner,
+  Tooltip,
+  useThrottledFetch
+} from "common-ui";
+import DOMPurify from "dompurify";
+import { Field, FormikProps } from "formik";
+import moment from "moment";
+import { ScientificNameSourceDetails } from "../../../../dina-ui/types/collection-api/resources/Determination";
+import { DinaMessage, useDinaIntl } from "../../../intl/dina-ui-intl";
+import { GlobalNamesSearchResult } from "./global-names-search-result-type";
+
+export interface GlobalNamesSearchBoxProps {
+  /** Optionally mock out the HTTP fetch for testing. */
+  fetchJson?: (url: string) => Promise<any>;
+
+  onSelect?: (
+    selection: (string | ScientificNameSourceDetails | undefined)[]
+  ) => void;
+
+  /** The determination index within the material sample. */
+  index?: number;
+
+  setValue?: (newValue: any) => void;
+
+  /** user entered initial search value. */
+  initSearchValue?: string;
+
+  onChange?: (selection: string | null, formik: FormikProps<any>) => void;
+
+  formik?: FormikProps<any>;
+
+  isDetermination?: boolean;
+
+  /** Mock this out in tests so it gives a predictable value. */
+  dateSupplier?: () => string;
+}
+
+export function GlobalNamesSearchBox({
+  fetchJson,
+  onSelect,
+  index,
+  setValue,
+  initSearchValue,
+  onChange,
+  formik,
+  isDetermination,
+  dateSupplier = () => moment().format("YYYY-MM-DD") // Today
+}: GlobalNamesSearchBoxProps) {
+  const { formatMessage } = useDinaIntl();
+
+  const {
+    searchIsLoading,
+    searchResult,
+    inputValue,
+    setInputValue,
+    searchIsDisabled,
+    doThrottledSearch
+  } = useThrottledFetch({
+    fetcher: searchValue =>
+      globalNamesQuery<GlobalNamesSearchResult[]>({
+        url: `https://verifier.globalnames.org/api/v1/verifications/${
+          searchValue[0].toUpperCase() + searchValue.substring(1)
+        }`,
+        params: {
+          capitalize: "false"
+        },
+        searchValue,
+        fetchJson
+      }),
+    timeoutMs: 1000,
+    initSearchValue
+  });
+  const nameResults = searchResult;
+
+  const onInputChange = value => {
+    setInputValue(value);
+    // Will save the user entry if it is not the determination scientific name
+    // use case is for association host organism
+    if (!isDetermination) {
+      setValue?.(value);
+      onChange?.(value, formik as any);
+    }
+  };
+
+  return (
+    <div className="card card-body border">
+      {/* Hide this for now for the demo */}
+      {/* <div className="d-flex align-items-center mb-3">
+        <label className="pt-2 d-flex align-items-center">
+          <strong>
+            <DinaMessage id="dataset" />
+          </strong>
+          <Tooltip id="datasetSearchTooltip" />
+        </label>
+        <div className="flex-grow-1">
+          <ColDataSetDropdown
+            onChange={setDataSet}
+            value={dataSet}
+            fetchJson={fetchJson}
+          />
+        </div>
+      </div> */}
+      <div className="d-flex align-items-center mb-3">
+        {isDetermination && (
+          <label className="pt-2 d-flex align-items-center">
+            <strong>
+              <DinaMessage id="colSearchLabel" />
+            </strong>
+            <Tooltip id="colSearchBoxTooltip" />
+          </label>
+        )}
+        <div className="flex-grow-1">
+          {isDetermination ? (
+            <div className="input-group">
+              <input
+                aria-label={formatMessage("colSearchLabel")}
+                className="form-control col-search-input"
+                onChange={e => onInputChange(e.target.value)}
+                onFocus={e => e.target.select()}
+                onKeyDown={e => {
+                  if (e.keyCode === 13) {
+                    e.preventDefault();
+                    doThrottledSearch(inputValue);
+                  }
+                }}
+                value={inputValue}
+              />
+              <button
+                style={{ width: "10rem" }}
+                onClick={doThrottledSearch}
+                className="btn btn-primary ms-auto col-search-button"
+                type="button"
+                disabled={searchIsDisabled}
+              >
+                <DinaMessage id="searchButton" />
+              </button>
+            </div>
+          ) : (
+            <div>
+              <input
+                aria-label={formatMessage("colSearchLabel")}
+                className="form-control col-search-input"
+                onChange={e => onInputChange(e.target.value)}
+                onFocus={e => e.target.select()}
+                onKeyDown={e => {
+                  if (e.keyCode === 13) {
+                    e.preventDefault();
+                    doThrottledSearch(inputValue);
+                  }
+                }}
+                value={inputValue}
+              />
+              <button
+                onClick={doThrottledSearch}
+                className="btn btn-primary ms-auto mt-2 col-search-button"
+                type="button"
+                disabled={searchIsDisabled}
+              >
+                <DinaMessage id="searchOnCOL" />
+              </button>
+            </div>
+          )}
+        </div>
+      </div>
+      {isDetermination && (
+        <Field>
+          {({ form: { values: formState } }) => {
+            const materialSample = formState;
+            const verbatimScientificName =
+              materialSample.determination?.[index ?? 0]
+                ?.verbatimScientificName;
+            const hasVerbatimScientificName = !!verbatimScientificName;
+            return (
+              hasVerbatimScientificName && (
+                <div className="d-flex align-items-center mb-3">
+                  <div className="pe-3">
+                    <DinaMessage id="search" />:
+                  </div>
+                  <FormikButton
+                    className="btn btn-link"
+                    onClick={() => doThrottledSearch(verbatimScientificName)}
+                  >
+                    <DinaMessage id="field_verbatimScientificName" />
+                  </FormikButton>
+                </div>
+              )
+            );
+          }}
+        </Field>
+      )}
+      {searchIsLoading && <LoadingSpinner loading={true} />}
+      {!!nameResults && (
+        <div className="list-group">
+          {nameResults.map((result, idx) => {
+            const link = document.createElement("a");
+            link.setAttribute("href", result.bestResult?.outlink);
+            link.setAttribute("target", "_blank");
+            link.setAttribute("rel", "noopener");
+
+            link.innerHTML =
+              result.bestResult?.matchedCanonicalFull ??
+              result.bestResult?.matchedCanonicalSimple;
+
+            // Use DOMPurify to sanitize against XSS when using dangerouslySetInnerHTML:
+            const safeHtmlLink: string = DOMPurify.sanitize(link.outerHTML, {
+              ADD_ATTR: ["target", "rel"]
+            });
+
+            const detail: ScientificNameSourceDetails = {};
+            detail.labelHtml = link.innerHTML ?? "";
+            detail.sourceUrl = link.href;
+            detail.recordedOn = dateSupplier();
+
+            // Use detail to populate source details fields, result.label to populate the searchbox bound field
+            const resultArray = [detail, link.innerHTML];
+
+            return (
+              <div
+                key={result.inputId ?? idx}
+                className="list-group-item list-group-item-action d-flex"
+              >
+                <div className="flex-grow-1 d-flex align-items-center col-search-result-label">
+                  <span dangerouslySetInnerHTML={{ __html: safeHtmlLink }} />
+                </div>
+                <FormikButton
+                  className="btn btn-primary col-name-select-button"
+                  buttonProps={() => ({ style: { width: "8rem" } })}
+                  onClick={() => onSelect?.(resultArray)}
+                >
+                  <DinaMessage id="select" />
+                </FormikButton>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      {!searchResult?.length && (
+        <p>
+          <DinaMessage id="noResultsFound" />
+        </p>
+      )}
+    </div>
+  );
+}
+export interface GlobalNamesSearchParams {
+  url: string;
+  params?: Record<string, any>;
+  searchValue: string;
+  fetchJson?: (url: string) => Promise<any>;
+}
+
+export async function globalNamesQuery<T>({
+  url,
+  params,
+  searchValue,
+  fetchJson = urlArg => window.fetch(urlArg).then(res => res.json())
+}: GlobalNamesSearchParams): Promise<T | null> {
+  if (!searchValue?.trim()) {
+    return null;
+  }
+
+  const urlObject = new URL(url);
+  urlObject.search = new URLSearchParams(params).toString();
+
+  try {
+    const response = await fetchJson(urlObject.toString());
+
+    if (response.error) {
+      throw new Error(String(response.error));
+    }
+
+    // Search API returns an array ; Reverse API returns a single place:
+    return response as T;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
