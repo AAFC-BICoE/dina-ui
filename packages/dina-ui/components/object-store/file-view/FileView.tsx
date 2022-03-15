11db3d4fca0307c1812b9d532c11c801edd9df63
John Phan <johnphan010@gmail.com>
2022-03-15 10:50:49 -0700

    open link in new tab, but still downloading

--- packages/dina-ui/components/object-store/file-view/FileView.tsx
+++ packages/dina-ui/components/object-store/file-view/FileView.tsx
@@ -66,6 +66,7 @@ export function FileView({
       {showFile ? (
         <a
           href={authenticatedFilePath}
+          target="_blank"
           style={{
             color: "inherit",
             textDecoration: "none",
