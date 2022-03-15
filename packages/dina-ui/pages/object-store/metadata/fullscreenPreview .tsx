9db8ada9cb7c2ea1bc2df3ebabb6c8915f05357e
John Phan <johnphan010@gmail.com>
2022-03-15 11:20:18 -0700

    new page to view image

---
+++ packages/dina-ui/pages/object-store/metadata/fullscreenPreview .tsx
@@ -0,0 +1,14 @@
+import { OBJECT_STORE_MODULE_REVISION_ROW_CONFIG } from "../../../components/revisions/revision-modules";
+import { RevisionsPage } from "../../../components/revisions/RevisionsPageLayout";
+
+export default () => (
+  // TODO: change to preview image in new tab
+  <RevisionsPage
+    auditSnapshotPath="objectstore-api/audit-snapshot"
+    detailsPageLink={`/object-store/object`}
+    queryPath="objectstore-api/metadata"
+    resourceType="metadata"
+    revisionRowConfigsByType={OBJECT_STORE_MODULE_REVISION_ROW_CONFIG}
+    nameField="originalFilename"
+  />
+);
