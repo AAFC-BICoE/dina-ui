5d35aec11271274dbd6538bbc7b1a35a97f21809
John Phan <johnphan010@gmail.com>
2022-05-09 09:05:35 -0700

    download PDF returned from reports-labels-api, name of file is materialSampleName

--- packages/dina-ui/components/collection/material-sample/GenerateLabelSection.tsx
+++ packages/dina-ui/components/collection/material-sample/GenerateLabelSection.tsx
@@ -78,7 +78,12 @@ export function GenerateLabelSection({
         { responseType: "blob" }
       )
       .then((response) => {
-        window.open(URL.createObjectURL(response.data));
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', `${materialSample?.materialSampleName}.pdf`); //or any other extension
+        document.body.appendChild(link);
+        link.click();
       });
   }
 
