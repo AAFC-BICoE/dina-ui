b81a13eddf141c16a6508bec7167219f0e4e2d23
Brandon Andre <andr0284@algonquinlive.com>
2022-05-12 11:51:22 -0400

    Support #27489 - Support value suggestion (#720)
    * Support #27489 - Support value suggestion
    
    - Early work on the custom useElasticSearchDistinctTerm hook. This will be used with the AutoSuggestTextField component.
    - Started to look in the elastic search index for the distinct_term_agg field. More testing needed.
    
    * Support #27489 - Support value suggestion
    
    - Fixed a bug where options with the same name (like "name" for collection and preparedType) were selecting all the same options.
    - Now able to find the distinct_term_agg option to see if it should display the auto complete.
    - Setup body builder to generate the query when it's first loaded. Doesn't work just yet.
    
    * Support #27489 - Support value suggestion
    
    - Index name and field name is now passed to the useElasticSearchDistinctTerm component.
    - Working on finding the aggregation and query. Currently the buckets are not being given.
    - Also working on fixing an issue with the autoComplete box only appearing after deselecting the correct search filter type.
    
    * Support #27489 - Support value suggestion
    
    - Fixed issue with type visibility not updating properly. It's now not a state and just determines it on the component load.
    - RelationshipType is now passed to the useElasticSearchDistinctTerm.
    - The relationship aggrgation will now use a nested aggreation. It's now working properly.
    - Added support for attribute aggreation as well.
    
    * Support #27489 - Support value suggestion
    
    - Added a TOTAL_SUGGESTIONS constant to define the total number of suggestions returned.
    - Group array is now passed to the useElasticSearchDistinctTerm
    - Filtering by group is now supported and will add the filter to the aggregation.
    
    * Support #27489 - Support value suggestion
    
    - Fixed typescript issue in QueryRow.test.tsx
    - The auto complete type will now automatically use exact instead of giving the option.
    - Simplified the group logic. I can always just use terms.
    - Started working on adding the new types and tests for the auto complete type in QueryRow.test.tsx.
    
    * Support #27489 - Support value suggestion
    
    - Created the useElasticSearchDistinctTerm.test.tsx, to test what the query sent to elastic search and results returned.
    - Fixed the additional matchType for test.
    - Handled elastic search errors better.
    - Fixed issue with attribute based suggestions.
    
    * Support #27489 - Support value suggestion
    
    - Fixed issue when no filter options are found.
    - Found an issue with relationship queries not being generated correctly, added a test to catch this in the future. This test will insure relationships are being properly made, will also test the aggregation more as well.
    - Fixed the issue with the unique values not performing the right search.
    
    * Support #27489 - Support value suggestion
    
    - Fixed bugs with useElasticSearchDistinctTerms test. Now it's properly checking the hook values.
    - Been working on a way to improve the amount of calls
    
    * Support #27489 - Support value suggestion
    
    - Decided to just remove the test and add it back in a later ticket. I'm having too much trouble getting this work. QueryPage already has some test coverage.
    
    * Support #27489 - Support value suggestion
    
    - Fixed test, group is now always an array.
    
    * Support #27489 - Support value suggestion
    
    - Removed hidden input, does not work with formik.
    - Now just using the column definition in order to see if it should be a .keyword.
    
    * Support #27489 - Support value suggestion
    
    - The test should have had .keyword to begin with. Add it back.

---
+++ packages/common-ui/lib/query-builder/useElasticSearchDistinctTerm.tsx
@@ -0,0 +1,111 @@
+import Bodybuilder from "bodybuilder";
+import { castArray } from "lodash";
+import { useEffect, useState } from "react";
+import { useApiClient } from "..";
+
+const TOTAL_SUGGESTIONS: number = 100;
+const AGGREGATION_NAME: string = "term_aggregation";
+const NEST_AGGREGATION_NAME: string = "included_aggregation";
+
+interface QuerySuggestionFieldProps {
+  /** The string you want elastic search to use. */
+  fieldName: string;
+
+  /** If the field is a relationship, we need to know the type to filter it. */
+  relationshipType?: string;
+
+  /** An array of the groups to filter the distinct terms by. This can be an empty group which will skip filtering by group. */
+  groups: string[];
+
+  /** The index you want elastic search to perform the search on */
+  indexName: string;
+}
+
+export function useElasticSearchDistinctTerm({
+  fieldName,
+  relationshipType,
+  groups,
+  indexName
+}: QuerySuggestionFieldProps) {
+  const { apiClient } = useApiClient();
+
+  const [suggestions, setSuggestions] = useState<string[]>([]);
+
+  // Every time the textEntered has changed, perform a new request for new suggestions.
+  useEffect(() => {
+    queryElasticSearchForSuggestions();
+  }, [fieldName, relationshipType, groups]);
+
+  async function queryElasticSearchForSuggestions() {
+    // Use bodybuilder to generate the query to send to elastic search.
+    const builder = Bodybuilder();
+    builder.size(0);
+
+    // Group needs to be queried to only show the users most used values.
+    if (groups && groups.length !== 0) {
+      // terms is used to be able to support multiple groups.
+      builder.query("terms", "data.attributes.group", castArray(groups));
+    }
+
+    // If the field has a relationship type, we need to do a nested query to filter it.
+    if (relationshipType) {
+      builder
+        .query("nested", { path: "included" }, queryBuilder => {
+          return queryBuilder.query("match", "included.type", relationshipType);
+        })
+        .aggregation(
+          "nested",
+          { path: "included" },
+          NEST_AGGREGATION_NAME,
+          agg =>
+            agg.aggregation(
+              "terms",
+              fieldName + ".keyword",
+              {
+                size: TOTAL_SUGGESTIONS
+              },
+              AGGREGATION_NAME
+            )
+        );
+    } else {
+      // If it's an attribute, no need to use nested filters.
+      builder.aggregation(
+        "terms",
+        fieldName + ".keyword",
+        {
+          size: TOTAL_SUGGESTIONS
+        },
+        AGGREGATION_NAME
+      );
+    }
+
+    await apiClient.axios
+      .post(`search-api/search-ws/search`, builder.build(), {
+        params: {
+          indexName
+        }
+      })
+      .then(resp => {
+        // The path to the results in the response changes if it contains the nested aggregation.
+        if (relationshipType) {
+          setSuggestions(
+            resp?.data?.aggregations?.[NEST_AGGREGATION_NAME]?.[
+              AGGREGATION_NAME
+            ]?.buckets?.map(bucket => bucket.key)
+          );
+        } else {
+          setSuggestions(
+            resp?.data?.aggregations?.[AGGREGATION_NAME]?.buckets?.map(
+              bucket => bucket.key
+            )
+          );
+        }
+      })
+      .catch(() => {
+        // If any issues have occurred, just return an empty list.
+        setSuggestions([]);
+      });
+  }
+
+  return suggestions;
+}
