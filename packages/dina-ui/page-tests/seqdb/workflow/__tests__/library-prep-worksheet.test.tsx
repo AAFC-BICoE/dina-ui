297d801cdf975c6d11f0fac71d02befac8097695
Mike Sun <mikesun197@gmail.com>
2022-06-20 14:35:20 -0400

    27903 Update sequence module protocol component
    testing changes

--- packages/dina-ui/page-tests/seqdb/workflow/__tests__/library-prep-worksheet.test.tsx
+++ packages/dina-ui/page-tests/seqdb/workflow/__tests__/library-prep-worksheet.test.tsx
@@ -83,7 +83,7 @@ describe("Library Prep Worksheet page", () => {
             {
               id: "3",
               indexI5: { id: "3", type: "ngs-index", name: "index 3" },
-              indexI7: { id: "6", type: "ngs-index", name: "index 6" },
+              indexI7: { id: "6", type: "ngs-index" },
               molecularSample: {
                 id: "1",
                 type: "molecular-sample",
