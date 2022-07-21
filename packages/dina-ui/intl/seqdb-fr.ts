1b7d4958ff942299a06f538a41dd29f472892290
Mike Sun <mikesun197@gmail.com>
2022-07-21 18:06:44 -0400

    Support #28214 Change Sanger Select Samples
    -Added the correct titles messages
    -Changed the molecular sample to material sample in pcr batch item

--- packages/dina-ui/intl/seqdb-fr.ts
+++ packages/dina-ui/intl/seqdb-fr.ts
@@ -88,6 +88,7 @@ export const SEQDB_MESSAGES_FRENCH: Partial<typeof SEQDB_MESSAGES_ENGLISH> = {
   selectAllCheckedSamplesButtonText:
     "Sélectionner tous les échantillons cochés -->",
   selectButtonText: "Sélectionner",
+  selectMaterialSamples: "Sélectionner échantillons matériaux",
   selectSamples: "Sélectionner échantillons moléculaires",
   selectedSamplesTitle: "Échantillons sélectionnés",
   seqdbTitle: "Base de données des séquences (fr)",
