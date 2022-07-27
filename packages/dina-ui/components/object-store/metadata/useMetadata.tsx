1dd3dbe1b3dadbb9684c29ad403d42415d82c0cc
John Phan <johnphan010@gmail.com>
2022-07-27 15:03:09 -0700

    28355 Progress - Write useMetadataQuery inside useMetadata

---
+++ packages/dina-ui/components/object-store/metadata/useMetadata.tsx
@@ -0,0 +1,38 @@
+import { useApiClient, useQuery } from "../../../../common-ui/lib";
+import { Metadata, License } from "../../../../dina-ui/types/objectstore-api"; // packages/dina-ui/types/objectstore-api"
+import React from "react";
+
+export function useMetadataQuery(id?: string | null) {
+  const { apiClient } = useApiClient();
+
+  const metadataQuery = useQuery<Metadata>(
+    {
+      path: `objectstore-api/metadata/${id}`,
+      include: "dcCreator,derivatives"
+    },
+    {
+      joinSpecs: [
+        // Join to persons api:
+        {
+          apiBaseUrl: "/agent-api",
+          idField: "dcCreator",
+          joinField: "dcCreator",
+          path: metadata => `person/${metadata.dcCreator.id}`
+        }
+      ],
+      onSuccess: async ({ data: metadata }) => {
+        // Get the License resource based on the Metadata's xmpRightsWebStatement field:
+        if (metadata.xmpRightsWebStatement) {
+          const url = metadata.xmpRightsWebStatement;
+          (metadata as any).license = (
+            await apiClient.get<License[]>("objectstore-api/license", {
+              filter: { url }
+            })
+          ).data[0];
+        }
+      }
+    }
+  );
+
+  return metadataQuery;
+}
