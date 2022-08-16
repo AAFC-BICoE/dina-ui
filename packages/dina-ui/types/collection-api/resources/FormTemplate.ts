b2ea9e7dad637fb5fa616f08892dc20032f78bf2
Brandon Andre <andr0284@algonquinlive.com>
2022-08-16 10:59:00 -0400

    Feature #28513 - Form template to generate new structure
    - Added the TypeScript type for the new component field of Form Templates. This will be used for saving the new Form Template structure.

--- packages/dina-ui/types/collection-api/resources/FormTemplate.ts
+++ packages/dina-ui/types/collection-api/resources/FormTemplate.ts
@@ -10,4 +10,81 @@ export interface FormTemplate extends KitsuResource {
 
   /** This field is JSON with unknown structure, so use Yup or Zod for schema validation / casting. */
   viewConfiguration?: unknown;
+
+  components?: FormTemplateComponent[];
+}
+
+/**
+ * The FormTemplateComponent resource represents a data component of a FormTemplate.
+ *
+ * This is considered the high level section of the form. It can only be ordered in the Y axis.
+ *
+ * For example, the "Associations" data component of the Material Sample edit page is a
+ * FormTemplateComponent.
+ */
+export interface FormTemplateComponent {
+  /** Name of the component. */
+  name?: string;
+
+  /** Data component order. Lowest being at the top, highest at the bottom of the list. */
+  order?: number;
+
+  /** Data component visibility. */
+  visible?: boolean;
+
+  /** Data component maximum X position allowed. */
+  gridSizeX?: number;
+
+  sections?: FormTemplateSection[];
+}
+
+/**
+ * The FormTemplateSection resource represents a section of a FormTemplate.
+ *
+ * A data component can have multiple sections. Sections can be ordered in the X and Y axis.
+ * Sections are usually considered the sub categories of the data component.
+ *
+ * For example, in the "Associations" data component of the Material Sample edit page, the
+ * "Host Organism" section is a FormTemplateSection.
+ */
+export interface FormTemplateSection {
+  /** Name of the section. */
+  name?: string;
+
+  /** Section visibility. */
+  visible?: boolean;
+
+  /** The X position to display the section. */
+  gridPositionX?: number;
+
+  /** The Y position to display the section. */
+  gridPositionY?: number;
+
+  items?: FormTemplateSectionItem[];
+}
+
+/**
+ * An item is considered a field in a Form Template.
+ *
+ * Fields can be ordered in the X and Y axis within the section they are in. Items can have a
+ * default value set they represent a unit of data.
+ *
+ * For example, In the "Associations" data component, the "Host Organism" section, the "Host" field
+ * is a FormTemplateSectionItem.
+ */
+export interface FormTemplateSectionItem {
+  /** Name of the item. */
+  name?: string;
+
+  /** Section visibility. */
+  visible?: boolean;
+
+  /** The X position to display the section. */
+  gridPositionX?: number;
+
+  /** The Y position to display the section. */
+  gridPositionY?: number;
+
+  /** Default value for the section. Can be any type. */
+  defaultValue?: any;
 }
