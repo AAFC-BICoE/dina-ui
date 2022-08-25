0f551f28928201190cb416cc6723cd4cbf889b79
John Phan <johnphan010@gmail.com>
2022-08-25 14:09:25 -0700

    28470 Progress - Refactor single metadata upload functionality into its own component

--- packages/dina-ui/pages/object-store/metadata/edit.tsx
+++ packages/dina-ui/pages/object-store/metadata/edit.tsx
@@ -1,94 +1,18 @@
-import {
-  withResponse,
-  ApiClientContext,
-  useAccount,
-  BackButton,
-  ButtonBar,
-  SubmitButton
-} from "common-ui";
+import { withResponse, BackButton, ButtonBar, SubmitButton } from "common-ui";
 import { useRouter } from "next/router";
 import { Footer, Head, Nav } from "../../../components";
 import { DinaMessage, useDinaIntl } from "../../../intl/dina-ui-intl";
-import {
-  DefaultValue,
-  Metadata,
-  ObjectUpload
-} from "../../../types/objectstore-api";
+import { Metadata } from "../../../types/objectstore-api";
 import { useMetadataEditQuery } from "../../../components/object-store/metadata/useMetadata";
-import { useLocalStorage } from "@rehooks/local-storage";
-import { BULK_ADD_IDS_KEY } from "../upload";
-import { InputResource, PersistedResource } from "kitsu";
-import moment from "moment";
-import { useContext, useState, useEffect } from "react";
+import { InputResource } from "kitsu";
 import { MetadataForm } from "../../../components/object-store/metadata/MetadataForm";
+import { MetadataUpload } from "../../../components/object-store/metadata/MetadataUpload";
 
 export default function MetadataEditPage() {
   const router = useRouter();
   const id = router.query.id?.toString();
   const { formatMessage } = useDinaIntl();
   const query = useMetadataEditQuery(id);
-  const { apiClient, bulkGet } = useContext(ApiClientContext);
-  const { agentId } = useAccount();
-
-  const [objectUploadIds] = useLocalStorage<string[]>(BULK_ADD_IDS_KEY);
-
-  const group = router?.query?.group as string;
-  const [uploadMetadata, setMetadata] = useState<Metadata>();
-  useEffect(() => {
-    loadData().then((value) => {
-      setMetadata(value);
-    });
-  }, []);
-
-  async function loadData() {
-    let objectUploads: PersistedResource<ObjectUpload>[] = [];
-    if (group && objectUploadIds) {
-      objectUploads = await bulkGet<ObjectUpload>(
-        objectUploadIds.map((metadataId) => `/object-upload/${metadataId}`),
-        {
-          apiBaseUrl: "/objectstore-api"
-        }
-      );
-    }
-    // Set default values for the new Metadatas:
-    const data = await apiClient.get<{ values: DefaultValue[] }>(
-      "objectstore-api/config/default-values",
-      {}
-    );
-    const metadataDefaults: Partial<Metadata> = {
-      publiclyReleasable: true
-    };
-    const defaultValues = data?.data?.values;
-    if (data) {
-      for (const defaultValue of defaultValues.filter(
-        ({ type }) => type === "metadata"
-      )) {
-        metadataDefaults[defaultValue.attribute as keyof Metadata] =
-          defaultValue.value as any;
-      }
-    }
-
-    const newMetadatas = objectUploads.map<Metadata>((objectUpload) => ({
-      ...metadataDefaults,
-      acCaption: objectUpload.originalFilename,
-      acDigitizationDate: objectUpload.dateTimeDigitized
-        ? moment(objectUpload.dateTimeDigitized).format()
-        : null,
-      acMetadataCreator: agentId
-        ? {
-            id: agentId,
-            type: "person"
-          }
-        : null,
-      bucket: group,
-      dcType: objectUpload.dcType,
-      fileIdentifier: objectUpload.id,
-      originalFilename: objectUpload.originalFilename,
-      type: "metadata"
-    }));
-    return newMetadatas[0];
-  }
-
   const title = id ? "editMetadataTitle" : "addMetadataTitle";
   const buttonBar = (
     <ButtonBar>
@@ -119,13 +43,7 @@ export default function MetadataEditPage() {
             ))}
           </div>
         ) : (
-          uploadMetadata && (
-            <MetadataForm
-              metadata={uploadMetadata as InputResource<Metadata>}
-              onSaved={redirectToSingleMetadataPage}
-              buttonBar={buttonBar}
-            />
-          )
+          <MetadataUpload buttonBar={buttonBar} />
         )}
       </main>
       <Footer />
