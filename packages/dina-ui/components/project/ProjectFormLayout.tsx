3c483337f75bb900d2cdd94282fe4ef316f7bc0c
John Phan <johnphan010@gmail.com>
2022-09-06 10:44:38 -0700

    28251 Progress - Refactor ProjectFormLayout component out of edit page into its own file

---
+++ packages/dina-ui/components/project/ProjectFormLayout.tsx
@@ -0,0 +1,67 @@
+import { DateField, TextField, useDinaFormContext } from "common-ui";
+import { AttachmentsField, GroupSelectField } from "..";
+import { DinaMessage, useDinaIntl } from "../../intl/dina-ui-intl";
+
+export function ProjectFormLayout() {
+  const { readOnly, initialValues } = useDinaFormContext();
+  const { formatMessage } = useDinaIntl();
+
+  return (
+    <div>
+      <div className="row">
+        <GroupSelectField
+          name="group"
+          enableStoredDefaultGroup={true}
+          className="col-md-6"
+        />
+      </div>
+      <div className="row">
+        <TextField
+          className="col-md-6 name"
+          name="name"
+          label={formatMessage("field_projectName")}
+        />
+        <DateField
+          className="col-md-6 startDate"
+          name="startDate"
+          label={formatMessage("field_startDate")}
+        />
+      </div>
+      <div className="row">
+        <TextField
+          className="col-md-6 status"
+          name="status"
+          label={formatMessage("field_projectStatus")}
+        />
+        <DateField
+          className="col-md-6 endDate"
+          name="endDate"
+          label={formatMessage("field_endDate")}
+        />
+      </div>
+      <div className="row">
+        <TextField
+          className="col-md-6 english-description"
+          name="multilingualDescription.en"
+          label={formatMessage("field_description.en")}
+          multiLines={true}
+        />
+        <TextField
+          className="col-md-6 french-description"
+          name="multilingualDescription.fr"
+          label={formatMessage("field_description.fr")}
+          multiLines={true}
+        />
+      </div>
+      <AttachmentsField
+        name="attachment"
+        title={<DinaMessage id="projectAttachments" />}
+        id="project-attachments-section"
+        allowNewFieldName="attachmentsConfig.allowNew"
+        allowExistingFieldName="attachmentsConfig.allowExisting"
+        attachmentPath={`collection-api/project/${initialValues?.id}/attachment`}
+        hideAddAttchmentBtn={true}
+      />
+    </div>
+  );
+}
