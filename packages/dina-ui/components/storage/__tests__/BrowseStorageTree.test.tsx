df08efc0136c6f1794669525f9ba1f32cd5a1013
John Phan <johnphan010@gmail.com>
2022-10-06 11:51:08 -0700

    28771 Progress - Fixed tests in StorageUnitChildrenViewer

--- packages/dina-ui/components/storage/__tests__/BrowseStorageTree.test.tsx
+++ packages/dina-ui/components/storage/__tests__/BrowseStorageTree.test.tsx
@@ -7,7 +7,7 @@ import { BrowseStorageTree } from "../BrowseStorageTree";
 /** Top-level storage. */
 const STORAGE_A: PersistedResource<StorageUnit> = {
   id: "A",
-  group: "group",
+  group: "aafc",
   name: "A",
   type: "storage-unit"
 };
@@ -15,7 +15,7 @@ const STORAGE_A: PersistedResource<StorageUnit> = {
 /** Units B and C are inside Unit A. */
 const STORAGE_B: PersistedResource<StorageUnit> = {
   id: "B",
-  group: "group",
+  group: "aafc",
   name: "B",
   type: "storage-unit",
   parentStorageUnit: {
@@ -25,7 +25,7 @@ const STORAGE_B: PersistedResource<StorageUnit> = {
 };
 const STORAGE_C: PersistedResource<StorageUnit> = {
   id: "C",
-  group: "group",
+  group: "aafc",
   name: "C",
   type: "storage-unit",
   parentStorageUnit: {
@@ -37,7 +37,7 @@ const STORAGE_C: PersistedResource<StorageUnit> = {
 /** D is inside C. */
 const STORAGE_D: PersistedResource<StorageUnit> = {
   id: "D",
-  group: "group",
+  group: "aafc",
   name: "D",
   type: "storage-unit",
   parentStorageUnit: {
@@ -49,7 +49,10 @@ const STORAGE_D: PersistedResource<StorageUnit> = {
 const mockGet = jest.fn<any, any>(async (path, params = {}) => {
   switch (path) {
     case "collection-api/storage-unit":
-      if (params.filter?.parentStorageUnit === null) {
+      if (
+        params.filter?.parentStorageUnit === null ||
+        params.filter?.rsql === "group==aafc;group==cnc"
+      ) {
         // Top-level units:
         return { data: [STORAGE_A], meta: { totalResourceCount: 1 } };
       } else if (params.filter?.rsql === "parentStorageUnit.uuid==A") {
