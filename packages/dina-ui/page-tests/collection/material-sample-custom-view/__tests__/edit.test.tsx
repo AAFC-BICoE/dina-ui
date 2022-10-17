0d8166962000c41612aa3031fc6776074ffa0d68
John Phan <johnphan010@gmail.com>
2022-10-17 11:08:04 -0700

    28748 Improve Form Template editing test coverage - Fixed mockOnSave not having any saved mock objects - Added mock Form Template to be used for tests - Added test for custom Nav Order

--- packages/dina-ui/page-tests/collection/material-sample-custom-view/__tests__/edit.test.tsx
+++ packages/dina-ui/page-tests/collection/material-sample-custom-view/__tests__/edit.test.tsx
@@ -13,6 +13,7 @@ import {
   StorageUnit
 } from "../../../../types/collection-api";
 import { noop } from "lodash";
+import { getComponentOrderFromTemplate } from "../../../../components/form-template/formTemplateUtils";
 
 const mockOnSaved = jest.fn();
 
@@ -123,8 +124,9 @@ async function mountForm(
 ) {
   const wrapper = mountWithAppContext(
     <FormTemplateEditPageLoaded
+      id={existingActionDefinition?.id}
       fetchedFormTemplate={existingActionDefinition}
-      onSaved={noop}
+      onSaved={mockOnSaved}
     />,
     { apiContext }
   );
@@ -227,6 +229,715 @@ async function mountForm(
   };
 }
 
+const formTemplate: PersistedResource<FormTemplate> = {
+  id: "123",
+  type: "form-template",
+  name: "form1",
+  group: "aafc",
+  restrictToCreatedBy: false,
+  viewConfiguration: {},
+  components: [
+    {
+      name: "identifiers-component",
+      visible: true,
+      order: 0,
+      sections: [
+        {
+          name: "general-section",
+          visible: true,
+          items: [
+            {
+              name: "tags",
+              visible: false
+            },
+            {
+              name: "projects",
+              visible: false
+            },
+            {
+              name: "assemblages",
+              visible: false
+            },
+            {
+              name: "publiclyReleasable",
+              visible: false
+            },
+            {
+              name: "notPubliclyReleasableReason",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "identifiers-section",
+          visible: true,
+          items: [
+            {
+              name: "collection",
+              visible: false
+            },
+            {
+              name: "materialSampleName",
+              visible: false
+            },
+            {
+              name: "useNextSequence",
+              visible: false
+            },
+            {
+              name: "dwcOtherCatalogNumbers",
+              visible: false
+            },
+            {
+              name: "barcode",
+              visible: false
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "material-sample-info-component",
+      visible: true,
+      order: 1,
+      sections: [
+        {
+          name: "material-sample-info-section",
+          visible: true,
+          items: [
+            {
+              name: "materialSampleType",
+              visible: false
+            },
+            {
+              name: "materialSampleRemarks",
+              visible: false
+            },
+            {
+              name: "materialSampleState",
+              visible: false
+            },
+            {
+              name: "stateChangeRemarks",
+              visible: false
+            },
+            {
+              name: "stateChangedOn",
+              visible: false
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "collecting-event-component",
+      visible: false,
+      order: 3,
+      sections: [
+        {
+          name: "general-section",
+          visible: true,
+          items: [
+            {
+              name: "tags",
+              visible: false
+            },
+            {
+              name: "publiclyReleasable",
+              visible: false
+            },
+            {
+              name: "notPubliclyReleasableReason",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "identifiers-section",
+          visible: true,
+          items: [
+            {
+              name: "dwcFieldNumber",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "collecting-date-section",
+          visible: true,
+          items: [
+            {
+              name: "verbatimEventDateTime",
+              visible: false
+            },
+            {
+              name: "startEventDateTime",
+              visible: false
+            },
+            {
+              name: "endEventDateTime",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "collecting-agents-section",
+          visible: true,
+          items: [
+            {
+              name: "dwcRecordedBy",
+              visible: false
+            },
+            {
+              name: "collectors",
+              visible: false
+            },
+            {
+              name: "dwcRecordNumber",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "verbatim-label-section",
+          visible: true,
+          items: [
+            {
+              name: "dwcVerbatimLocality",
+              visible: false
+            },
+            {
+              name: "dwcVerbatimCoordinateSystem",
+              visible: false
+            },
+            {
+              name: "dwcVerbatimCoordinates",
+              visible: false
+            },
+            {
+              name: "dwcVerbatimLatitude",
+              visible: false
+            },
+            {
+              name: "dwcVerbatimLongitude",
+              visible: false
+            },
+            {
+              name: "dwcVerbatimSRS",
+              visible: false
+            },
+            {
+              name: "dwcVerbatimElevation",
+              visible: false
+            },
+            {
+              name: "dwcVerbatimDepth",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "collecting-event-details",
+          visible: true,
+          items: [
+            {
+              name: "habitat",
+              visible: false
+            },
+            {
+              name: "host",
+              visible: false
+            },
+            {
+              name: "collectionMethod",
+              visible: false
+            },
+            {
+              name: "substrate",
+              visible: false
+            },
+            {
+              name: "dwcMinimumElevationInMeters",
+              visible: false
+            },
+            {
+              name: "dwcMaximumElevationInMeters",
+              visible: false
+            },
+            {
+              name: "dwcMinimumDepthInMeters",
+              visible: false
+            },
+            {
+              name: "dwcMaximumDepthInMeters",
+              visible: false
+            },
+            {
+              name: "remarks",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "georeferencing-section",
+          visible: true,
+          items: [
+            {
+              name: "geoReferenceAssertions[0].dwcGeoreferenceVerificationStatus",
+              visible: false
+            },
+            {
+              name: "geoReferenceAssertions[0].dwcDecimalLatitude",
+              visible: false
+            },
+            {
+              name: "geoReferenceAssertions[0].dwcDecimalLongitude",
+              visible: false
+            },
+            {
+              name: "geoReferenceAssertions[0].dwcCoordinateUncertaintyInMeters",
+              visible: false
+            },
+            {
+              name: "geoReferenceAssertions[0].dwcGeoreferencedDate",
+              visible: false
+            },
+            {
+              name: "geoReferenceAssertions[0].dwcGeodeticDatum",
+              visible: false
+            },
+            {
+              name: "geoReferenceAssertions[0].literalGeoreferencedBy",
+              visible: false
+            },
+            {
+              name: "geoReferenceAssertions[0].georeferencedBy",
+              visible: false
+            },
+            {
+              name: "geoReferenceAssertions[0].dwcGeoreferenceProtocol",
+              visible: false
+            },
+            {
+              name: "geoReferenceAssertions[0].dwcGeoreferenceSources",
+              visible: false
+            },
+            {
+              name: "geoReferenceAssertions[0].dwcGeoreferenceRemarks",
+              visible: false
+            },
+            {
+              name: "geoReferenceAssertions",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "current-geographic-place",
+          visible: true,
+          items: [
+            {
+              name: "srcAdminLevels",
+              visible: false
+            },
+            {
+              name: "geographicPlaceNameSourceDetail.stateProvince",
+              visible: false
+            },
+            {
+              name: "geographicPlaceNameSourceDetail.country",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "collecting-event-managed-attributes-section",
+          visible: true,
+          items: []
+        },
+        {
+          name: "collecting-event-attachments-section",
+          visible: true,
+          items: [
+            {
+              name: "managedAttributes.attachmentsConfig.allowNew",
+              visible: false
+            },
+            {
+              name: "managedAttributes.attachmentsConfig.allowExisting",
+              visible: false
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "acquisition-event-component",
+      visible: false,
+      order: 2,
+      sections: [
+        {
+          name: "acquisition-event-reception-section",
+          visible: true,
+          items: [
+            {
+              name: "group",
+              visible: false,
+              defaultValue: "aafc"
+            },
+            {
+              name: "receivedFrom",
+              visible: false
+            },
+            {
+              name: "receivedDate",
+              visible: false
+            },
+            {
+              name: "receptionRemarks",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "acquisition-event-isolation-section",
+          visible: true,
+          items: [
+            {
+              name: "isolatedBy",
+              visible: false
+            },
+            {
+              name: "isolatedOn",
+              visible: false
+            },
+            {
+              name: "isolationRemarks",
+              visible: false
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "preparations-component",
+      visible: false,
+      order: 4,
+      sections: [
+        {
+          name: "general-section",
+          visible: true,
+          items: [
+            {
+              name: "preparationType",
+              visible: false
+            },
+            {
+              name: "preparationMethod",
+              visible: false
+            },
+            {
+              name: "preservationType",
+              visible: false
+            },
+            {
+              name: "preparationFixative",
+              visible: false
+            },
+            {
+              name: "preparationMaterials",
+              visible: false
+            },
+            {
+              name: "preparationSubstrate",
+              visible: false
+            },
+            {
+              name: "preparationRemarks",
+              visible: false
+            },
+            {
+              name: "dwcDegreeOfEstablishment",
+              visible: false
+            },
+            {
+              name: "preparedBy",
+              visible: false
+            },
+            {
+              name: "preparationDate",
+              visible: false
+            },
+            {
+              name: "preparationProtocol",
+              visible: false
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "organisms-component",
+      visible: false,
+      order: 5,
+      sections: [
+        {
+          name: "organisms-general-section",
+          visible: true,
+          items: [
+            {
+              name: "organism[0].lifeStage",
+              visible: false
+            },
+            {
+              name: "organism[0].sex",
+              visible: false
+            },
+            {
+              name: "organism[0].remarks",
+              visible: false
+            },
+            {
+              name: "organism",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "organism-verbatim-determination-section",
+          visible: true,
+          items: [
+            {
+              name: "organism[0].determination[0].verbatimScientificName",
+              visible: false
+            },
+            {
+              name: "organism[0].determination[0].verbatimDeterminer",
+              visible: false
+            },
+            {
+              name: "organism[0].determination[0].verbatimDate",
+              visible: false
+            },
+            {
+              name: "organism[0].determination[0].verbatimRemarks",
+              visible: false
+            },
+            {
+              name: "organism[0].determination[0].transcriberRemarks",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "organism-determination-section",
+          visible: true,
+          items: [
+            {
+              name: "organism[0].determination[0].scientificName",
+              visible: false
+            },
+            {
+              name: "organism[0].determination[0].scientificNameInput",
+              visible: false
+            },
+            {
+              name: "organism[0].determination[0].determiner",
+              visible: false
+            },
+            {
+              name: "organism[0].determination[0].determinedOn",
+              visible: false
+            },
+            {
+              name: "organism[0].determination[0].determinationRemarks",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "organism-type-specimen-section",
+          visible: true,
+          items: [
+            {
+              name: "organism[0].determination[0].typeStatus",
+              visible: false
+            },
+            {
+              name: "organism[0].determination[0].typeStatusEvidence",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "organism-managed-attributes-section",
+          visible: true,
+          items: []
+        }
+      ]
+    },
+    {
+      name: "associations-component",
+      visible: false,
+      order: 6,
+      sections: [
+        {
+          name: "associations-host-organism-section",
+          visible: true,
+          items: [
+            {
+              name: "hostOrganism.name",
+              visible: false
+            },
+            {
+              name: "hostOrganism.remarks",
+              visible: false
+            }
+          ]
+        },
+        {
+          name: "associations-material-sample-section",
+          visible: true,
+          items: [
+            {
+              name: "associations.associationType",
+              visible: false
+            },
+            {
+              name: "associations.associatedSample",
+              visible: false
+            },
+            {
+              name: "associations.remarks",
+              visible: false
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "storage-component",
+      visible: false,
+      order: 7,
+      sections: [
+        {
+          name: "storage-selection-section",
+          visible: true,
+          items: [
+            {
+              name: "storageUnit",
+              visible: false
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "restriction-component",
+      visible: false,
+      order: 8,
+      sections: [
+        {
+          name: "restriction-general-section",
+          visible: true,
+          items: [
+            {
+              name: "phac_animal_rg",
+              visible: false
+            },
+            {
+              name: "cfia_ppc",
+              visible: false
+            },
+            {
+              name: "phac_human_rg",
+              visible: false
+            },
+            {
+              name: "phac_cl",
+              visible: false
+            },
+            {
+              name: "isRestricted",
+              visible: false
+            },
+            {
+              name: "restrictionRemarks",
+              visible: false
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "scheduled-actions-component",
+      visible: false,
+      order: 9,
+      sections: [
+        {
+          name: "scheduled-actions-add-section",
+          visible: true,
+          items: [
+            {
+              name: "scheduledAction.actionType",
+              visible: false
+            },
+            {
+              name: "scheduledAction.actionStatus",
+              visible: false
+            },
+            {
+              name: "scheduledAction.date",
+              visible: false
+            },
+            {
+              name: "scheduledAction.assignedTo",
+              visible: false
+            },
+            {
+              name: "scheduledAction.remarks",
+              visible: false
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: "managed-attributes-component",
+      visible: true,
+      order: 10,
+      sections: [
+        {
+          name: "managed-attributes-section",
+          visible: true,
+          items: []
+        }
+      ]
+    },
+    {
+      name: "material-sample-attachments-component",
+      visible: true,
+      order: 11,
+      sections: [
+        {
+          name: "material-sample-attachments-sections",
+          visible: true,
+          items: [
+            {
+              name: "attachmentsConfig.allowNew",
+              visible: false
+            },
+            {
+              name: "attachmentsConfig.allowExisting",
+              visible: false
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+
 describe("Workflow template edit page", () => {
   beforeEach(jest.clearAllMocks);
 
@@ -243,20 +954,34 @@ describe("Workflow template edit page", () => {
     );
   });
 
+  it("Renders the template page with a custom Nav Order.", async () => {
+    const { submitForm } = await mountForm(formTemplate);
+
+    await submitForm();
+
+    // The nav order was re-saved:
+    const navOrder = getComponentOrderFromTemplate(
+      mockOnSaved.mock.calls[0][0]
+    );
+    expect(navOrder).toEqual([
+      "identifiers-component",
+      "material-sample-info-component",
+      "acquisition-event-component",
+      "collecting-event-component",
+      "preparations-component",
+      "organisms-component",
+      "associations-component",
+      "storage-component",
+      "restriction-component",
+      "scheduled-actions-component",
+      "managed-attributes-component",
+      "material-sample-attachments-component"
+    ]);
+  });
+
   it("Edits an existing action-definition: Renders the form with minimal data.", async () => {
     const { colEventSwitch, catalogSwitch, scheduledActionsSwitch } =
-      await mountForm({
-        type: "form-template",
-        viewConfiguration: {
-          formTemplate: {},
-          navOrder: null,
-          type: "material-sample-form-template"
-        },
-        group: "test-group-1",
-        id: "123",
-        restrictToCreatedBy: false,
-        name: "test-config"
-      });
+      await mountForm(formTemplate);
 
     // Checkboxes are unchecked:
     expect(colEventSwitch().prop("checked")).toEqual(false);
