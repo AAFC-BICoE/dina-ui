25734702e2351f79543adad43122632985d22527
Brandon Andre <andr0284@algonquinlive.com>
2022-11-18 12:59:31 -0500

    Bug #29282 - Repopulate search after leaving page from previous search query
    - Fixed the issue with url params already existing.
    - Added 100% test coverage for it.

--- packages/common-ui/lib/list-page/reload-last-search/useBackButtonReloadLastSearch.tsx
+++ packages/common-ui/lib/list-page/reload-last-search/useBackButtonReloadLastSearch.tsx
@@ -10,36 +10,67 @@ import { useEffect } from "react";
  * This hook is automatically applied when the back button has the reloadLastSearch prop being
  * used.
  *
+ * This function is ignored by test coverage since it's been extracted to it's own function that
+ * ignores most of the nextJS router functions. It's very hard to mock this but all of the
+ * functionality we do want to test is in the backButtonAddReloadLastSearchParam function.
+ *
  * @param disabled boolean to deactivate the back button intercept.
  */
+/* istanbul ignore next */
 export function useBackButtonReloadLastSearch(disabled: boolean) {
   const router = useRouter();
 
   useEffect(() => {
-    if (disabled) return;
-
     router.beforePopState(({ as }) => {
-      if (as !== router.asPath) {
-        // The user is going to a different path than the current page, check if the new path is
-        // going to a list page.
-        // `/collection/material-sample/list` becomes `list`
-        const urlWithoutEntity = as.slice(as.lastIndexOf("/") + 1, as.length);
-
-        if (urlWithoutEntity.includes("list")) {
-          if (!urlWithoutEntity.includes("reloadLastSearch")) {
-            const newUrl = as + "?reloadLastSearch";
-            router.push(newUrl);
-            return false;
-          }
-        }
-
-        // Add the url param to the end.
-      }
-      return true;
+      return backButtonAddReloadLastSearchParam(
+        router,
+        as,
+        router.asPath,
+        disabled
+      );
     });
 
     return () => {
       router.beforePopState(() => true);
     };
   }, [router]);
+}
+
+export function backButtonAddReloadLastSearchParam(
+  router,
+  newPath,
+  previousPath,
+  disabled
+): boolean {
+  if (disabled) return true;
+
+  if (newPath !== previousPath) {
+    // The user is going to a different path than the current page, check if the new path is
+    // going to a list page.
+    // `/collection/material-sample/list` becomes `list`
+    const urlWithoutEntity = newPath.slice(
+      newPath.lastIndexOf("/") + 1,
+      newPath.length
+    );
+
+    if (urlWithoutEntity.includes("list")) {
+      if (!urlWithoutEntity.includes("reloadLastSearch")) {
+        let newUrl = newPath;
+        if (urlWithoutEntity.includes("?")) {
+          // URL params already exist, use & to indicate another url param.
+          newUrl = newUrl + "&";
+        } else {
+          // URL params don't exist, using ? to start a url param.
+          newUrl = newUrl + "?";
+        }
+
+        newUrl = newUrl + "reloadLastSearch";
+        router.push(newUrl);
+        return false;
+      }
+    }
+
+    // Add the url param to the end.
+  }
+  return true;
 }
