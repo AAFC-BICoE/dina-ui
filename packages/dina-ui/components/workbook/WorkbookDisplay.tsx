ac2a0e427a8feaefe717cfc5f38103b2dd7ea08b
Brandon Andre <andr0284@algonquinlive.com>
2022-11-29 15:19:26 -0500

    Support #29366 - Add back workbook uploading to homescreen and navigation
    - Added back "Upload Workbook" to nav and home page.
    - Converted old class style react components to functional components.
    - Pages are now using Page Layout to use more of the screen.

--- packages/dina-ui/components/workbook/WorkbookDisplay.tsx
+++ packages/dina-ui/components/workbook/WorkbookDisplay.tsx
@@ -1,56 +1,32 @@
-import { Component } from "react";
-import { DinaMessage } from "../../intl/dina-ui-intl";
-import { WorkbookJSON, WorkbookRow } from "./WorkbookConversion";
+import { WorkbookRow, WorkbookJSON } from "./types/Workbook";
 
-interface WorkbookDisplayProps {
-  jsonData: WorkbookJSON;
-  backButton: () => void;
-}
-
-export class WorkbookDisplay extends Component<WorkbookDisplayProps> {
-  render() {
-    const { jsonData } = this.props;
-
-    if (jsonData) {
-      return (
-        <div>
-          <button
-            type="button"
-            className="btn btn-outline-secondary mrgn-bttm-md"
-            onClick={this.props.backButton}
-          >
-            <DinaMessage id="cancelButtonText" />
-          </button>
-          <div>
-            <table className="table">
-              <thead>
-                <tr>
-                  {jsonData[0].content.map(col => (
-                    <th key={col}>{col}</th>
-                  ))}
+export function WorkbookDisplay({ jsonData }: { jsonData: WorkbookJSON }) {
+  return (
+    <div style={{ width: "100%", overflowX: "auto", height: "70hp" }}>
+      <table className="table">
+        <thead>
+          <tr>
+            {jsonData[0].content.map((col) => (
+              <th key={col}>{col}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {jsonData.map((row: WorkbookRow, index: number) => {
+            // Skip the first row since it's already been displayed.
+            if (index !== 0) {
+              return (
+                <tr key={row.rowNumber}>
+                  {row.content.map((col) => {
+                    // Render the columns inside of the row.
+                    return <td key={col}>{col}</td>;
+                  })}
                 </tr>
-              </thead>
-              <tbody>
-                {jsonData.map((row: WorkbookRow, index: number) => {
-                  // Skip the first row since it's already been displayed.
-                  if (index !== 0) {
-                    return (
-                      <tr key={row.rowNumber}>
-                        {row.content.map(col => {
-                          // Render the columns inside of the row.
-                          return <td key={col}>{col}</td>;
-                        })}
-                      </tr>
-                    );
-                  }
-                })}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      );
-    }
-  }
+              );
+            }
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
 }
-
-export default WorkbookDisplay;
