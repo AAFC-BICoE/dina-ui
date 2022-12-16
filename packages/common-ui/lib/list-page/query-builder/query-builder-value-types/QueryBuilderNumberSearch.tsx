8b00d97b8d47045df68a79fb7e8f6e37ebc01139
John Phan <johnphan010@gmail.com>
2022-12-16 08:30:33 -0800

    Revert "Support #28893 - Add test support back to the query page (#854)"
    This reverts commit f59d281b94707e6a0f33f7ffd556391881e3ef9c.

--- packages/common-ui/lib/list-page/query-builder/query-builder-value-types/QueryBuilderNumberSearch.tsx
+++ packages/common-ui/lib/list-page/query-builder/query-builder-value-types/QueryBuilderNumberSearch.tsx
@@ -241,5 +241,7 @@ function buildNumberRangeObject(matchType, value) {
       return { lt: value };
     case "lessThanOrEqualTo":
       return { lte: value };
+    default:
+      return value;
   }
 }
