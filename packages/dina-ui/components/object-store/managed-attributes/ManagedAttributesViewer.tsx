6f761f85f66492563186b39c95aeb7c7809fe779
Chuanbao Lu <chuanbao.lu@gmail.com>
2022-12-21 17:14:05 -0500

    29497 object store detail page sending unnecessary requests

--- packages/dina-ui/components/object-store/managed-attributes/ManagedAttributesViewer.tsx
+++ packages/dina-ui/components/object-store/managed-attributes/ManagedAttributesViewer.tsx
@@ -1,6 +1,7 @@
-import { DinaForm, FieldView, useQuery } from "common-ui";
-import { KitsuResource } from "kitsu";
+import { DinaForm, FieldView, useApiClient, useIsMounted } from "common-ui";
 import { toPairs } from "lodash";
+import { ManagedAttribute } from "packages/dina-ui/types/objectstore-api";
+import { useEffect, useState } from "react";
 import { DinaMessage } from "../../../intl/dina-ui-intl";
 
 export interface ManagedAttributesViewerProps {
@@ -9,39 +10,63 @@ export interface ManagedAttributesViewerProps {
    * Key is Managed Attribute UUID and value is the Managed Attribute value object.
    */
   values?: Record<string, string | null | undefined> | null;
-
-  /** Function that returns the API find-one path given the Managed Attribute key or ID. */
-  managedAttributeApiPath: (key: string) => string;
 }
 
 export function ManagedAttributesViewer({
-  values,
-  managedAttributeApiPath
+  values
 }: ManagedAttributesViewerProps) {
-  const managedAttributeValues = values
-    ? toPairs(values).map(([key, mav]) => ({
-        key,
-        value: mav
-      }))
-    : [];
+  const { apiClient } = useApiClient();
+  const [allAttrKeyNameMap, setAllAttrKeyNameMap] = useState<{
+    [key: string]: string;
+  }>({});
+  const isMounted = useIsMounted();
+
+  useEffect(() => {
+    async function getAllManagedAttributes() {
+      const { data } = await apiClient.get<ManagedAttribute[]>(
+        "objectstore-api/managed-attribute",
+        {}
+      );
+      const attrKeyNameMap = data.reduce(
+        (accu, obj) => ({
+          ...accu,
+          [obj.key]: obj.name
+        }),
+        {} as { [key: string]: string }
+      );
+      if (isMounted.current) {
+        setAllAttrKeyNameMap(attrKeyNameMap);
+      }
+    }
+    getAllManagedAttributes();
+  }, []);
+
+  const managedAttributeValues = (
+    values
+      ? toPairs(values).map(([key, mav]) => ({
+          key,
+          value: mav
+        }))
+      : []
+  ).map((item) => ({
+    ...item,
+    name: allAttrKeyNameMap[item.key]
+  }));
 
   return (
     <DinaForm initialValues={managedAttributeValues} readOnly={true}>
       {managedAttributeValues.length ? (
         <div className="row">
-          {managedAttributeValues.map((mav, index) => (
-            <FieldView
-              key={mav.key}
-              className="col-6"
-              label={
-                <ManagedAttributeName
-                  managedAttributeApiPath={managedAttributeApiPath}
-                  managedAttributeKey={mav.key}
-                />
-              }
-              name={`${index}.value`}
-            />
-          ))}
+          {managedAttributeValues.map((mav, index) => {
+            return (
+              <FieldView
+                key={mav.key}
+                className="col-6"
+                label={mav.name}
+                name={`${index}.value`}
+              />
+            );
+          })}
         </div>
       ) : (
         <div className="mb-3">
@@ -50,21 +75,4 @@ export function ManagedAttributesViewer({
       )}
     </DinaForm>
   );
-}
-
-export interface ManagedAttributeNameProps {
-  managedAttributeKey: string;
-  managedAttributeApiPath: (key: string) => string;
-}
-
-/** Render the name of a ManagedAttribute. */
-export function ManagedAttributeName({
-  managedAttributeKey,
-  managedAttributeApiPath
-}: ManagedAttributeNameProps) {
-  const { response } = useQuery<KitsuResource & { name: string }>({
-    path: managedAttributeApiPath(managedAttributeKey)
-  });
-
-  return <>{response?.data?.name ?? managedAttributeKey}</>;
 }
