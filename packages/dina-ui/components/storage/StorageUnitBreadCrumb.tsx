c99e5071b83f7e34072ca90b30c17de616d565c3
Brandon Andre <andr0284@algonquinlive.com>
2023-01-03 16:05:13 -0500

    - Added back Mike's changes.

--- packages/dina-ui/components/storage/StorageUnitBreadCrumb.tsx
+++ packages/dina-ui/components/storage/StorageUnitBreadCrumb.tsx
@@ -27,7 +27,7 @@ export function StorageUnitBreadCrumb({
       className="breadcrumb mb-0"
       style={{ "--bs-breadcrumb-divider": "'>'" } as any}
     >
-      {parentPath.map(node => (
+      {parentPath.map((node) => (
         <li className="breadcrumb-item" key={node.uuid}>
           <Link href={`/collection/storage-unit/view?id=${node.uuid}`}>
             <a>
@@ -54,5 +54,5 @@ export function StorageUnitBreadCrumb({
 }
 
 export function storageUnitDisplayName({ name, storageUnitType }: StorageUnit) {
-  return [storageUnitType?.name, name].filter(it => it).join(" ");
+  return name + " (" + storageUnitType?.name + ")";
 }
