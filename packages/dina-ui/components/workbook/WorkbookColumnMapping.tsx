7192529b76ba24e28a6e0224664ffae5f545ec5d
Brandon Andre <andr0284@algonquinlive.com>
2023-01-11 13:51:33 -0500

    Feature #24268 - Add column mapping support to the Workbook uploader
    - Index mapping now loaded for all the fields. No relationships are shown in the dropdowns just core fields.
    - Sheets and type disabled for now, set the default values.

--- packages/dina-ui/components/workbook/WorkbookColumnMapping.tsx
+++ packages/dina-ui/components/workbook/WorkbookColumnMapping.tsx
@@ -4,6 +4,9 @@ import { WorkbookJSON } from "./types/Workbook";
 import { getColumnHeaders } from "./utils/workbookMappingUtils";
 import Table from "react-bootstrap/Table";
 import { SelectField } from "packages/common-ui/lib";
+import { useIndexMapping } from "packages/common-ui/lib/list-page/useIndexMapping";
+import { useIntl } from "react-intl";
+import startCase from "lodash/startCase";
 
 export interface WorkbookColumnMappingProps {
   spreadsheetData: WorkbookJSON;
@@ -12,17 +15,48 @@ export interface WorkbookColumnMappingProps {
 export function WorkbookColumnMapping({
   spreadsheetData
 }: WorkbookColumnMappingProps) {
+  const { formatMessage, messages, locale } = useIntl();
+
+  // Retrieve the index map for the selected index. (Currently on the material sample is supported.)
+  const { indexMap } = useIndexMapping("dina_material_sample_index");
+
+  // Generate the options that can be selected for the field dropdown.
+  const queryRowOptions = useMemo(() => {
+    // Get all of the attributes from the index for the filter dropdown.
+    const simpleRowOptions = indexMap
+      ?.filter((prop) => !prop.parentPath)
+      ?.map((prop) => ({
+        label: messages["field_" + prop.label]
+          ? formatMessage({ id: "field_" + prop.label })
+          : startCase(prop.label),
+        value: prop.value
+      }))
+      ?.sort((aProp, bProp) => aProp.label.localeCompare(bProp.label));
+
+    return simpleRowOptions ? simpleRowOptions : [];
+  }, [indexMap, locale]);
+
   // Retrieve a string array of the headers from the uploaded spreadsheet.
   const headers = useMemo(() => {
     return getColumnHeaders(spreadsheetData);
   }, []);
 
   return (
-    <DinaForm initialValues={{}}>
+    <DinaForm initialValues={{ sheet: 1, type: "material-sample" }}>
       <div className="mb-3 border card px-4 py-2">
-        <div className="row">
-          <SelectField name={"sheet"} options={[]} className="col-md-6" />
-          <SelectField name={"type"} options={[]} className="col-md-6" />
+        <div className="row mt-2">
+          <SelectField
+            name={"sheet"}
+            options={[{ label: "Sheet 1", value: 1 }]}
+            disabled={true}
+            className="col-md-6"
+          />
+          <SelectField
+            name={"type"}
+            options={[{ label: "Material Sample", value: "material-sample" }]}
+            disabled={true}
+            className="col-md-6"
+          />
         </div>
       </div>
 
@@ -41,7 +75,7 @@ export function WorkbookColumnMapping({
               <td>
                 <SelectField
                   name={"fieldMap[" + columnHeader + "]"}
-                  options={[]}
+                  options={queryRowOptions as any}
                   hideLabel={true}
                 />
               </td>
