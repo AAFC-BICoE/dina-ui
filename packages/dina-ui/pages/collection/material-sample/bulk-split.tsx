023d8d2c358da86b417a918b551ac47ea3451098
Brandon Andre <andr0284@algonquinlive.com>
2023-01-20 09:07:43 -0500

    Feature #29702 - Refactor split view
    - Created the new endpoint response/request type.
    - Added the bulk split preview which updates as you change the settings.
    - Added a debounce to the useEffect to prevent overloading the endpoint.
    - Added the seriesGenerationOptions dropdown which is disabled if "continue series" is selected.

--- packages/dina-ui/pages/collection/material-sample/bulk-split.tsx
+++ packages/dina-ui/pages/collection/material-sample/bulk-split.tsx
@@ -8,10 +8,15 @@ import {
   RadioButtonsField,
   NumberSpinnerField,
   SubmitButton,
-  DinaFormSection
+  SelectField,
+  FieldSpy,
+  useApiClient,
+  LoadingSpinner
 } from "common-ui/lib";
 import { Card } from "react-bootstrap";
 import { DinaMessage, useDinaIntl } from "../../../intl/dina-ui-intl";
+import { useFormikContext } from "formik";
+import { MaterialSampleIdentifierGenerator } from "../../../types/collection-api/resources/MaterialSampleIdentifierGenerator";
 
 /**
  * String key for the local storage of the bulk split ids.
@@ -19,6 +24,12 @@ import { DinaMessage, useDinaIntl } from "../../../intl/dina-ui-intl";
 const BULK_SPLIT_IDS = "bulk_split_ids";
 
 const ENTITY_LINK = "/collection/material-sample";
+
+interface MaterialSampleBulkSplitFields {
+  numberToCreate: number;
+  seriesOptions: "continue" | "new";
+  seriesGenerationOptions: "lowercase" | "uppercase" | "numeric";
+}
 
 export function MaterialSampleBulkSplitPage() {
   const { formatMessage } = useDinaIntl();
@@ -47,8 +58,14 @@ export function MaterialSampleBulkSplitPage() {
     </>
   );
 
+  const initialValues: MaterialSampleBulkSplitFields = {
+    numberToCreate: 1,
+    seriesOptions: "continue",
+    seriesGenerationOptions: "lowercase"
+  };
+
   return (
-    <DinaForm initialValues={{ numberToCreate: 1 }}>
+    <DinaForm<MaterialSampleBulkSplitFields> initialValues={initialValues}>
       <PageLayout titleId="splitSubsampleTitle" buttonBarContent={buttonBar}>
         <>
           <Card>
@@ -58,17 +75,17 @@ export function MaterialSampleBulkSplitPage() {
           </Card>
 
           <div className="row mt-3">
-            <div className="col-md-6">
+            <div className="col-md-4">
               <NumberSpinnerField
                 name="numberToCreate"
-                max={30}
                 min={1}
+                max={500}
                 label={formatMessage("materialSamplesToCreate")}
               />
             </div>
-            <div className="col-md-6">
+            <div className="col-md-4">
               <RadioButtonsField<string>
-                name="generationOption"
+                name="seriesOptions"
                 horizontalOptions={true}
                 options={[
                   { value: "continue", label: "Continue series" },
@@ -76,16 +93,106 @@ export function MaterialSampleBulkSplitPage() {
                 ]}
               />
             </div>
+            <div className="col-md-4">
+              <FieldSpy fieldName="seriesOptions">
+                {(selected) => (
+                  <SelectField
+                    name="seriesGenerationOptions"
+                    disabled={selected === "continue"}
+                    options={[
+                      { value: "lowercase", label: "a (Lowercase letters)" },
+                      { value: "uppercase", label: "A (Uppercase letters)" },
+                      { value: "numeric", label: "1 (Numerical)" }
+                    ]}
+                  />
+                )}
+              </FieldSpy>
+            </div>
           </div>
 
-          <div className="row mt-4">
-            <h4>
-              <DinaMessage id="previewLabel" />
-            </h4>
-          </div>
+          <PreviewGeneratedNames />
         </>
       </PageLayout>
     </DinaForm>
+  );
+}
+
+function PreviewGeneratedNames() {
+  const { save } = useApiClient();
+  const formik = useFormikContext<MaterialSampleBulkSplitFields>();
+
+  const [generatedIdentifiers, setGeneratedIdentifiers] = useState<string[]>(
+    []
+  );
+
+  // To prevent spamming the network calls, this useEffect has a debounce.
+  useEffect(() => {
+    async function callGenerateIdentifierAPI() {
+      const input: MaterialSampleIdentifierGenerator = {
+        amount: formik.values.numberToCreate,
+        identifier: "CNC-1",
+        type: "material-sample-identifier-generator"
+      };
+
+      const response = await save<MaterialSampleIdentifierGenerator>(
+        [
+          {
+            resource: input,
+            type: "material-sample-identifier-generator"
+          }
+        ],
+        { apiBaseUrl: "/collection-api", overridePatchOperation: true }
+      );
+
+      if (response?.[0]?.nextIdentifiers) {
+        setGeneratedIdentifiers(response?.[0]?.nextIdentifiers);
+      }
+    }
+
+    let timeoutId;
+    const debouncedHandleChange = () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        callGenerateIdentifierAPI();
+      }, 500);
+    };
+    debouncedHandleChange();
+    return () => {
+      clearTimeout(timeoutId);
+    };
+  }, [formik.values]);
+
+  return (
+    <div className="row mt-4">
+      <h4>
+        <DinaMessage id="previewLabel" />
+      </h4>
+      <table className="table">
+        <thead>
+          <tr>
+            <th>Number</th>
+            <th>Generated name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {Array.from(
+            { length: formik.values.numberToCreate },
+            (_, i) => i
+          ).map((_, index) => (
+            <tr key={index + 1}>
+              <td>{index + 1}</td>
+              <td>
+                {generatedIdentifiers[index] ? (
+                  generatedIdentifiers[index]
+                ) : (
+                  <LoadingSpinner loading={true} />
+                )}
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 }
 
