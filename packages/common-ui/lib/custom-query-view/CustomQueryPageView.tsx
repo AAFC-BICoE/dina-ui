67091b35602dd8da49b8d3832d18eb9519399954
Brandon Andre <andr0284@algonquinlive.com>
2023-02-13 11:20:40 -0500

    Support #29934 - Add customized query to display material sample child list
    - Added prop to display options, by default it won't unless options are specified.
    - New interface created for the CustomQueryOptions. Custom Query is passed here.

--- packages/common-ui/lib/custom-query-view/CustomQueryPageView.tsx
+++ packages/common-ui/lib/custom-query-view/CustomQueryPageView.tsx
@@ -2,18 +2,71 @@ import { KitsuResource } from "kitsu";
 import { DinaMessage } from "../../../dina-ui/intl/dina-ui-intl";
 import { FieldSet, QueryPage, QueryPageProps } from "..";
 import { DINAUI_MESSAGES_ENGLISH } from "../../../dina-ui/intl/dina-ui-en";
+import Select from "react-select";
+import { JsonTree } from "react-awesome-query-builder";
+
+export interface CustomQueryOption {
+  /**
+   * Value to be saved when selecting the option.
+   */
+  readonly value: string;
+
+  /**
+   * DINA Label Key to display. Uses the <DinaMessage> component.
+   */
+  readonly label: keyof typeof DINAUI_MESSAGES_ENGLISH;
+
+  /**
+   * Custom query to be applied to the QueryPage if this option is selected.
+   */
+  readonly customQuery: JsonTree | undefined;
+}
 
 export interface CustomQueryPageViewProps<TData extends KitsuResource>
   extends QueryPageProps<TData> {
+  /**
+   * Legend title to be displayed in the card view.
+   */
   titleKey: keyof typeof DINAUI_MESSAGES_ENGLISH;
+
+  /**
+   * If options are provided, a dropdown menu to the right of the legend title will be displayed
+   * to allow the user to choose the query thats being displayed.
+   *
+   * This will overwrite the QueryPageProps customViewQuery.
+   */
+  customQueryOptions?: CustomQueryOption[];
 }
 
 export function CustomQueryPageView<TData extends KitsuResource>({
   titleKey,
+  customQueryOptions,
   ...queryPageProps
 }: CustomQueryPageViewProps<TData>) {
+  // const [customQuerySelected, setCustomQuerySelected] = useState
+
   return (
-    <FieldSet legend={<DinaMessage id={titleKey} />}>
+    <FieldSet
+      legend={<DinaMessage id={titleKey} />}
+      wrapLegend={(legend) => (
+        <div className="row">
+          {customQueryOptions ? (
+            <>
+              <div className="col-sm-8">{legend}</div>
+              <div className="col-sm-4">
+                <Select
+                  className="mt-2"
+                  name="customQueryOptions"
+                  options={customQueryOptions}
+                />
+              </div>
+            </>
+          ) : (
+            <>{legend}</>
+          )}
+        </div>
+      )}
+    >
       <QueryPage<TData> {...queryPageProps} />
     </FieldSet>
   );
