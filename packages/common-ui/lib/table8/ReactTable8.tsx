55990e8775766dd6d6942cc18855941d5ebee960
Chuanbao <chuanbao.lu@gmail.com>
2023-05-13 22:33:55 -0400

    30936 table column sizing

--- packages/common-ui/lib/table8/ReactTable8.tsx
+++ packages/common-ui/lib/table8/ReactTable8.tsx
@@ -1,14 +1,16 @@
 import {
   ColumnDef,
+  Row,
   SortingState,
   flexRender,
   getCoreRowModel,
+  getExpandedRowModel,
   getPaginationRowModel,
   getSortedRowModel,
   useReactTable
 } from "@tanstack/react-table";
 import classnames from "classnames";
-import { useState } from "react";
+import { Fragment, useState } from "react";
 
 import { useIntl } from "react-intl";
 import { v4 as uuidv4 } from "uuid";
@@ -16,6 +18,27 @@ import { Pagination } from "./Pagination";
 import { DefaultRow, DraggableRow } from "./RowComponents";
 
 export const DEFAULT_PAGE_SIZE_OPTIONS = [25, 50, 100, 200, 500];
+
+export interface ReactTable8Props<TData> {
+  // Columns definations, ref: https://tanstack.com/table/v8/docs/api/core/column
+  columns: ColumnDef<TData>[];
+  data: TData[];
+  // When DnD is enabled, need to call setData() after DnD
+  setData?: (data?: TData[]) => void;
+  // Enable row drag and drop
+  enableDnd?: boolean;
+  className?: string;
+  // Show pagination on the bottom
+  showPagination?: boolean;
+  // Show pagination on the top
+  showPaginationTop?: boolean;
+  pageSizeOptions?: number[];
+  defaultSorted?: SortingState;
+  // A function to reander the SubComponent in the expended area.
+  renderSubComponent?: (props: { row: Row<TData> }) => React.ReactElement;
+  // A function that returns true, the the row is extendable
+  getRowCanExpand?: (row: Row<TData>) => boolean;
+}
 
 export function ReactTable8<TData>({
   data,
@@ -26,18 +49,10 @@ export function ReactTable8<TData>({
   showPagination = false,
   showPaginationTop = false,
   pageSizeOptions = DEFAULT_PAGE_SIZE_OPTIONS,
-  defaultSorting
-}: {
-  columns: ColumnDef<TData>[];
-  data: TData[];
-  setData?: (data?: TData[]) => void;
-  enableDnd?: boolean;
-  className?: string;
-  showPagination?: boolean;
-  showPaginationTop?: boolean;
-  pageSizeOptions?: number[];
-  defaultSorting?: SortingState;
-}) {
+  defaultSorted,
+  renderSubComponent,
+  getRowCanExpand
+}: ReactTable8Props<TData>) {
   const { formatMessage } = useIntl();
   const [sorting, setSorting] = useState<SortingState>([]);
 
@@ -51,18 +66,22 @@ export function ReactTable8<TData>({
   const table = useReactTable<TData>({
     data,
     columns,
+    defaultColumn: { minSize: 0, size: 0 },
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
+    getRowCanExpand,
+    getExpandedRowModel:
+      renderSubComponent && getRowCanExpand ? getExpandedRowModel() : undefined,
+    getPaginationRowModel:
+      showPagination || showPaginationTop ? getPaginationRowModel() : undefined,
     state: {
       sorting
     },
     onSortingChange: setSorting,
     getRowId: (row) => ((row as any).id ? (row as any).id : uuidv4()),
-    getPaginationRowModel:
-      showPagination || showPaginationTop ? getPaginationRowModel() : undefined,
     initialState: {
       pagination: { pageSize: pageSizeOptions[0], pageIndex: 0 },
-      sorting: defaultSorting
+      sorting: defaultSorted
     }
   });
 
@@ -84,6 +103,12 @@ export function ReactTable8<TData>({
                     header.column.getIsSorted() === "asc" && "-sort-asc",
                     header.column.getIsSorted() === "desc" && "-sort-desc"
                   )}
+                  style={{
+                    width:
+                      header.column.columnDef.size === 0
+                        ? "auto"
+                        : header.column.columnDef.size
+                  }}
                 >
                   {header.isPlaceholder ? null : (
                     <div
@@ -116,10 +141,9 @@ export function ReactTable8<TData>({
               </td>
             </tr>
           ) : (
-            table
-              .getRowModel()
-              .rows.map((row, index) =>
-                enableDnd ? (
+            table.getRowModel().rows.map((row, index) => (
+              <Fragment key={index}>
+                {enableDnd ? (
                   <DraggableRow
                     row={row}
                     reorderRow={reorderRow}
@@ -130,8 +154,17 @@ export function ReactTable8<TData>({
                     row={row}
                     className={index % 2 === 0 ? "-odd" : "-even"}
                   />
-                )
-              )
+                )}
+                {row.getIsExpanded() && (
+                  <tr>
+                    {/* 2nd row is a custom 1 cell row that contains the extended area */}
+                    <td colSpan={row.getVisibleCells().length}>
+                      {renderSubComponent?.({ row })}
+                    </td>
+                  </tr>
+                )}
+              </Fragment>
+            ))
           )}
         </tbody>
       </table>
