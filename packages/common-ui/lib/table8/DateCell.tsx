1b606a5e7fc759c82acbb9ec74624ff23c60aa63
Chuanbao <chuanbao.lu@gmail.com>
2023-07-24 20:40:06 -0400

    remove old code that use old version of react-table

---
+++ packages/common-ui/lib/table8/DateCell.tsx
@@ -0,0 +1,22 @@
+import { DateView } from "../date/DateView";
+import { get } from "lodash";
+import { TableColumn8 } from "../list-page/types";
+import { FieldHeader } from "../field-header/FieldHeader";
+import { KitsuResource } from "kitsu";
+
+/** Renders a date cell into a table in a user-friendly / readable format. */
+export function dateCell8<TData extends KitsuResource>(
+  label: string,
+  accessorKey?: string
+): TableColumn8<TData> {
+  return {
+    cell: ({ row: { original } }) => {
+      const value = get(original, accessorKey ?? label);
+      return <DateView date={value} />;
+    },
+    header: () => <FieldHeader name={label} />,
+    isKeyword: false,
+    accessorKey: accessorKey ?? label,
+    id: label
+  };
+}
