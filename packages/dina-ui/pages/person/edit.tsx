abbd2a74ccc36216c438f1fa3d2548a1cc75d1e7
Saurabh Vishwakarma <saurabhv.test@outlook.com>
2023-11-10 07:02:32 -0800

    fix the non-compliant page layout of the /person/edit page

--- packages/dina-ui/pages/person/edit.tsx
+++ packages/dina-ui/pages/person/edit.tsx
@@ -1,8 +1,9 @@
-import { BackButton, ButtonBar, useQuery, withResponse } from "common-ui";
+import { BackButton, useQuery, withResponse } from "common-ui";
 import { useRouter } from "next/router";
-import { Footer, Head, Nav, PersonForm } from "../../components";
-import { DinaMessage, useDinaIntl } from "../../intl/dina-ui-intl";
+import { PersonForm } from "../../components";
 import { Person } from "../../types/agent-api/resources/Person";
+import PageLayout from "../../components/page/PageLayout";
+import ButtonBarLayout from "../../components/page/ButtonBarLayout";
 
 export default function PersonEditPage() {
   const router = useRouter();
@@ -10,7 +11,6 @@ export default function PersonEditPage() {
     query: { id }
   } = router;
 
-  const { formatMessage } = useDinaIntl();
   const title = id ? "editPersonTitle" : "addPersonTitle";
 
   async function onSubmitSuccess() {
@@ -18,13 +18,13 @@ export default function PersonEditPage() {
   }
 
   const buttonBar = (
-    <ButtonBar>
+    <ButtonBarLayout>
       <BackButton
         entityId={id as string}
         entityLink="/person"
         byPassView={true}
       />
-    </ButtonBar>
+    </ButtonBarLayout>
   );
 
   const query = useQuery<Person>({
@@ -32,30 +32,15 @@ export default function PersonEditPage() {
   });
 
   return (
-    <div>
-      <Head title={formatMessage(title)} />
-      <Nav />
-      <main className="container-fluid">
-        {buttonBar}
-        {id ? (
-          <div>
-            <h1 id="wb-cont">
-              <DinaMessage id="editPersonTitle" />
-            </h1>
-            {withResponse(query, ({ data }) => (
-              <PersonForm person={data} onSubmitSuccess={onSubmitSuccess} />
-            ))}
-          </div>
-        ) : (
-          <div>
-            <h1 id="wb-cont">
-              <DinaMessage id="addPersonTitle" />
-            </h1>
-            <PersonForm onSubmitSuccess={onSubmitSuccess} />
-          </div>
-        )}
-      </main>
-      <Footer />
-    </div>
+    <PageLayout titleId={title}>
+      {buttonBar}
+      {id ? (
+        withResponse(query, ({ data }) => (
+          <PersonForm person={data} onSubmitSuccess={onSubmitSuccess} />
+        ))
+      ) : (
+        <PersonForm onSubmitSuccess={onSubmitSuccess} />
+      )}
+    </PageLayout>
   );
 }
