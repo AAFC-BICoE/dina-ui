edca12c134a7ac22c81da42e4f7e7f5e4ae12fb8
John Phan <johnphan010@gmail.com>
2024-02-22 14:39:16 -0800

    33085 Workbook upload preview table not skipping empty line - Upload workbook preview table now has horizontal scrollbar - Upload workbook preview table now skips rows that are empty

--- packages/dina-ui/components/workbook/WorkbookDisplay.tsx
+++ packages/dina-ui/components/workbook/WorkbookDisplay.tsx
@@ -27,8 +27,11 @@ export function WorkbookDisplay({
     </div>
   ));
   const dataRows = dataToDisplay.map((row: WorkbookRow, index: number) => {
+    // Skip row if all row content empty
+    const skipRow = row.content.every((content) => !!content === false);
+
     // Skip the first row since it's already been displayed.
-    if (index !== 0) {
+    if (index !== 0 && !skipRow) {
       return row.content.map((col, i) => (
         <div key={i} className="cells_content">
           {col}
@@ -91,9 +94,9 @@ export function WorkbookDisplay({
         `}</style>
       <Card
         className="mb-3"
-        style={{ width: "100%", overflowX: "auto", height: "70hp" }}
+        style={{ width: "100%", overflowX: "scroll", height: "70hp" }}
       >
-        <Card.Header style={{fontSize: "1.4em"}}>
+        <Card.Header style={{ fontSize: "1.4em" }}>
           <DinaMessage id="workbookPreviewTitle" /> ({numOfRows} /{" "}
           {workbookJsonData ? workbookJsonData[sheetIndex].length - 1 : 0})
         </Card.Header>
