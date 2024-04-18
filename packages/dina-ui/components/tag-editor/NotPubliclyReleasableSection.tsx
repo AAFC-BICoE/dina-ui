8e0964abf7cc4b93576157773404190e61b56734
Brandon Andre <brandon.andre@agr.gc.ca>
2024-04-18 11:19:11 -0400

    Support #33418 - Redesign material sample view badges (#1270)
    - Created a new Material Sample Badge component, moved everything into this component.
    - Created CSS for rounding the badges.
    - Rounded project, assemblage and tags.
    - Assemblage badge is now a link to the assemblage.
    - Created collection badge, and removed it from the identifier section.
    - Collection, Project, Assemblage and Tags now have tooltips that indicate the type.
    - Unpluralized the tooltips since you hovering over one item.
    - Rounded the restriction, moved the restriction notes to the same line.
    - Material Sample State warning is now a badge rounded, added tooltip.
    - Added tooltips for the Restriction badges.
    - Cleaned up the margins between the badges.
    - Transaction direction is now rounded, and the tooltip is activated over the whole badge.
    - Moved it beside the white icons.
    - Changed ! icon to use font awesome and filled in.
    - Created a NotPubliclyReleasableSection, instead of sharing it with the tags.
    - Not Publically Releasable is now on it's own side and doesn't cause the dropdowns on the left to be pushed down.
    - Cleaned up the spacing between the dropdowns to take up less space.
    - Added customName prop that was missing, and disabled the ability to deactive the collection in form template like before.
    - Fixed SetDefaultSampleName test, since the collection moved to a different component.
    - Restrictions are now displayed even if the "isRestricted" is off. They are now displayed in yellow instead of red if isRestricted is off.
    - Fixed some positioning issues for the restriction remarks.

---
+++ packages/dina-ui/components/tag-editor/NotPubliclyReleasableSection.tsx
@@ -0,0 +1,45 @@
+import { DinaFormSection, InverseToggleField, RadioButtonsField, TextField, useBulkEditTabContext } from "common-ui";
+import { useFormikContext } from "formik";
+import { DinaMessage } from "../../intl/dina-ui-intl";
+
+export function NotPubliclyReleasableSection() {
+  const isInBulkEditTab = !!useBulkEditTabContext();
+  const formik = useFormikContext<any>();
+
+  return (
+    <>
+      {isInBulkEditTab ? (
+        <RadioButtonsField<boolean | null>
+          name="publiclyReleasable"
+          label={<DinaMessage id="notPubliclyReleasable" />}
+          options={[
+            // null values are ignored when bulk editing:
+            {
+              value: null,
+              label: <DinaMessage id="dontChangeValues" />
+            },
+            // True and false are reversed to show "publiclyReleasable" as "notPubliclyReleasable".
+            { value: false, label: <DinaMessage id="true" /> },
+            { value: true, label: <DinaMessage id="false" /> }
+          ]}
+        />
+      ) : (
+        <InverseToggleField
+          className="notPubliclyReleasable"
+          name="publiclyReleasable"
+          label={<DinaMessage id="notPubliclyReleasable" />}
+        />
+      )}
+      <DinaFormSection horizontal={false}>
+        {formik.values.publiclyReleasable !== undefined &&
+          !formik.values.publiclyReleasable && (
+            <TextField
+              name="notPubliclyReleasableReason"
+              className="flex-grow-1 notPubliclyReleasableReason"
+              multiLines={true}
+            />
+          )}
+      </DinaFormSection>
+    </>
+  )
+}
