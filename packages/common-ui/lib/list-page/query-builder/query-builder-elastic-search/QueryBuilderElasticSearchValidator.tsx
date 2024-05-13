294aa8a65a8078ca0265c5ff4d75b2bb6ee495e5
Brandon Andre <brandon.andre@agr.gc.ca>
2024-05-13 13:59:07 -0400

    Bug #33752 - Validation error reported when using subquery on query builder (#1304)
    - Added the flattenDeep lodash function to support multiple levels for validation checking.
    - Added test cases for subqueries for the validator tests.

--- packages/common-ui/lib/list-page/query-builder/query-builder-elastic-search/QueryBuilderElasticSearchValidator.tsx
+++ packages/common-ui/lib/list-page/query-builder/query-builder-elastic-search/QueryBuilderElasticSearchValidator.tsx
@@ -2,6 +2,7 @@ import { Config, ImmutableTree } from "react-awesome-query-builder";
 import { validateDate } from "../query-builder-value-types/QueryBuilderDateSearch";
 import { validateNumber } from "../query-builder-value-types/QueryBuilderNumberSearch";
 import { validateManagedAttribute } from "../query-builder-value-types/QueryBuilderManagedAttributeSearch";
+import { flattenDeep } from "lodash";
 
 export interface ValidationError {
   /** Error message to display to the user. */
@@ -14,13 +15,13 @@ export interface ValidationError {
 export type ValidationResult = true | ValidationError;
 
 export function isValidationResult(object: any): object is ValidationResult {
-  return 'errorMessage' in object;
+  return "errorMessage" in object;
 }
 
 /**
  * Given a query tree, this function will return all the validation errors present. An empty array
  * if no validation errors exist.
- * 
+ *
  * @param queryTree The current query tree to test for errors.
  * @param config Query builder configuration
  * @param formatMessage Instance of the formatMessage for translation of error messages.
@@ -31,13 +32,17 @@ export function getElasticSearchValidationResults(
   config: Config,
   formatMessage: any
 ): ValidationError[] {
-  const results = elasticSearchFormatValidator(queryTree, config, formatMessage);
+  const results = elasticSearchFormatValidator(
+    queryTree,
+    config,
+    formatMessage
+  );
 
   if (results === true || isValidationResult(results)) {
     return results === true ? [] : [results];
   } else {
     // Filter out "true" values and return only ValidationError objects:
-    const validationErrors = results.filter(
+    const validationErrors = flattenDeep(results).filter(
       (result): result is ValidationError => result !== true
     );
 
@@ -48,9 +53,9 @@ export function getElasticSearchValidationResults(
 
 /**
  * Checks the current queryTree for validation errors (For example, date in wrong format).
- * 
+ *
  * This function will determine if it should be done on a rule or group.
- * 
+ *
  * @param queryTree The current tree that will be validated.
  * @param config Query builder elasticsearch.
  */
@@ -58,8 +63,7 @@ function elasticSearchFormatValidator(
   queryTree: ImmutableTree,
   config: Config,
   formatMessage: any
-) : ValidationResult[] | ValidationResult {
-
+): ValidationResult[] | ValidationResult {
   if (!queryTree) return true;
   const type = queryTree.get("type");
   const properties = queryTree.get("properties") || new Map();
@@ -88,7 +92,7 @@ function elasticSearchFormatValidator(
 
 /**
  * Function for validating a single rule.
- * 
+ *
  * @param fieldName path of the rule being validated.
  * @param value Unformatted rule.
  * @param operator Operator being used (e.g. "equals")
@@ -101,8 +105,7 @@ export function validateEsRule(
   operator: string,
   config: Config,
   formatMessage: any
-) : ValidationResult {
-
+): ValidationResult {
   const widgetName = config.fields?.[fieldName]?.type;
   const widgetConfig = config.widgets[widgetName];
   if (!widgetConfig) return true; // Unable to find the widget.
@@ -125,16 +128,28 @@ export function validateEsRule(
   }
 
   // Retrieve the field name label
-  const fieldLabel = formatMessage({ id: "field_" + config.fields?.[fieldName]?.label ?? fieldName })
+  const fieldLabel = formatMessage({
+    id: "field_" + config.fields?.[fieldName]?.label ?? fieldName
+  });
 
   // For all the different widgets, a validate date function can be setup to do custom validation.
   switch (widgetName) {
     case "number":
-      return validateNumber(fieldLabel, formattedValue, operator, formatMessage);
+      return validateNumber(
+        fieldLabel,
+        formattedValue,
+        operator,
+        formatMessage
+      );
     case "date":
       return validateDate(fieldLabel, formattedValue, operator, formatMessage);
     case "managedAttribute":
-      return validateManagedAttribute(fieldLabel, formattedValue, operator, formatMessage);
+      return validateManagedAttribute(
+        fieldLabel,
+        formattedValue,
+        operator,
+        formatMessage
+      );
   }
 
   return true;
@@ -142,26 +157,27 @@ export function validateEsRule(
 
 /**
  * Logic for validating a group of rules.
- * 
- * @param children 
- * @param conjunction 
- * @param recursiveFunction 
- * @param config 
- * @returns 
+ *
+ * @param children
+ * @param conjunction
+ * @param recursiveFunction
+ * @param config
+ * @returns
  */
 function validateEsGroup(
   children,
   recursiveFunction,
   config: Config,
   formatMessage: any
-) : ValidationResult[] | ValidationResult {
+): ValidationResult[] | ValidationResult {
   // If there is nothing in the group, then nothing needs to be validated.
   if (!children || !children.size) return true;
 
   const childrenArray = children.valueSeq().toArray();
 
-  const result = childrenArray
-    .map((childTree) => recursiveFunction(childTree, config, formatMessage));
+  const result = childrenArray.map((childTree) =>
+    recursiveFunction(childTree, config, formatMessage)
+  );
 
   // If no results, do not add this group to the query.
   if (!result.length) return true;
