361155f790f64388a00db117601838ae4195d3a2
BrandonAndre <brandon.andre@agr.gc.ca>
2024-06-19 14:42:44 -0400

    - Fix import

--- packages/common-ui/lib/list-page/QueryGroupSelection.tsx
+++ packages/common-ui/lib/list-page/QueryGroupSelection.tsx
@@ -1,4 +1,4 @@
-import { GroupSelectField } from "packages/dina-ui/components/group-select/GroupSelectField";
+import { GroupSelectField } from "../../../dina-ui/components/group-select/GroupSelectField";
 import React from "react";
 
 export const GroupSelectFieldMemoized = React.memo(GroupSelectField);
