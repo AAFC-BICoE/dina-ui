2c5df0ba96d43b5d6c2f306de6cfcbaf5e121c4a
John Phan <johnphan010@gmail.com>
2024-07-08 12:52:54 -0700

    34187 Progress - Fix failing tests

--- packages/dina-ui/components/seqdb/pcr-workflow/__tests__/PcrReactionTable.test.tsx
+++ packages/dina-ui/components/seqdb/pcr-workflow/__tests__/PcrReactionTable.test.tsx
@@ -27,14 +27,14 @@ describe("PcrReactionTable component", () => {
     wrapper.find("table tbody tr").forEach((row) => {
       // Well coordinates
       expect(row.find("td").at(0).text()).toEqual(
-        PCR_BATCH_ITEMS?.[rowNumber]?.wellRow +
+        PCR_BATCH_ITEMS?.[rowNumber]?.storageUnitCoordinates?.wellRow +
           "" +
-          PCR_BATCH_ITEMS?.[rowNumber]?.wellColumn
+          PCR_BATCH_ITEMS?.[rowNumber]?.storageUnitCoordinates?.wellColumn
       );
 
       // Tube Number
       expect(row.find("td").at(1).text()).toEqual(
-        "" + PCR_BATCH_ITEMS?.[rowNumber]?.cellNumber
+        "" + PCR_BATCH_ITEMS?.[rowNumber]?.storageUnitCoordinates?.cellNumber
       );
 
       // Primary ID
