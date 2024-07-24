51b5492cfadaabcc320c3fe7db6ae3431f08f300
BrandonAndre <brandon.andre@agr.gc.ca>
2024-07-24 08:43:39 -0400

    Support #34304 - Migrate Enzyme tests to React Testing Library - Part 3
    - Migrated UseElasticSearchDistinctTerm test
    - Working on the QueryConjunctionSwitch tests, switching to another task just want to commit my changes.

--- packages/common-ui/lib/list-page/query-builder/query-builder-core-components/__tests__/QueryConjunctionSwitch.test.tsx
+++ packages/common-ui/lib/list-page/query-builder/query-builder-core-components/__tests__/QueryConjunctionSwitch.test.tsx
@@ -1,41 +1,51 @@
-import { mountWithAppContext } from "common-ui/lib/test-util/mock-app-context";
+import { mountWithAppContext2 } from "common-ui/lib/test-util/mock-app-context";
 import { useState } from "react";
 import { QueryConjunctionSwitch } from "../QueryConjunctionSwitch";
+import { screen } from "@testing-library/react";
 
 describe("QueryConjunctionSwitch component", () => {
   test("Snapshot Test", async () => {
-    const andToggled = mountWithAppContext(
+    const andToggled = mountWithAppContext2(
       <>
         <QueryConjunctionSwitch currentConjunction="AND" />
       </>
     );
-    expect(andToggled.find(QueryConjunctionSwitch).debug()).toMatchSnapshot();
+    expect(andToggled.asFragment()).toMatchSnapshot();
 
-    const orToggled = mountWithAppContext(
+    const orToggled = mountWithAppContext2(
       <>
         <QueryConjunctionSwitch currentConjunction="OR" />
       </>
     );
-    expect(orToggled.find(QueryConjunctionSwitch).debug()).toMatchSnapshot();
+    expect(orToggled.asFragment()).toMatchSnapshot();
   });
 
   test("Toggle between the options", async () => {
+    const conjunctionChangedMock = jest.fn();
+
     function TestToggle() {
       const [conjunction, setConjunction] = useState<string>("AND");
 
       return (
         <QueryConjunctionSwitch
           currentConjunction={conjunction}
-          setConjunction={(newConjunction) => setConjunction(newConjunction)}
+          setConjunction={(newConjunction) => {
+            setConjunction(newConjunction);
+            conjunctionChangedMock(newConjunction);
+          }}
         />
       );
     }
 
-    const toggle = mountWithAppContext(
+    const toggle = mountWithAppContext2(
       <>
         <TestToggle />
       </>
     );
+
+    const buttons = toggle.getAllByRole("button");
+    const andButton = buttons[0];
+    const orButton = buttons[1];
 
     // Expect the initial state.
     expect(
