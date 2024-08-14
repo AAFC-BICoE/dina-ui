e37016d2cd680cfc5e4376b9a0d70a27eab51041
BrandonAndre <brandon.andre@agr.gc.ca>
2024-08-14 11:33:56 -0400

    Support #34363 - MaterialSample List view sends search query 4 times
    - Reverted IdentifierFields temporary changes.
    - Plus and Minus icons are now being displayed and add/remove fields.
    - Moved SelectField and text field to OtherIdentifiersSection part.

--- packages/dina-ui/components/collection/material-sample/OtherIdentifiersSection.tsx
+++ packages/dina-ui/components/collection/material-sample/OtherIdentifiersSection.tsx
@@ -1,19 +1,87 @@
-import { IdentifierFields } from "../../identifier/IdentifierFields";
+import { FieldSet, SelectField, TextField } from "common-ui/lib";
 import useVocabularyOptions from "../useVocabularyOptions";
+import { FieldArray } from "formik";
+import { DinaMessage, useDinaIntl } from "packages/dina-ui/intl/dina-ui-intl";
+import { FaMinus, FaPlus } from "react-icons/fa";
 
 export function OtherIdentifiersSection() {
+  const { formatMessage } = useDinaIntl();
+
   const { vocabOptions } = useVocabularyOptions({
     path: "collection-api/vocabulary2/materialSampleIdentifierType"
   });
 
   return (
-    <IdentifierFields
-      typeOptions={vocabOptions.map((vocab) => ({
-        label: vocab.label,
-        value: vocab.value
-      }))}
-      legendId={"otherIdentifiers"}
-      otherIdentifiersMode={true}
-    />
+    <FieldSet
+      legend={<DinaMessage id={"otherIdentifiers"} />}
+      id="identifierLegend"
+    >
+      <FieldArray name="identifiers">
+        {({ form, push, remove }) => {
+          const identifiers = form.values?.identifiers ?? [];
+
+          // If empty, just display one.
+          if (identifiers.length === 0) {
+            push({});
+          }
+
+          function addIdentifier() {
+            push({});
+          }
+
+          function removeIdentifier(index: number) {
+            remove(index);
+          }
+
+          return (
+            <div className={`identifier-section`}>
+              {identifiers.map((_, index) => (
+                <div className="row" key={index}>
+                  <div className="col-md-5">
+                    <SelectField
+                      name={"identifiersType"}
+                      options={vocabOptions as any}
+                      label={formatMessage("identifierType")}
+                    />
+                  </div>
+                  <div className="col-md-6">
+                    <TextField
+                      name={"identifiersValue"}
+                      label={formatMessage("identifierURI")}
+                    />
+                  </div>
+                  <div className="col-md-1">
+                    <FaPlus
+                      className="ms-1"
+                      onClick={addIdentifier}
+                      size="2em"
+                      onMouseOver={(event) =>
+                        (event.currentTarget.style.color = "blue")
+                      }
+                      onMouseOut={(event) =>
+                        (event.currentTarget.style.color = "")
+                      }
+                      data-testid="add row button"
+                    />
+                    <FaMinus
+                      className="ms-1"
+                      onClick={() => removeIdentifier(index)}
+                      size="2em"
+                      onMouseOver={(event) =>
+                        (event.currentTarget.style.color = "blue")
+                      }
+                      onMouseOut={(event) =>
+                        (event.currentTarget.style.color = "")
+                      }
+                      data-testid="add row button"
+                    />
+                  </div>
+                </div>
+              ))}
+            </div>
+          );
+        }}
+      </FieldArray>
+    </FieldSet>
   );
 }
