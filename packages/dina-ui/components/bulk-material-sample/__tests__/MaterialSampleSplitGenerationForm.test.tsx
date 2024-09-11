fab2c5f2b08e5711ab60a4ece52914a0b7e82aba
BrandonAndre <brandon.andre@agr.gc.ca>
2024-09-11 09:26:39 -0400

    - Converted test over to react-testing and fixed missing mocks.

--- packages/dina-ui/components/bulk-material-sample/__tests__/MaterialSampleSplitGenerationForm.test.tsx
+++ packages/dina-ui/components/bulk-material-sample/__tests__/MaterialSampleSplitGenerationForm.test.tsx
@@ -1,7 +1,7 @@
 import { SEPARATORS_DASH } from "../../../types/collection-api/resources/SplitConfiguration";
-import { mountWithAppContext } from "../../../test-util/mock-app-context";
+import { mountWithAppContext2 } from "../../../test-util/mock-app-context";
 import { MaterialSampleSplitGenerationForm } from "../MaterialSampleSplitGenerationForm";
-import { LoadingSpinner } from "common-ui/lib";
+import { waitForElementToBeRemoved } from "@testing-library/react";
 
 const NO_CHILDREN_MATERIAL_SAMPLE_UUID = "a503d31d-8203-4766-af85-db271e087853";
 const SPLIT_CONFIGURATION_UUID = "706eece4-3105-4e96-bc2f-7530a80a6163";
@@ -29,15 +29,43 @@ const mockBulkGet = jest.fn<any, any>(async (paths: string[]) =>
   })
 );
 
-const mockSave = jest.fn();
+const mockSave = jest.fn((ops) =>
+  ops.map((op) => ({
+    ...op.resource,
+    id: op.resource.id ?? "11111"
+  }))
+);
 
-const apiContext: any = { bulkGet: mockBulkGet, save: mockSave };
+const mockGet = jest.fn(async (path) => {
+  if (path === "collection-api/split-configuration") {
+    return {
+      data: {
+        type: "split-configuration",
+        characterType: "LOWER_LETTER",
+        conditionalOnMaterialSampleTypes: ["WHOLE_ORGANISM"],
+        id: SPLIT_CONFIGURATION_UUID,
+        separator: SEPARATORS_DASH,
+        name: "test-splitconfig",
+        materialSampleTypeCreatedBySplit: "CULTURE_STRAIN",
+        strategy: "DIRECT_PARENT"
+      }
+    };
+  } else {
+    return {};
+  }
+});
+
+const apiContext: any = {
+  bulkGet: mockBulkGet,
+  save: mockSave,
+  apiClient: { get: mockGet }
+};
 
 describe("MaterialSampleSplitGenerationForm", () => {
   beforeEach(() => jest.clearAllMocks);
 
   it("Layout snapshot with matching material sample types.", async () => {
-    const wrapper = mountWithAppContext(
+    const wrapper = mountWithAppContext2(
       <MaterialSampleSplitGenerationForm
         onGenerate={mockOnGenerate}
         ids={[NO_CHILDREN_MATERIAL_SAMPLE_UUID]}
@@ -56,19 +84,17 @@ describe("MaterialSampleSplitGenerationForm", () => {
       { apiContext }
     );
 
-    while (wrapper.find(LoadingSpinner).exists()) {
-      await new Promise((resolve) => setTimeout(resolve, 100));
-      wrapper.update();
-    }
+    // Wait for the material sample request.
+    await waitForElementToBeRemoved(wrapper.getByText(/loading\.\.\./i));
 
     // This test is designed to simply ensure the design doesn't change when it's not expected.
     // If it's expected, the snapshot will need to be updated.
-    expect(wrapper.debug()).toMatchSnapshot();
+    expect(wrapper.asFragment()).toMatchSnapshot();
     wrapper.unmount();
   });
 
   it("Layout snapshot without matching material sample types.", async () => {
-    const wrapper = mountWithAppContext(
+    const wrapper = mountWithAppContext2(
       <MaterialSampleSplitGenerationForm
         onGenerate={mockOnGenerate}
         ids={[NO_CHILDREN_MATERIAL_SAMPLE_UUID]}
@@ -88,14 +114,11 @@ describe("MaterialSampleSplitGenerationForm", () => {
     );
 
     // Wait for the material sample request.
-    while (wrapper.find(LoadingSpinner).exists()) {
-      await new Promise((resolve) => setTimeout(resolve, 100));
-      wrapper.update();
-    }
+    await waitForElementToBeRemoved(wrapper.getByText(/loading\.\.\./i));
 
     // This test is designed to simply ensure the design doesn't change when it's not expected.
     // If it's expected, the snapshot will need to be updated.
-    expect(wrapper.debug()).toMatchSnapshot();
+    expect(wrapper.asFragment()).toMatchSnapshot();
     wrapper.unmount();
   });
 });
