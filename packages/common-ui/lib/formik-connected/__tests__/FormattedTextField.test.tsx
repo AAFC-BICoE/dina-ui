bf768c2bfac66787cacc5d2c9514d030bea25556
John Phan <johnphan010@gmail.com>
2024-09-18 10:30:07 -0700

    34303 Progress - Migrated FormattedTextField.test.tsx

--- packages/common-ui/lib/formik-connected/__tests__/FormattedTextField.test.tsx
+++ packages/common-ui/lib/formik-connected/__tests__/FormattedTextField.test.tsx
@@ -1,21 +1,29 @@
-import { mountWithAppContext } from "../../test-util/mock-app-context";
+import {
+  mountWithAppContext,
+  mountWithAppContext2
+} from "../../test-util/mock-app-context";
 import { DinaForm } from "../DinaForm";
 import { FormattedTextField } from "../FormattedTextField";
+import { fireEvent, screen, waitFor } from "@testing-library/react";
+import "@testing-library/jest-dom";
 
 describe("FormattedTextField component", () => {
   it("Displays the field's label and value.", () => {
-    const wrapper = mountWithAppContext(
+    const wrapper = mountWithAppContext2(
       <DinaForm initialValues={{ testObject: { testField: "2020" } }}>
         <FormattedTextField name="testObject.testField" />
       </DinaForm>
     );
-
-    expect(wrapper.find("label").text()).toEqual("Test Object Test Field");
-    expect((wrapper.find("input").instance() as any).value).toEqual("2020");
+    expect(wrapper.container.querySelector("label")?.textContent).toEqual(
+      "Test Object Test Field"
+    );
+    expect(
+      wrapper.container.querySelector("input")?.getAttribute("value")
+    ).toEqual("2020");
   });
 
   it("Changes the field's value.", () => {
-    const wrapper = mountWithAppContext(
+    const wrapper = mountWithAppContext2(
       <DinaForm initialValues={{ testObject: { testField: "2020" } }}>
         {({
           values: {
@@ -24,22 +32,33 @@ describe("FormattedTextField component", () => {
         }) => (
           <>
             <FormattedTextField name="testObject.testField" />
-            <div id="value-display">{testField}</div>
+            <div className="value-display">{testField}</div>
           </>
         )}
       </DinaForm>
     );
 
-    wrapper.find("input").simulate("change", {
-      target: { name: "testObject.testField", value: "201912" }
-    });
+    // Simulate changing the input value to "201912"
+    fireEvent.change(
+      screen.getByRole("textbox", {
+        name: /test object test field/i
+      }),
+      { target: { value: "201912" } }
+    );
 
-    expect(wrapper.find("#value-display").text()).toEqual("2019-12");
+    // Check if the displayed value is updated to "2019-12"
+    expect(
+      wrapper.container.querySelector(".value-display")?.textContent
+    ).toEqual("2019-12");
 
-    wrapper.find("input").simulate("change", {
-      target: { name: "testObject.testField", value: "2019we" }
-    });
-
-    expect(wrapper.find("#value-display").text()).toEqual("2019");
+    fireEvent.change(
+      screen.getByRole("textbox", {
+        name: /test object test field/i
+      }),
+      { target: { value: "2019we" } }
+    );
+    expect(
+      wrapper.container.querySelector(".value-display")?.textContent
+    ).toEqual("2019");
   });
 });
