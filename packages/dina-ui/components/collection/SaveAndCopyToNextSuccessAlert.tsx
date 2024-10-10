e5153004f90656dcc49f2cc13301eb29ae7eb270
BrandonAndre <brandon.andre@agr.gc.ca>
2024-10-10 15:54:40 -0400

    Bug #34961 - Material Sample Save and Copy to Next with Storage Unit Issue
    - Created an alert that will display if a data-component was not copied over, and if they would like to duplicate it anyway.
    - Currently working on the data structure to store which data-components should be copied over automatically.

--- packages/dina-ui/components/collection/SaveAndCopyToNextSuccessAlert.tsx
+++ packages/dina-ui/components/collection/SaveAndCopyToNextSuccessAlert.tsx
@@ -1,6 +1,8 @@
 import { useDinaIntl } from "../../../dina-ui/intl/dina-ui-intl";
 import { BsFillFileEarmarkCheckFill } from "react-icons/bs";
 import Link from "next/link";
+import { FaInfoCircle } from "react-icons/fa";
+import Button from "react-bootstrap/Button";
 
 export interface SaveAndCopyToNextSuccessAlertProps {
   id: string;
@@ -17,26 +19,46 @@ export function SaveAndCopyToNextSuccessAlert({
 }: SaveAndCopyToNextSuccessAlertProps) {
   const { formatMessage } = useDinaIntl();
   return (
-    <div className="alert alert-success">
-      <div className=" d-flex gap-3">
-        <BsFillFileEarmarkCheckFill style={{ width: "24px", height: "24px" }} />
-        <Link
-          href={`/${entityPath}/view?id=${id}`}
-          className={className}
-          passHref={true}
-        >
-          <a>
-            <span style={{ fontSize: "1.2em", font: "fw-bold" }}>
-              {" "}
-              {displayName}
-            </span>
-          </a>
-        </Link>
+    <>
+      <div className="alert alert-success">
+        <div className="d-flex gap-2">
+          <div className="d-flex gap-3">
+            <BsFillFileEarmarkCheckFill
+              style={{ width: "24px", height: "24px" }}
+            />
+            <Link
+              href={`/${entityPath}/view?id=${id}`}
+              className={className}
+              passHref={true}
+            >
+              <a>
+                <span style={{ fontSize: "1.2em", font: "fw-bold" }}>
+                  {" "}
+                  {displayName}
+                </span>
+              </a>
+            </Link>
+          </div>
 
-        <span style={{ fontSize: "1.2em", font: "fw-bold" }}>
-          {formatMessage("created")}{" "}
-        </span>
+          <span style={{ fontSize: "1.2em", font: "fw-bold" }}>
+            {formatMessage("created")}{" "}
+          </span>
+        </div>
       </div>
-    </div>
+      <div className="alert alert-info">
+        <div className="d-flex gap-3">
+          <FaInfoCircle style={{ width: "24px", height: "24px" }} />
+          <span style={{ fontSize: "1.2em", font: "fw-bold" }}>
+            The <strong>Storage</strong> data component was not automatically
+            copied over since it's specific to the previous Material Sample.
+            Would you like to duplicate it anyway?
+          </span>
+        </div>
+
+        <Button className="mt-3" variant="secondary">
+          Duplicate Storage from "{displayName}"
+        </Button>
+      </div>
+    </>
   );
 }
