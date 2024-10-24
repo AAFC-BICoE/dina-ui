61d1a52e08400b0ced143d06ff2e80a86c3c303b
BrandonAndre <brandon.andre@agr.gc.ca>
2024-10-24 13:56:34 -0400

    Feature #34643 - Generate and download spreadsheet templates for import
    - Started working on generating the network request.
    - Added error handling if the request fails.
    - Added the submit button
    - Other layout improvements.

--- packages/dina-ui/pages/workbook/generator.tsx
+++ packages/dina-ui/pages/workbook/generator.tsx
@@ -4,10 +4,12 @@ import {
   BackButton,
   ColumnSelectorMemo,
   DinaForm,
-  FieldWrapper
+  FieldWrapper,
+  SubmitButton,
+  useApiClient
 } from "common-ui";
 import { DinaMessage, useDinaIntl } from "../../intl/dina-ui-intl";
-import { Card } from "react-bootstrap";
+import { Alert, Card, Spinner } from "react-bootstrap";
 import Select from "react-select";
 import { useState } from "react";
 import {
@@ -17,6 +19,7 @@ import {
 import { dynamicFieldMappingForMaterialSample } from "../collection/material-sample/list";
 import { useIndexMapping } from "common-ui/lib/list-page/useIndexMapping";
 import { KitsuResource } from "kitsu";
+import { WorkbookGeneration } from "packages/dina-ui/types/dina-export-api/resources/WorkbookGeneration";
 
 export interface EntityConfiguration {
   name: string;
@@ -37,9 +40,13 @@ const ENTITY_TYPES: EntityConfiguration[] = [
 
 export function WorkbookTemplateGenerator<TData extends KitsuResource>() {
   const { formatMessage } = useDinaIntl();
+  const { apiClient } = useApiClient();
 
   // Loading state
   const [loading, setLoading] = useState<boolean>(false);
+
+  // Generator errors
+  const [errorMessage, setErrorMessage] = useState<string>();
 
   // Entity to be generated (e.g. material-sample)
   const [type, setType] = useState<EntityConfiguration>(ENTITY_TYPES[0]);
@@ -61,12 +68,85 @@ export function WorkbookTemplateGenerator<TData extends KitsuResource>() {
     dynamicFieldMapping: type.dynamicConfig
   });
 
+  async function generateTemplate() {
+    setLoading(true);
+    setErrorMessage(undefined);
+
+    const generateTemplateArg: WorkbookGeneration = {
+      columns: columnsToGenerate.map((col) => col.id),
+      aliases: columnsToGenerate.map((col) =>
+        col?.exportHeader ? col.exportHeader : col.id
+      )
+    } as WorkbookGeneration;
+
+    try {
+      const workbookGenerationPostResponse = await apiClient.axios.post(
+        "objectstore-api/workbook/generation",
+        generateTemplateArg
+      );
+    } catch (error) {
+      // Log the error for debugging
+      console.error("Error generating workbook template:", error);
+
+      // Extract a user-friendly error message:
+      let userFriendlyErrorMessage =
+        "An error occurred while generating the workbook template. Please try again later.";
+      if (error.response) {
+        // If a response object is available, extract the status code and message
+        userFriendlyErrorMessage = `Error ${error.response.status}: ${
+          error.response.data.message || "Unknown error"
+        }`;
+      }
+
+      // Set the user-friendly error message for display
+      setErrorMessage(userFriendlyErrorMessage);
+    }
+
+    setLoading(false);
+  }
+
+  const LoadingSpinner = (
+    <>
+      <Spinner
+        as="span"
+        animation="border"
+        size="sm"
+        role="status"
+        aria-hidden="true"
+      />
+      <span className="visually-hidden">
+        <DinaMessage id="loadingSpinner" />
+      </span>
+    </>
+  );
+
   return (
-    <PageLayout
-      titleId="workbookGenerateTemplateTitle"
-      buttonBarContent={<div className="col-md-6 col-sm-12 mt-2" />}
-    >
-      <DinaForm initialValues={{}}>
+    <DinaForm initialValues={{}}>
+      <PageLayout
+        titleId="workbookGenerateTemplateTitle"
+        buttonBarContent={
+          <>
+            <div className="col-md-6 col-sm-12 mt-2" />
+            <div className="col-md-6 col-sm-12 d-flex">
+              <SubmitButton
+                className="ms-auto"
+                buttonProps={() => ({
+                  style: { width: "12rem" },
+                  disabled: loading || columnsToGenerate.length === 0,
+                  onClick: () => generateTemplate()
+                })}
+              >
+                {loading ? (
+                  LoadingSpinner
+                ) : (
+                  <DinaMessage id="generateButtonText" />
+                )}
+              </SubmitButton>
+            </div>
+          </>
+        }
+      >
+        {errorMessage && <Alert variant="danger">{errorMessage}</Alert>}
         <h4 className="mt-4">
           <DinaMessage id="settingLabel" />
         </h4>
@@ -93,7 +173,6 @@ export function WorkbookTemplateGenerator<TData extends KitsuResource>() {
               </FieldWrapper>
             </div>
           </Card.Body>
-          <Card.Footer />
         </Card>
         <h4 className="mt-4">
           <DinaMessage id="templateGenerator_columnsToGenerate" />
@@ -111,8 +190,8 @@ export function WorkbookTemplateGenerator<TData extends KitsuResource>() {
             />
           </Card.Body>
         </Card>
-      </DinaForm>
-    </PageLayout>
+      </PageLayout>
+    </DinaForm>
   );
 }
 
