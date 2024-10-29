2fff01c1cdef4555ef9f7e01b42c96f153d205c0
Brandon Andre <brandon.andre@agr.gc.ca>
2024-10-29 15:48:20 -0400

    Support #31660 - Allow ui to run without keycloak (#1167)
    * Support #31660 - Allow ui to run without keycloak
    
    - Updated caddy file to make a dev-user response to allow the front end to access these variables. (DEV_USER_ENABLED, DEV_USER_GROUP_ROLE)
    - For now I am just doing it as a comma separated string for the environment variable since it's difficult to parse in Caddyfile.
    - AuthenticatorProvider will get the dev-user config using the caddy response generated.
    - Setup the AccountProvider for this dev-user - still in progress...
    
    * - Found issue why the JSON couldn't be parsed, had a comma at the end. Fixed the caddy responses.
    
    * - Added parsing function for the groupRole string
    - Added test coverage for the function.
    - Fixed logic for displaying the loading indicator.
    
    * Fixed build issue
    
    * clean up
    
    * Support #31660 - Allow UI to run without keycloak
    
    - Added KEYCLOAK_ENABLED to the caddy files.
    - Added check to ensure that KEYCLOAK_ENABLED is false and DEV_USER_ENABLED is true in order to use dev user mode. (To prevent accidentally enabling it)
    - Added an error message dev mode is enabled but keycloak is still enabled.
    
    * Support #31660 - Allow UI to run without keycloak
    
    - Created DevUserProvider
    - Reverted the AccountProvider changes.
    - Added DevUserProvider test for the parseGroupRoleDevUser function testing.
    - Removed dev-user endpoint from the prod Caddyfile.
    
    * Support #31660 - Allow UI to run without keycloak
    
    - KeycloakAccountProvider is skipped if the context is already created (dev user)
    - Fixed logic error.
    
    * - Found issue with missing context dependency, much more stable now.
    
    * - Improved stability of the columnSelector generator.
    
    * Support #31660 - Allow UI to run without keycloak
    
    - Fixed issue with accessorKeys being generated incorrectly for longer paths.
    - Updated tests.
    
    * oops
    
    * oops, revert changes unrelated to this ticket

--- packages/common-ui/lib/account/__tests__/AccountProvider.test.tsx
+++ packages/common-ui/lib/account/__tests__/AccountProvider.test.tsx
@@ -4,33 +4,35 @@ import {
   generateKeycloakRolesPerGroup
 } from "../AccountProvider";
 
-describe("AccountProvider", () => {
-  // Keycloak test data
-  const keycloakGroups = [
-    "/cnc/" + DINA_ADMIN,
-    "/cnc/" + USER,
-    "/aafc/" + USER,
-    "/othergroup",
-    "no-leading-slash"
-  ];
-
-  it("Converts Keycloak's group names to bare group names.", () => {
-    const bareGroupNames = keycloakGroupNamesToBareGroupNames(keycloakGroups);
-
-    expect(bareGroupNames).toEqual([
-      "cnc",
-      "aafc",
-      "othergroup",
+describe("AccountProvider component", () => {
+  describe("keycloakGroupNamesToBareGroupNames function", () => {
+    // Keycloak test data
+    const keycloakGroups = [
+      "/cnc/" + DINA_ADMIN,
+      "/cnc/" + USER,
+      "/aafc/" + USER,
+      "/othergroup",
       "no-leading-slash"
-    ]);
-  });
+    ];
 
-  it("Converts keycloak group + role name string into a unique key and values.", () => {
-    const rolesPerGroup = generateKeycloakRolesPerGroup(keycloakGroups);
+    it("Converts Keycloak's group names to bare group names.", () => {
+      const bareGroupNames = keycloakGroupNamesToBareGroupNames(keycloakGroups);
 
-    expect(rolesPerGroup).toEqual({
-      aafc: [USER],
-      cnc: [DINA_ADMIN, USER]
+      expect(bareGroupNames).toEqual([
+        "cnc",
+        "aafc",
+        "othergroup",
+        "no-leading-slash"
+      ]);
+    });
+
+    it("Converts keycloak group + role name string into a unique key and values.", () => {
+      const rolesPerGroup = generateKeycloakRolesPerGroup(keycloakGroups);
+
+      expect(rolesPerGroup).toEqual({
+        aafc: [USER],
+        cnc: [DINA_ADMIN, USER]
+      });
     });
   });
 });
