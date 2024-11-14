83f1d0c56aff2b3d78e8f2c5a63321943223d982
BrandonAndre <brandon.andre@agr.gc.ca>
2024-11-14 16:00:51 -0500

    - Added homepage link.

--- packages/dina-ui/pages/index.tsx
+++ packages/dina-ui/pages/index.tsx
@@ -216,6 +216,11 @@ export function Home() {
                     <SeqdbMessage id="libraryPrepBatchListTitle" />
                   </a>
                 </Link>
+                <Link href="/seqdb/molecular-analysis-run/list">
+                  <a>
+                    <SeqdbMessage id="molecularAnalysisRunListTitle" />
+                  </a>
+                </Link>
                 <Link href="/seqdb/ngs-workflow/list" passHref={true}>
                   <a>
                     <SeqdbMessage id="ngsWorkflowWholeGenomeSeqTitle" />
