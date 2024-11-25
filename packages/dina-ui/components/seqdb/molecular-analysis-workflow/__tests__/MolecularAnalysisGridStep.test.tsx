f456513d0e8ffec4f49287225fa379d32f74db74
BrandonAndre <brandon.andre@agr.gc.ca>
2024-11-25 15:03:12 -0500

    Support #35239 - Molecular analysis workflow improvements
    - Finished adding test coverage for step 3. Test coverage complete.

--- packages/dina-ui/components/seqdb/molecular-analysis-workflow/__tests__/MolecularAnalysisGridStep.test.tsx
+++ packages/dina-ui/components/seqdb/molecular-analysis-workflow/__tests__/MolecularAnalysisGridStep.test.tsx
@@ -23,6 +23,7 @@ import userEvent from "@testing-library/user-event";
 
 const onSavedMock = jest.fn();
 const mockSetEditMode = jest.fn();
+const mockSave = jest.fn();
 
 const mockGet = jest.fn<any, any>(async (path, params) => {
   switch (path) {
@@ -84,8 +85,8 @@ const testCtx = {
         get: mockGet
       }
     },
-    bulkGet: mockBulkGet
-    // save: mockSave
+    bulkGet: mockBulkGet,
+    save: mockSave
   }
 } as any;
 
@@ -217,5 +218,200 @@ describe("Molecular Analysis Workflow - Step 3 - Molecular Analysis Coordinate S
         /no coordinates have been saved yet, click "edit" to begin adding coordinates\./i
       )
     ).toBeInTheDocument();
+  });
+
+  it("Storage units don't exist, add storage coordinates for all material samples", async () => {
+    const wrapper = mountWithAppContext2(
+      <TestComponentWrapper
+        molecularAnalysisId={TEST_MOLECULAR_ANALYSIS_WITHOUT_STORAGE_ID}
+      />,
+      testCtx
+    );
+
+    // Wait for loading to be finished.
+    await waitForElementToBeRemoved(wrapper.getByText(/loading\.\.\./i));
+
+    // Should not be in edit mode since storage units exist.
+    expect(wrapper.getByText(/edit mode: true/i)).toBeInTheDocument();
+
+    // Change the dropdowns.
+    userEvent.click(wrapper.getByRole("combobox"));
+    userEvent.click(
+      wrapper.getByRole("option", { name: /test storage unit type 1/i })
+    );
+    await new Promise(setImmediate);
+
+    userEvent.click(wrapper.getAllByRole("combobox")[1]);
+    userEvent.click(
+      wrapper.getAllByRole("option", { name: /storage unit name/i })[0]
+    );
+
+    // 3 material samples should appear in the list, not in the grid yet.
+    expect(
+      wrapper.getByText(/selected material samples \(3 in list\)/i)
+    ).toBeInTheDocument();
+
+    // Fill by row, so it should have automatically selected the row option.
+    expect(wrapper.getByRole("radio", { name: /row/i })).toHaveAttribute(
+      "checked"
+    );
+
+    // Click the move all button.
+    userEvent.click(wrapper.getByRole("button", { name: /move all/i }));
+
+    // Save the new coordinates.
+    userEvent.click(wrapper.getByRole("button", { name: /save selections/i }));
+    await new Promise(setImmediate);
+
+    // Expect the 3 API calls for the storage-unit-usages.
+    expect(mockSave.mock.calls).toEqual([
+      [
+        [
+          {
+            resource: {
+              id: undefined,
+              storageUnit: {
+                id: "6f5f6d1c-69cc-49b1-b3ae-1675c18ef5b5",
+                type: "storage-unit"
+              },
+              type: "storage-unit-usage",
+              usageType: "generic-molecular-analysis-item",
+              wellColumn: 1,
+              wellRow: "A"
+            },
+            type: "storage-unit-usage"
+          },
+          {
+            resource: {
+              id: undefined,
+              storageUnit: {
+                id: "6f5f6d1c-69cc-49b1-b3ae-1675c18ef5b5",
+                type: "storage-unit"
+              },
+              type: "storage-unit-usage",
+              usageType: "generic-molecular-analysis-item",
+              wellColumn: 2,
+              wellRow: "A"
+            },
+            type: "storage-unit-usage"
+          },
+          {
+            resource: {
+              id: undefined,
+              storageUnit: {
+                id: "6f5f6d1c-69cc-49b1-b3ae-1675c18ef5b5",
+                type: "storage-unit"
+              },
+              type: "storage-unit-usage",
+              usageType: "generic-molecular-analysis-item",
+              wellColumn: 3,
+              wellRow: "A"
+            },
+            type: "storage-unit-usage"
+          }
+        ],
+        {
+          apiBaseUrl: "/collection-api"
+        }
+      ]
+    ]);
+  });
+
+  it("Storage units exist, remove existing storage unit usage", async () => {
+    const wrapper = mountWithAppContext2(<TestComponentWrapper />, testCtx);
+
+    // Wait for loading to be finished.
+    await waitForElementToBeRemoved(wrapper.getByText(/loading\.\.\./i));
+
+    // All the material sample should be on the grid, not the list on the sidebar:
+    expect(
+      wrapper.getByText(/selected material samples \(0 in list\)/i)
+    ).toBeInTheDocument();
+
+    // Switch to edit mode.
+    userEvent.click(wrapper.getByRole("button", { name: /edit/i }));
+
+    // Clear the grid.
+    userEvent.click(wrapper.getByRole("button", { name: /clear grid/i }));
+    expect(
+      wrapper.getByText(/selected material samples \(3 in list\)/i)
+    ).toBeInTheDocument();
+
+    // Save, should delete the 3 storage unit usages.
+    userEvent.click(wrapper.getByRole("button", { name: /save selections/i }));
+    await new Promise(setImmediate);
+
+    // Update each generic-molecular-analysis-item to remove the storage unit usage relationship.
+    // Then delete the storage unit usages.
+    expect(mockSave.mock.calls).toEqual([
+      [
+        [
+          {
+            resource: {
+              id: "99ecc6fc-7378-4641-8914-1b9104e37b95",
+              relationships: {
+                storageUnitUsage: {
+                  data: null
+                }
+              },
+              type: "generic-molecular-analysis-item"
+            },
+            type: "generic-molecular-analysis-item"
+          },
+          {
+            resource: {
+              id: "169eafe4-44f2-407e-aa90-1a5483edf522",
+              relationships: {
+                storageUnitUsage: {
+                  data: null
+                }
+              },
+              type: "generic-molecular-analysis-item"
+            },
+            type: "generic-molecular-analysis-item"
+          },
+          {
+            resource: {
+              id: "9df16fe8-8510-4723-8f88-0a6bc0536624",
+              relationships: {
+                storageUnitUsage: {
+                  data: null
+                }
+              },
+              type: "generic-molecular-analysis-item"
+            },
+            type: "generic-molecular-analysis-item"
+          }
+        ],
+        {
+          apiBaseUrl: "/seqdb-api"
+        }
+      ],
+      [
+        [
+          {
+            delete: {
+              id: "45ed6126-26b8-4ebd-a89f-1bbcf6c69d27",
+              type: "storage-unit-usage"
+            }
+          },
+          {
+            delete: {
+              id: "be81e29a-b634-43c7-8f1a-53bf394d87f2",
+              type: "storage-unit-usage"
+            }
+          },
+          {
+            delete: {
+              id: "0192fd01-9104-72fa-a18f-80d97da0c935",
+              type: "storage-unit-usage"
+            }
+          }
+        ],
+        {
+          apiBaseUrl: "/collection-api"
+        }
+      ]
+    ]);
   });
 });
