af2eaedf2aa8d2f92f7b600fbb2f8ce80e63c524
Jan Schmidt <jschmidt@infolytics.com>
2024-12-02 13:29:09 +0100

    Fix tests

--- packages/common-ui/lib/test-util/mock-app-context.tsx
+++ packages/common-ui/lib/test-util/mock-app-context.tsx
@@ -63,7 +63,8 @@ export function MockAppContextProvider({
     () => ({
       supportedLanguages: "en,fr",
       instanceMode: "developer",
-      instanceName: "AAFC"
+      instanceName: "AAFC",
+      supportedGeographicReferences: "OSM"
     }),
     []
   );
