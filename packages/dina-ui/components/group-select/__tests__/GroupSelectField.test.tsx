3e5fb2159bbd11de7b0207566a960e8addf18f49
BrandonAndre <brandon.andre@agr.gc.ca>
2024-12-05 09:24:59 -0500

    Support #35243 - Migrate Enzyme tests to react testing library (Part 10)
    - Converted GroupSelectField tests.

--- packages/dina-ui/components/group-select/__tests__/GroupSelectField.test.tsx
+++ packages/dina-ui/components/group-select/__tests__/GroupSelectField.test.tsx
@@ -1,9 +1,10 @@
-import { DinaForm, SelectField } from "common-ui";
-import { mountWithAppContext } from "../../../test-util/mock-app-context";
+import { DinaForm, SubmitButton } from "common-ui";
+import { mountWithAppContext2 } from "../../../test-util/mock-app-context";
 import { GroupSelectField } from "../GroupSelectField";
 import { deleteFromStorage, writeStorage } from "@rehooks/local-storage";
 import { DEFAULT_GROUP_STORAGE_KEY } from "../useStoredDefaultGroup";
-import Select from "react-select";
+import userEvent from "@testing-library/user-event";
+import "@testing-library/jest-dom";
 
 const mockSubmit = jest.fn();
 
@@ -15,31 +16,29 @@ const testCtx = {
 
 describe("GroupSelectField component", () => {
   // Clear the local storage:
-  beforeEach(() => deleteFromStorage(DEFAULT_GROUP_STORAGE_KEY));
-  beforeEach(jest.clearAllMocks);
+  beforeEach(() => {
+    jest.clearAllMocks();
+    deleteFromStorage(DEFAULT_GROUP_STORAGE_KEY);
+  });
   afterEach(() => deleteFromStorage(DEFAULT_GROUP_STORAGE_KEY));
 
   it("Renders the default group list without accessing the user API.", async () => {
-    const wrapper = mountWithAppContext(
+    const wrapper = mountWithAppContext2(
       <DinaForm initialValues={{}}>
         <GroupSelectField name="group" />
       </DinaForm>,
       testCtx
     );
-
     await new Promise(setImmediate);
-    wrapper.update();
 
-    expect(wrapper.find(SelectField).prop("options")).toEqual([
-      {
-        label: "aafc",
-        value: "aafc"
-      },
-      {
-        label: "cnc",
-        value: "cnc"
-      }
-    ]);
+    // Click the dropdown to show options.
+    userEvent.click(
+      wrapper.getByRole("combobox", { name: /group select\.\.\./i })
+    );
+
+    // "aafc" and "cnc" should appear as options.
+    expect(wrapper.getByRole("option", { name: /aafc/i })).toBeInTheDocument();
+    expect(wrapper.getByRole("option", { name: /cnc/i })).toBeInTheDocument();
   });
 
   it("Renders the retrieved group list from the user API.", async () => {
@@ -56,7 +55,7 @@ describe("GroupSelectField component", () => {
       ]
     }));
 
-    const wrapper = mountWithAppContext(
+    const wrapper = mountWithAppContext2(
       <DinaForm initialValues={{}}>
         <GroupSelectField name="group" />
       </DinaForm>,
@@ -66,26 +65,22 @@ describe("GroupSelectField component", () => {
         }
       }
     );
-
     await new Promise(setImmediate);
-    wrapper.update();
 
-    expect(wrapper.find(SelectField).prop("options")).toEqual([
-      {
-        label: "AAFC", // uses englsh label.
-        value: "aafc"
-      },
-      {
-        label: "cnc",
-        value: "cnc" // no english label available ; default to the group name.
-      }
-    ]);
+    // Click the dropdown to show options.
+    userEvent.click(
+      wrapper.getByRole("combobox", { name: /group select\.\.\./i })
+    );
+
+    // "aafc" and "cnc" should appear as options. AAFC is using the english label.
+    expect(wrapper.getByRole("option", { name: "AAFC" })).toBeInTheDocument();
+    expect(wrapper.getByRole("option", { name: "cnc" })).toBeInTheDocument();
   });
 
   it("By default doesn't set the default group from local storage.", async () => {
     writeStorage(DEFAULT_GROUP_STORAGE_KEY, "cnc");
 
-    const wrapper = mountWithAppContext(
+    const wrapper = mountWithAppContext2(
       <DinaForm initialValues={{}}>
         <GroupSelectField name="group" />
       </DinaForm>,
@@ -93,70 +88,58 @@ describe("GroupSelectField component", () => {
     );
 
     await new Promise(setImmediate);
-    wrapper.update();
 
-    expect(wrapper.find(Select).prop("value")).toEqual(null);
+    expect(wrapper.getByText(/select\.\.\./i)).toBeInTheDocument();
   });
 
   it("Sets the default group from local storage when this feature is enabled.", async () => {
     writeStorage(DEFAULT_GROUP_STORAGE_KEY, "cnc");
 
-    const wrapper = mountWithAppContext(
+    const wrapper = mountWithAppContext2(
       <DinaForm initialValues={{}}>
         <GroupSelectField name="group" enableStoredDefaultGroup={true} />
       </DinaForm>,
       testCtx
     );
-
     await new Promise(setImmediate);
-    wrapper.update();
 
-    expect(wrapper.find(Select).prop("value")).toEqual({
-      label: "cnc",
-      value: "cnc"
-    });
+    expect(wrapper.getByText(/cnc/i)).toBeInTheDocument();
   });
 
   it("Doesn't set the default group if a group is passed using initialValues.", async () => {
     writeStorage(DEFAULT_GROUP_STORAGE_KEY, "cnc");
 
-    const wrapper = mountWithAppContext(
+    const wrapper = mountWithAppContext2(
       <DinaForm initialValues={{ group: "aafc" }}>
         <GroupSelectField name="group" enableStoredDefaultGroup={true} />
       </DinaForm>,
       // User has only one group:
       { ...testCtx, accountContext: { groupNames: ["cnc"] } }
     );
-
     await new Promise(setImmediate);
-    wrapper.update();
 
-    expect(wrapper.find(Select).prop("value")).toEqual({
-      label: "aafc",
-      value: "aafc"
-    });
+    expect(wrapper.getByText(/aafc/i)).toBeInTheDocument();
   });
 
   it("Hides the field and sets the only option when the default option is the only option.", async () => {
-    const wrapper = mountWithAppContext(
+    const wrapper = mountWithAppContext2(
       <DinaForm
         initialValues={{}}
         onSubmit={({ submittedValues }) => mockSubmit(submittedValues)}
       >
         <GroupSelectField name="group" enableStoredDefaultGroup={true} />
+        <SubmitButton />
       </DinaForm>,
       // User has only one group:
       { ...testCtx, accountContext: { groupNames: ["cnc"] } }
     );
-
     await new Promise(setImmediate);
-    wrapper.update();
 
-    expect(wrapper.find(Select).exists()).toEqual(false);
+    expect(wrapper.queryByRole("combobox")).not.toBeInTheDocument();
 
-    wrapper.find("form").simulate("submit");
+    userEvent.click(wrapper.getByRole("button", { name: /save/i }));
     await new Promise(setImmediate);
-    wrapper.update();
+
     // The default group was selected:
     expect(mockSubmit).lastCalledWith({ group: "cnc" });
   });
@@ -164,23 +147,22 @@ describe("GroupSelectField component", () => {
   it("Doesn't set the default value when the initial value is null.", async () => {
     writeStorage(DEFAULT_GROUP_STORAGE_KEY, "cnc");
 
-    const wrapper = mountWithAppContext(
+    const wrapper = mountWithAppContext2(
       <DinaForm
         initialValues={{ group: null }}
         onSubmit={({ submittedValues }) => mockSubmit(submittedValues)}
       >
         <GroupSelectField name="group" enableStoredDefaultGroup={true} />
+        <SubmitButton />
       </DinaForm>,
       // User has only one group:
       { ...testCtx, accountContext: { groupNames: ["cnc"] } }
     );
-
     await new Promise(setImmediate);
-    wrapper.update();
 
-    wrapper.find("form").simulate("submit");
+    userEvent.click(wrapper.getByRole("button", { name: /save/i }));
     await new Promise(setImmediate);
-    wrapper.update();
+
     // The default group was selected:
     expect(mockSubmit).lastCalledWith({ group: null });
   });
