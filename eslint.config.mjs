9f7859b98bd3a566a17ba67e05a2fd6939235799
BrandonAndre <brandon.andre@agr.gc.ca>
2025-01-10 09:18:30 -0500

    Support #35523 - Migrate from tslint to eslint
    - Added eslint to the project using the eslint installer.
    - Changed the lint scripts to use eslint.
    - Upgraded typescript to a supported version.
    - Currently I have no rules being applied, next I will be bringing over all the rules form the tslint configuration.

---
+++ eslint.config.mjs
@@ -0,0 +1,10 @@
+import globals from "globals";
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.{js,ts,jsx,tsx}"]},
+  {languageOptions: { globals: globals.browser }}
+];
