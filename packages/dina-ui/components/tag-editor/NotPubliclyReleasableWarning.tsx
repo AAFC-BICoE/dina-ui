09e71c62bbab7244b7f7e97d4fe0c8f03a5a6e55
adamzhou03 <90469720+adamzhou03@users.noreply.github.com>
2025-06-20 13:07:37 -0400

    Support #36364 re add object store not publicly releasable switch (#1588)
    * added derivative view page
    
    * changed derivative view to display derivative tags and added unit tests for derivative view page
    
    * added unit tests for DerivativeDetails component, added acTags relationship to Derivative
    
    * fixed derivative redirect link
    
    * renamed derivative-view to view, moved acTags to Derivative attributes
    
    * added useDerivativeMetadataViewQuery function to optimize derivative queries, modified FileView and MetadataFileView to support Derivatives as input
    
    * Fixed type issue in Metadata type
    
    * updated fileview download button to work with derivatives
    
    * updated object view to use new link
    
    * updated derivative pages to use new links
    
    * fixed unit test for derivative view page
    
    * fixed derivative view unit test
    
    * readded notpubliclyreleasable tag to metadata edit page
    
    * added derivative view page
    
    * added unit test for derivative edit page and for notpubliclyreleaseable switch on object metadata edit page
    
    * fixed typing issues
    
    * removed extra notPubliclyReleasable section on
    
    * fixed derivative view type error and readded derivative edit button
    
    * moved NotPubliclyReleasable component out of TagsAndRestrictionsSection component and added notPubliclyReleasable tooltip to NotPubliclyReleasableWarning
    
    * - Fixed logic to display notPubliclyReleasableReason
    
    * Fix small typo
    
    ---------
    
    Co-authored-by: John Phan <johnphan010@gmail.com>

--- packages/dina-ui/components/tag-editor/NotPubliclyReleasableWarning.tsx
+++ packages/dina-ui/components/tag-editor/NotPubliclyReleasableWarning.tsx
@@ -14,7 +14,8 @@ export function NotPubliclyReleasableWarning() {
   return (
     <div className="align-items-center not-publicly-releasable-alert">
       <Tooltip
-        id="notPubliclyReleasable"
+        id="notPubliclyReleasableWithReason"
+        intlValues={{ reason: values.notPubliclyReleasableReason }}
         setVisible={setVisible}
         visible={visible}
         placement="left"
--- packages/dina-ui/intl/dina-ui-en.ts
+++ packages/dina-ui/intl/dina-ui-en.ts
@@ -202,6 +202,7 @@ export const DINAUI_MESSAGES_ENGLISH = {
   editCollectionMethodTitle: "Edit Collection Method",
   editCollectionTitle: "Edit Collection",
   editDinaUserTitle: "Edit User",
+  editDerivativeMetadata: "Edit Derivative Metadata",
   editExternalResourceTitle: "Edit External Resource",
   editInstitutionTitle: "Edit Institution",
   editManagedAttributeTitle: "Edit Managed Attribute",
@@ -1248,5 +1249,6 @@ export const DINAUI_MESSAGES_ENGLISH = {
   taxonomyHierarchyChartInstructions:
     "Interactions: Scroll to zoom, drag to pan, click on nodes to fetch child taxonomic ranks",
   taxonomyHierarchySubtitle:
-    "A real-time, aggregated view of taxonomic hierarchies applied to each primary determination."
+    "A real-time, aggregated view of taxonomic hierarchies applied to each primary determination.",
+  notPubliclyReleasableWithReason: "Not Publicly Releasable: {reason}"
 };
