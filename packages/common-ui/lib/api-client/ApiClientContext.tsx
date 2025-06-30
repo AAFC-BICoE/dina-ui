0a6c068673ab04422cfa1f0f30cda00acfb5cd41
adamzhou03 <adamzhou03@gmail.com>
2025-06-30 14:55:29 +0000

    changed default error handling for bulkLoadResources to use makeAxiosErrorMoreReadable

--- packages/common-ui/lib/api-client/ApiClientContext.tsx
+++ packages/common-ui/lib/api-client/ApiClientContext.tsx
@@ -452,7 +452,7 @@ export class ApiClientImpl implements ApiClientI {
           missingIds.push(...missingIdsThisRun);
           ids = ids.filter((id) => !missingIds.includes(id));
         } else {
-          throw error;
+          makeAxiosErrorMoreReadable(error);
         }
       }
     }
