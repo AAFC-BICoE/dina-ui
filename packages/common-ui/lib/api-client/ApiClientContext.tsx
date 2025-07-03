1944377e8dd5505096497b3740d7c48b23533208
adamzhou03 <adamzhou03@gmail.com>
2025-07-03 18:17:18 +0000

    removed serialization in bulk functions, added support for different types of requests in one save call

--- packages/common-ui/lib/api-client/ApiClientContext.tsx
+++ packages/common-ui/lib/api-client/ApiClientContext.tsx
@@ -481,48 +481,104 @@ export class ApiClientImpl implements ApiClientI {
     const deleteArgs = args.filter(
       (arg) => (arg as any).delete
     ) as DeleteArgs[];
+
     const saveArgs = args.filter((arg) => !(arg as any).delete) as SaveArgs[];
 
     // Serialize the resources to JSONAPI format.
     const serializePromises = saveArgs.map((saveArg) => serialize(saveArg));
     const serialized = await Promise.all(serializePromises);
 
-    // Create the jsonpatch operations objects.
-    const saveOperations = serialized.map<Operation>((jsonapiResource) => ({
-      op: options?.overridePatchOperation
-        ? "POST"
-        : jsonapiResource.id
-        ? "PATCH"
-        : "POST",
-      path: options?.overridePatchOperation
-        ? jsonapiResource.type
-        : jsonapiResource.id
-        ? `${jsonapiResource.type}/${jsonapiResource.id}`
-        : jsonapiResource.type,
-      value: {
-        ...jsonapiResource,
-        id: String(jsonapiResource.id || this.cfg.newId?.() || uuidv4())
-      }
-    }));
-
     const deleteOperations = deleteArgs.map<Operation>((deleteArg) => ({
       op: "DELETE",
       path: `${deleteArg.delete.type}/${deleteArg.delete.id}`
     }));
 
-    const operations = [...saveOperations, ...deleteOperations];
+    // Create the jsonpatch operations objects.
 
-    // Do the operations request.
-    const responses = await this.doOperations(operations, options);
+    if (options?.apiBaseUrl === "/agent-api") {
+      const postOperations: any = [];
+      const patchOperations: any = [];
 
-    // Deserialize the responses to Kitsu format.
-    const deserializePromises = responses.map((response) =>
-      deserialise(response)
-    );
-    const deserialized = await Promise.all(deserializePromises);
-    const kitsuResources = deserialized.map(({ data }) => data);
+      for (const jsonapiResource of serialized) {
+        if (jsonapiResource.id) {
+          patchOperations.push({
+            op: "PATCH",
+            path: `${jsonapiResource.type}/${jsonapiResource.id}`,
+            value: {
+              ...jsonapiResource,
+              id: String(jsonapiResource.id || this.cfg.newId?.() || uuidv4())
+            }
+          });
+        } else {
+          postOperations.push({
+            op: "POST",
+            path: jsonapiResource.type,
+            value: {
+              ...jsonapiResource,
+              id: String(jsonapiResource.id || this.cfg.newId?.() || uuidv4())
+            }
+          });
+        }
+      }
 
-    return kitsuResources;
+      const postResponses = postOperations.length
+        ? await this.doOperations(postOperations, options)
+        : [];
+      const patchResponses = patchOperations.length
+        ? await this.doOperations(patchOperations, options)
+        : [];
+      const deleteResponses = deleteOperations.length
+        ? await this.doOperations(deleteOperations, options)
+        : [];
+
+      // Deserialize the responses to Kitsu format.
+      const deserializedPostPromises = postResponses.map((response) =>
+        deserialise(response)
+      );
+      const deserializedPatchPromises = patchResponses.map((response) =>
+        deserialise(response)
+      );
+      const deserializedDeletePromises = deleteResponses.map((response) =>
+        deserialise(response)
+      );
+      const deserialized = await Promise.all([
+        ...deserializedPostPromises,
+        ...deserializedPatchPromises,
+        ...deserializedDeletePromises
+      ]);
+      const kitsuResources = deserialized.map(({ data }) => data);
+      return kitsuResources;
+    } else {
+      const saveOperations = serialized.map<Operation>((jsonapiResource) => ({
+        op: options?.overridePatchOperation
+          ? "POST"
+          : jsonapiResource.id
+          ? "PATCH"
+          : "POST",
+        path: options?.overridePatchOperation
+          ? jsonapiResource.type
+          : jsonapiResource.id
+          ? `${jsonapiResource.type}/${jsonapiResource.id}`
+          : jsonapiResource.type,
+        value: {
+          ...jsonapiResource,
+          id: String(jsonapiResource.id || this.cfg.newId?.() || uuidv4())
+        }
+      }));
+
+      const operations = [...saveOperations, ...deleteOperations];
+
+      // Do the operations request.
+      const responses = await this.doOperations(operations, options);
+
+      // Deserialize the responses to Kitsu format.
+      const deserializePromises = responses.map((response) =>
+        deserialise(response)
+      );
+      const deserialized = await Promise.all(deserializePromises);
+      const kitsuResources = deserialized.map(({ data }) => data);
+      return kitsuResources;
+    }
   }
 
   /**
@@ -617,12 +673,8 @@ export class ApiClientImpl implements ApiClientI {
     resources: InputResource<KitsuResource>[],
     { apiBaseUrl, resourceType }: BulkCreateResourcesOptions
   ): Promise<AxiosResponse> {
-    const serializedResources = await Promise.all(
-      resources.map((resource) => serialize({ resource, type: resourceType }))
-    );
-
     const requestBody = {
-      data: serializedResources.map((resource) => ({
+      data: resources.map((resource) => ({
         ...resource
       }))
     };
@@ -652,12 +704,8 @@ export class ApiClientImpl implements ApiClientI {
     resources: InputResource<KitsuResource>[],
     { apiBaseUrl, resourceType }: BulkUpdateResourcesOptions
   ): Promise<AxiosResponse> {
-    const serializedResources = await Promise.all(
-      resources.map((resource) => serialize({ resource, type: resourceType }))
-    );
-
     const requestBody = {
-      data: serializedResources.map((resource) => ({
+      data: resources.map((resource) => ({
         ...resource
       }))
     };
@@ -844,7 +892,7 @@ export function makeAxiosErrorMoreReadable(error: AxiosError<any>) {
       error.request &&
       error.request.responseURL.split("/").at(-1).includes("bulk-load")
     ) {
-      if ([404, 410].includes(error.response?.status as number)) {
+      if ([404, 410, 500].includes(error.response?.status as number)) {
         throw error;
       }
     }
