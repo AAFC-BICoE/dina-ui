9008ddf0eccf5b6ba19bb6957a0e9840fb1831ae
BrandonAndre <brandon.andre@agr.gc.ca>
2025-08-27 10:15:16 -0400

    Support #36815 - Managed attribute list not fully populated
    - Instead of having 4 different components, I created one generic component which removes a lot of duplicated logic.
    - Now using ListPageLayout without inMemoryFilter option.
    - Filtering now using fiql.

--- packages/dina-ui/pages/managed-attribute/list.tsx
+++ packages/dina-ui/pages/managed-attribute/list.tsx
@@ -3,61 +3,25 @@ import {
   dateCell,
   descriptionCell,
   FieldHeader,
-  intlContext,
-  ListLayoutFilterType,
   ListPageLayout
 } from "common-ui";
 import Link from "next/link";
 import { useRouter } from "next/router";
-import { useContext, useState } from "react";
+import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 import { DINAUI_MESSAGES_ENGLISH } from "../../intl/dina-ui-en";
 import { DinaMessage, useDinaIntl } from "../../intl/dina-ui-intl";
 
-import { groupCell, GroupSelectField } from "../../components";
+import { groupCell } from "../../components";
 import PageLayout from "../../components/page/PageLayout";
 import {
   COLLECTION_MODULE_TYPE_LABELS,
   CollectionModuleType,
   MANAGED_ATTRIBUTE_TYPE_OPTIONS,
-  ManagedAttribute,
-  SEQDB_MODULE_TYPE_LABELS,
-  SeqDBModuleType
+  ManagedAttribute
 } from "../../types/collection-api";
-
-export function useFilterManagedAttribute() {
-  const { locale: language } = useContext(intlContext);
-  const filterManagedAttributes = (
-    filterForm: any,
-    value: ManagedAttribute
-  ) => {
-    let result = true;
-    if (filterForm?.group) {
-      result = result && value.group === filterForm.group;
-    }
-    if (filterForm?.filterBuilderModel?.value) {
-      result =
-        result &&
-        (value.name
-          .toLowerCase()
-          .indexOf(filterForm.filterBuilderModel.value.toLowerCase()) > -1 ||
-          (
-            value.multilingualDescription?.descriptions?.filter(
-              (item) =>
-                item.lang === language &&
-                (item.desc ?? "")
-                  .toLowerCase()
-                  .indexOf(filterForm.filterBuilderModel.value.toLowerCase()) >
-                  -1
-            ) ?? []
-          ).length > 0);
-    }
-    return result;
-  };
-  return { filterManagedAttributes };
-}
 
 export default function ManagedAttributesListPage() {
   const { formatMessage } = useDinaIntl();
@@ -82,16 +46,36 @@ export default function ManagedAttributesListPage() {
           <Tab>{formatMessage("seqdbManagedAttributeTitle")}</Tab>
         </TabList>
         <TabPanel>
-          <CollectionAttributeListView />
+          <GenericManagedAttributeListView
+            apiPath="/collection-api/managed-attribute"
+            prependLink="/collection/managed-attribute"
+            componentSupport={true}
+            titleKey="collectionListTitle"
+          />
         </TabPanel>
         <TabPanel>
-          <ObjectStoreAttributeListView />
+          <GenericManagedAttributeListView
+            apiPath="/objectstore-api/managed-attribute"
+            prependLink="/object-store/managed-attribute"
+            componentSupport={false}
+            titleKey="objectStoreTitle"
+          />
         </TabPanel>
         <TabPanel>
-          <TransactionAttributeListView />
+          <GenericManagedAttributeListView
+            apiPath="/loan-transaction-api/managed-attribute"
+            prependLink="/loan-transaction/managed-attribute"
+            componentSupport={false}
+            titleKey="loanTransactionsSectionTitle"
+          />
         </TabPanel>
         <TabPanel>
-          <SeqDBAttributeListView />
+          <GenericManagedAttributeListView
+            apiPath="/seqdb-api/managed-attribute"
+            prependLink="/seqdb/managed-attribute"
+            componentSupport={false}
+            titleKey="seqdbManagedAttributeTitle"
+          />
         </TabPanel>
       </Tabs>
     </PageLayout>
@@ -116,17 +100,38 @@ function CreateNewSection({ href }: CreateButtonProps) {
   );
 }
 
-function CollectionAttributeListView() {
+interface GenericManagedAttributeListViewProps {
+  /**
+   * Example: "/collection-api/managed-attribute"
+   */
+  apiPath: string;
+
+  /**
+   * Example: "/collection/managed-attribute" which will add /view?id=ID to the link
+   */
+  prependLink: string;
+
+  /**
+   * If data components are supported by the endpoint.
+   */
+  componentSupport: boolean;
+
+  /**
+   * Example: "collectionListTitle"
+   */
+  titleKey: string;
+}
+
+function GenericManagedAttributeListView({
+  apiPath,
+  prependLink,
+  componentSupport,
+  titleKey
+}: GenericManagedAttributeListViewProps) {
   const { formatMessage } = useDinaIntl();
 
-  const { filterManagedAttributes } = useFilterManagedAttribute();
-
-  const COLLECTIONS_ATTRIBUTES_FILTER_ATTRIBUTES = [
-    "name",
-    "multilingualDescription"
-  ];
-
-  const COLLECTION_ATTRIBUTES_LIST_COLUMNS: ColumnDefinition<
+  const MANAGED_ATTRIBUTE_FILTER_COLUMNS = ["name", "key", "unit", "createdBy"];
+  const MANAGED_ATTRIBUTE_COLUMNS: ColumnDefinition<
     ManagedAttribute<CollectionModuleType>
   >[] = [
     {
@@ -134,27 +139,29 @@ function CollectionAttributeListView() {
         row: {
           original: { id, name }
         }
-      }) => (
-        <Link href={`/collection/managed-attribute/view?id=${id}`}>{name}</Link>
-      ),
+      }) => <Link href={`${prependLink}/view?id=${id}`}>{name}</Link>,
       header: "Name",
       accessorKey: "name"
     },
-    {
-      cell: ({ row: { original } }) => {
-        const ma: ManagedAttribute<CollectionModuleType> = original;
-        return (
-          <div>
-            {formatMessage(
-              COLLECTION_MODULE_TYPE_LABELS[
-                ma.managedAttributeComponent ?? "MATERIAL_SAMPLE"
-              ] as any
-            )}
-          </div>
-        );
-      },
-      accessorKey: "managedAttributeComponent"
-    },
+    ...(componentSupport
+      ? [
+          {
+            cell: ({ row: { original } }) => {
+              const ma: ManagedAttribute<CollectionModuleType> = original;
+              return (
+                <div>
+                  {formatMessage(
+                    COLLECTION_MODULE_TYPE_LABELS[
+                      ma.managedAttributeComponent ?? "MATERIAL_SAMPLE"
+                    ] as any
+                  )}
+                </div>
+              );
+            },
+            accessorKey: "managedAttributeComponent"
+          }
+        ]
+      : []),
     {
       cell: ({
         row: {
@@ -195,305 +202,23 @@ function CollectionAttributeListView() {
 
   return (
     <>
+      {/* Title */}
       <h3 className="mb-3">
-        <DinaMessage id="collectionListTitle" />
+        <DinaMessage id={titleKey as any} />
       </h3>
 
       {/* Quick create menu */}
-      <CreateNewSection href="/collection/managed-attribute/edit" />
+      <CreateNewSection href={`${prependLink}/edit`} />
 
-      <ListPageLayout<ManagedAttribute<CollectionModuleType>>
-        enableInMemoryFilter={true}
-        filterFn={filterManagedAttributes}
-        filterType={ListLayoutFilterType.FREE_TEXT}
-        filterAttributes={COLLECTIONS_ATTRIBUTES_FILTER_ATTRIBUTES}
-        id="collections-module-managed-attribute-list"
-        queryTableProps={{
-          columns: COLLECTION_ATTRIBUTES_LIST_COLUMNS,
-          path: "collection-api/managed-attribute?page[limit]=1000"
-        }}
-        additionalFilters={(filterForm) => ({
-          isCompleted: false,
-          // Apply group filter:
-          ...(filterForm.group && { rsql: `group==${filterForm.group}` })
-        })}
-        filterFormchildren={({ submitForm }) => (
-          <div className="mb-3">
-            <div style={{ width: "300px" }}>
-              <GroupSelectField
-                onChange={() => setImmediate(submitForm)}
-                name="group"
-                showAnyOption={true}
-              />
-            </div>
-          </div>
-        )}
-      />
-    </>
-  );
-}
-
-function ObjectStoreAttributeListView() {
-  const { filterManagedAttributes } = useFilterManagedAttribute();
-
-  const OBJECT_STORE_ATTRIBUTES_FILTER_ATTRIBUTES = ["name"];
-
-  const OBJECT_STORE_ATTRIBUTES_LIST_COLUMNS: ColumnDefinition<ManagedAttribute>[] =
-    [
-      {
-        cell: ({
-          row: {
-            original: { id, name }
-          }
-        }) => (
-          <Link href={`/object-store/managed-attribute/view?id=${id}`}>
-            {name}
-          </Link>
-        ),
-        header: "Name",
-        accessorKey: "name"
-      },
-      descriptionCell(false, false, "multilingualDescription"),
-      {
-        cell: ({
-          row: {
-            original: { acceptedValues, vocabularyElementType }
-          }
-        }) => {
-          const labelKey: keyof typeof DINAUI_MESSAGES_ENGLISH | undefined =
-            acceptedValues?.length
-              ? "field_vocabularyElementType_picklist_label"
-              : MANAGED_ATTRIBUTE_TYPE_OPTIONS.find(
-                  (option) => option.value === vocabularyElementType
-                )?.labelKey;
-
-          return <div>{labelKey && <DinaMessage id={labelKey} />}</div>;
-        },
-        accessorKey: "vocabularyElementType",
-        // The API sorts alphabetically by key, not displayed intl-ized value,
-        // so the displayed order wouldn't make sense.
-        enableSorting: false
-      },
-      {
-        cell: ({
-          row: {
-            original: { acceptedValues }
-          }
-        }) => <div>{acceptedValues?.map((val) => `"${val}"`)?.join(", ")}</div>,
-        accessorKey: "acceptedValues"
-      },
-      "createdBy",
-      {
-        accessorKey: "group",
-        header: () => <FieldHeader name={"group"} />
-      }
-    ];
-
-  return (
-    <>
-      <h3 className="mb-3">
-        <DinaMessage id="objectStoreTitle" />
-      </h3>
-
-      {/* Quick create menu */}
-      <CreateNewSection href="/object-store/managed-attribute/edit" />
-
+      {/* List Page */}
       <ListPageLayout
-        enableInMemoryFilter={true}
-        filterFn={filterManagedAttributes}
-        filterType={ListLayoutFilterType.FREE_TEXT}
-        filterAttributes={OBJECT_STORE_ATTRIBUTES_FILTER_ATTRIBUTES}
-        id="object-store-module-managed-attribute-list"
+        filterAttributes={MANAGED_ATTRIBUTE_FILTER_COLUMNS}
+        id={`managed-attribute-${titleKey}-list`}
         queryTableProps={{
-          columns: OBJECT_STORE_ATTRIBUTES_LIST_COLUMNS,
-          path: "objectstore-api/managed-attribute"
+          columns: MANAGED_ATTRIBUTE_COLUMNS,
+          path: apiPath
         }}
-      />
-    </>
-  );
-}
-
-function TransactionAttributeListView() {
-  const { filterManagedAttributes } = useFilterManagedAttribute();
-  const TRANSACTION_ATTRIBUTES_FILTER_ATTRIBUTES = ["name"];
-
-  const TRANSACTION_ATTRIBUTES_LIST_COLUMNS: ColumnDefinition<ManagedAttribute>[] =
-    [
-      {
-        cell: ({
-          row: {
-            original: { id, name }
-          }
-        }) => (
-          <Link href={`/loan-transaction/managed-attribute/view?id=${id}`}>
-            {name}
-          </Link>
-        ),
-        header: "Name",
-        accessorKey: "name"
-      },
-      {
-        cell: ({
-          row: {
-            original: { acceptedValues, vocabularyElementType }
-          }
-        }) => {
-          const labelKey: keyof typeof DINAUI_MESSAGES_ENGLISH | undefined =
-            acceptedValues?.length
-              ? "field_vocabularyElementType_picklist_label"
-              : MANAGED_ATTRIBUTE_TYPE_OPTIONS.find(
-                  (option) => option.value === vocabularyElementType
-                )?.labelKey;
-
-          return <div>{labelKey && <DinaMessage id={labelKey} />}</div>;
-        },
-        accessorKey: "vocabularyElementType",
-        // The API sorts alphabetically by key, not displayed intl-ized value,
-        // so the displayed order wouldn't make sense.
-        enableSorting: false
-      },
-      {
-        cell: ({
-          row: {
-            original: { acceptedValues }
-          }
-        }) => <div>{acceptedValues?.map((val) => `"${val}"`)?.join(", ")}</div>,
-        accessorKey: "acceptedValues"
-      },
-      descriptionCell(false, false, "multilingualDescription"),
-      "createdBy",
-      {
-        accessorKey: "group",
-        header: () => <FieldHeader name={"group"} />
-      }
-    ];
-
-  return (
-    <>
-      <h3 className="mb-3">
-        <DinaMessage id="loanTransactionsSectionTitle" />
-      </h3>
-
-      {/* Quick create menu */}
-      <CreateNewSection href="/loan-transaction/managed-attribute/edit" />
-
-      <ListPageLayout
-        enableInMemoryFilter={true}
-        filterFn={filterManagedAttributes}
-        filterType={ListLayoutFilterType.FREE_TEXT}
-        filterAttributes={TRANSACTION_ATTRIBUTES_FILTER_ATTRIBUTES}
-        id="loan-transaction-module-managed-attribute-list"
-        queryTableProps={{
-          columns: TRANSACTION_ATTRIBUTES_LIST_COLUMNS,
-          path: "loan-transaction-api/managed-attribute"
-        }}
-        additionalFilters={(filterForm) => ({
-          isCompleted: false,
-          // Apply group filter:
-          ...(filterForm.group && { rsql: `group==${filterForm.group}` })
-        })}
-        filterFormchildren={({ submitForm }) => (
-          <div className="mb-3">
-            <div style={{ width: "300px" }}>
-              <GroupSelectField
-                onChange={() => setImmediate(submitForm)}
-                name="group"
-                showAnyOption={true}
-              />
-            </div>
-          </div>
-        )}
-      />
-    </>
-  );
-}
-
-function SeqDBAttributeListView() {
-  const { filterManagedAttributes } = useFilterManagedAttribute();
-  const { formatMessage } = useDinaIntl();
-
-  const SEQDB_ATTRIBUTES_FILTER_ATTRIBUTES = ["name"];
-
-  const SEQDB_ATTRIBUTES_LIST_COLUMNS: ColumnDefinition<
-    ManagedAttribute<SeqDBModuleType>
-  >[] = [
-    {
-      cell: ({
-        row: {
-          original: { id, name }
-        }
-      }) => <Link href={`/seqdb/managed-attribute/view?id=${id}`}>{name}</Link>,
-      header: "Name",
-      accessorKey: "name"
-    },
-    {
-      cell: ({ row: { original } }) => {
-        const ma: ManagedAttribute<SeqDBModuleType> = original;
-        return (
-          <div>
-            {formatMessage(
-              SEQDB_MODULE_TYPE_LABELS[ma.managedAttributeComponent!] as any
-            )}
-          </div>
-        );
-      },
-      accessorKey: "managedAttributeComponent"
-    },
-    descriptionCell(false, false, "multilingualDescription"),
-    {
-      cell: ({
-        row: {
-          original: { acceptedValues, vocabularyElementType }
-        }
-      }) => {
-        const labelKey: keyof typeof DINAUI_MESSAGES_ENGLISH | undefined =
-          acceptedValues?.length
-            ? "field_vocabularyElementType_picklist_label"
-            : MANAGED_ATTRIBUTE_TYPE_OPTIONS.find(
-                (option) => option.value === vocabularyElementType
-              )?.labelKey;
-
-        return <div>{labelKey && <DinaMessage id={labelKey} />}</div>;
-      },
-      accessorKey: "vocabularyElementType",
-      // The API sorts alphabetically by key, not displayed intl-ized value,
-      // so the displayed order wouldn't make sense.
-      enableSorting: false
-    },
-    {
-      cell: ({
-        row: {
-          original: { acceptedValues }
-        }
-      }) => <div>{acceptedValues?.map((val) => `"${val}"`)?.join(", ")}</div>,
-      accessorKey: "acceptedValues"
-    },
-    "createdBy",
-    {
-      accessorKey: "group",
-      header: () => <FieldHeader name={"group"} />
-    }
-  ];
-
-  return (
-    <>
-      <h3 className="mb-3">
-        <DinaMessage id="seqdbManagedAttributeTitle" />
-      </h3>
-
-      {/* Quick create menu */}
-      <CreateNewSection href="/seqdb/managed-attribute/edit" />
-
-      <ListPageLayout
-        enableInMemoryFilter={true}
-        filterFn={filterManagedAttributes}
-        filterType={ListLayoutFilterType.FREE_TEXT}
-        filterAttributes={SEQDB_ATTRIBUTES_FILTER_ATTRIBUTES}
-        id="seqdb-module-managed-attribute-list"
-        queryTableProps={{
-          columns: SEQDB_ATTRIBUTES_LIST_COLUMNS,
-          path: "seqdb-api/managed-attribute"
-        }}
+        useFiql={true}
       />
     </>
   );
