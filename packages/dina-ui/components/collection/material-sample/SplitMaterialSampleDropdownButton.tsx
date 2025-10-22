1f2f79c48592ad2e3b7c8d01c5737459e873f28e
Brandon Andre <brandon.andre@agr.gc.ca>
2025-10-22 11:03:48 -0400

    Support #37014 - Split configuration v2 changes (#1664)
    * Support #37004 - Use materialSampleChildren as optional attributes
    
    - Removed include and now it's using the optfields instead.
    - Fixed split configuration dropdown filtering issue.
    
    * Support #37004 - Use materialSampleChildren as optional attributes
    
    - Fixed test
    
    * Support #37014 - Split Configuration v2 Changes
    
    - Replaced all RSQL for split configuration operations with simple filters.
    - List page is now using fiql for query builder.

--- packages/dina-ui/components/collection/material-sample/SplitMaterialSampleDropdownButton.tsx
+++ packages/dina-ui/components/collection/material-sample/SplitMaterialSampleDropdownButton.tsx
@@ -1,7 +1,13 @@
 import { DinaMessage } from "../../../intl/dina-ui-intl";
 import { useRouter } from "next/router";
 import { writeStorage } from "@rehooks/local-storage";
-import { BULK_SPLIT_IDS, Tooltip, useAccount, useQuery } from "common-ui";
+import {
+  BULK_SPLIT_IDS,
+  SimpleSearchFilterBuilder,
+  Tooltip,
+  useAccount,
+  useQuery
+} from "common-ui";
 import Dropdown from "react-bootstrap/Dropdown";
 import Select from "react-select";
 import React, { CSSProperties, useState } from "react";
@@ -56,15 +62,11 @@ export function SplitMaterialSampleDropdownButton({
       page: {
         limit: 1000
       },
-
       // Display all user form templates and public to the group templates.
-      // filter: SimpleSearchFilterBuilder.create<SplitConfiguration>()
-      //   .whereIn("group", groupNames)
-      //   .where("createdBy", "EQ", username)
-      //   .build()
-      filter: {
-        rsql: `group=in=(${groupNames});(createdBy==${username})`
-      }
+      filter: SimpleSearchFilterBuilder.create<SplitConfiguration>()
+        .whereIn("group", groupNames)
+        .where("createdBy", "EQ", username)
+        .build()
     },
     {
       disabled,
