511269c4909af73dd76d776af065182e214775c6
adamzhou03 <adamzhou03@gmail.com>
2026-01-27 19:31:44 +0000

    replaced svg icons with font awesome icons

--- packages/dina-ui/pages/object-store/object/image-view.tsx
+++ packages/dina-ui/pages/object-store/object/image-view.tsx
@@ -17,6 +17,8 @@ import {
 } from "react-zoom-pan-pinch";
 import { Button, ButtonGroup } from "react-bootstrap";
 import { useState } from "react";
+import { FaMagnifyingGlassPlus, FaMagnifyingGlassMinus } from "react-icons/fa6";
+import { TbZoomReset, TbPictureInPictureTop } from "react-icons/tb";
 
 /**
  * ImageViewer component displays an image fetched from the object store with simple zoom toggle.
@@ -154,82 +156,28 @@ export default function ImageViewer() {
                     aria-label="Zoom In"
                     onClick={() => zoomIn()}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-zoom-in"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11M13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0"
-                      />
-                      <path d="M10.344 11.742q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1 6.5 6.5 0 0 1-1.398 1.4z" />
-                      <path
-                        fill-rule="evenodd"
-                        d="M6.5 3a.5.5 0 0 1 .5.5V6h2.5a.5.5 0 0 1 0 1H7v2.5a.5.5 0 0 1-1 0V7H3.5a.5.5 0 0 1 0-1H6V3.5a.5.5 0 0 1 .5-.5"
-                      />
-                    </svg>
+                    <FaMagnifyingGlassPlus />
                   </Button>
                   <Button
                     variant="primary"
                     aria-label="Zoom Out"
                     onClick={() => zoomOut()}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-zoom-out"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11M13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0"
-                      />
-                      <path d="M10.344 11.742q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1 6.5 6.5 0 0 1-1.398 1.4z" />
-                      <path
-                        fill-rule="evenodd"
-                        d="M3 6.5a.5.5 0 0 1 .5-.5h6a.5.5 0 0 1 0 1h-6a.5.5 0 0 1-.5-.5"
-                      />
-                    </svg>
+                    <FaMagnifyingGlassMinus />
                   </Button>
                   <Button
                     variant="primary"
                     aria-label="Reset"
                     onClick={() => resetTransform()}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-x-circle"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14m0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16" />
-                      <path d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708" />
-                    </svg>
+                    <TbZoomReset />
                   </Button>
                   <Button
                     variant="primary"
                     aria-label="Toggle Minimap"
                     onClick={() => setMinimapShowing(!minimapShowing)}
                   >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="16"
-                      height="16"
-                      fill="currentColor"
-                      className="bi bi-images"
-                      viewBox="0 0 16 16"
-                    >
-                      <path d="M4.502 9a1.5 1.5 0 1 0 0-3 1.5 1.5 0 0 0 0 3" />
-                      <path d="M14.002 13a2 2 0 0 1-2 2h-10a2 2 0 0 1-2-2V5A2 2 0 0 1 2 3a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v8a2 2 0 0 1-1.998 2M14 2H4a1 1 0 0 0-1 1h9.002a2 2 0 0 1 2 2v7A1 1 0 0 0 15 11V3a1 1 0 0 0-1-1M2.002 4a1 1 0 0 0-1 1v8l2.646-2.354a.5.5 0 0 1 .63-.062l2.66 1.773 3.71-3.71a.5.5 0 0 1 .577-.094l1.777 1.947V5a1 1 0 0 0-1-1z" />
-                    </svg>
+                    <TbPictureInPictureTop />
                   </Button>
                 </ButtonGroup>
 
