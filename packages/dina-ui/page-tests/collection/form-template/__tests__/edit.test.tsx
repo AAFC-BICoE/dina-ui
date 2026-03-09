4a9980872624cf976467f33a25beeaa8a64f12bc
Brandon Andre <brandon.andre@agr.gc.ca>
2026-03-09 09:16:48 -0400

    Support #37899 - Load type status from controlled vocabulary
    - Switched endpoint to use controlled vocabulary items and look for that specific key.
    - Updated test cases.

--- packages/dina-ui/page-tests/collection/form-template/__tests__/edit.test.tsx
+++ packages/dina-ui/page-tests/collection/form-template/__tests__/edit.test.tsx
@@ -67,7 +67,7 @@ const mockGet = jest.fn<any, any>(async (path) => {
     case "collection-api/collection":
     case "collection-api/project":
     case "collection-api/vocabulary2/materialSampleType":
-    case "collection-api/vocabulary2/typeStatus":
+    case "collection-api/controlled-vocabulary-item?filter[controlledVocabulary.key][EQ]=type_status":
     case "collection-api/organism":
     case "collection-api/collection-method":
     case "collection-api/controlled-vocabulary-item?filter[controlledVocabulary.key][EQ]=coordinate_format":
