91402c6ab381cc0f1adda9d43f0790fdb04c646f
Brandon Andre <brandon.andre@agr.gc.ca>
2026-08-11 14:16:18 -0400

    Support #38744 - Determination and organism managed attributes no longer working for workbook generator (#1902)
    * Support #38744 - Determination and organism managed attributes no longer working for workbook generator
    
    - Fixed issue for generator
    - Automatching for workbook upload still needs some work
    - Added some test coverage.
    
    * Support #38744 - Determination and organism managed attributes no longer working for workbook generator
    
    - Fixed issue with controlled vocabulary based managed attributes not auto mapping.

--- packages/dina-ui/components/workbook/column-mapping/useColumnMapping.tsx
+++ packages/dina-ui/components/workbook/column-mapping/useColumnMapping.tsx
@@ -452,11 +452,17 @@ export function useColumnMapping() {
       return undefined;
     }
 
+    // Depending on if it's a controlled vocabulary managed attribute or legacy managed attribute,
+    // the dina component will be stored in a different part.
+    const configDataComponent =
+      config?.managedAttributeComponent ?? config?.filter?.dinaComponent;
+
+    // Find the matching managed attribute based on the key and the dina component.
     return managedAttributes.find(
       (managedAttribute) =>
         managedAttribute.key === key &&
-        (config.managedAttributeComponent === "ENTITY" ||
-          managedAttribute?.dinaComponent === config.managedAttributeComponent)
+        (configDataComponent === "ENTITY" ||
+          managedAttribute?.dinaComponent === configDataComponent)
     );
   }
 
