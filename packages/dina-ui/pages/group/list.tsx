081ec0c5324db0c9d3b1f49038ddccb565982be2
greysaruman <88000873+greysaruman@users.noreply.github.com>
2026-08-19 15:52:51 -0700

    display the localized label in the group list

--- packages/dina-ui/pages/group/list.tsx
+++ packages/dina-ui/pages/group/list.tsx
@@ -6,33 +6,44 @@ import {
 } from "common-ui";
 import Link from "next/link";
 import PageLayout from "packages/dina-ui/components/page/PageLayout";
+import { DinaMessage, useDinaIntl } from "../../intl/dina-ui-intl";
 import { Group } from "../../types/user-api";
-
-const GROUP_TABLE_COLUMNS: ColumnDefinition<Group>[] = [
-  {
-    cell: ({
-      row: {
-        original: { id, name }
-      }
-    }) => (
-      <Link href={`/group/view?id=${id}`} legacyBehavior>
-        {name}
-      </Link>
-    ),
-    accessorKey: "name"
-  },
-  {
-    cell: ({
-      row: {
-        original: { path }
-      }
-    }) => path,
-    accessorKey: "path"
-  }
-];
 
 export default function GroupListPage() {
   const { isAdmin } = useAccount();
+  const { locale } = useDinaIntl();
+
+  const groupTableColumns: ColumnDefinition<Group>[] = [
+    {
+      cell: ({
+        row: {
+          original: { id, name }
+        }
+      }) => (
+        <Link href={`/group/view?id=${id}`} legacyBehavior>
+          {name}
+        </Link>
+      ),
+      accessorKey: "name"
+    },
+    {
+      id: "label",
+      header: () => <DinaMessage id="groupLabel" />,
+      cell: ({ row: { original } }) => (
+        <>{original.labels?.[locale] ?? ""}</>
+      ),
+      enableSorting: false
+    },
+    {
+      cell: ({
+        row: {
+          original: { path }
+        }
+      }) => path,
+      accessorKey: "path"
+    }
+  ];
+
   const buttonBarContent = (
     <div className="flex d-flex ms-auto">
       {isAdmin && <CreateButton entityLink="/group" />}
@@ -45,7 +56,7 @@ export default function GroupListPage() {
         defaultSort={[{ id: "name", desc: false }]}
         id="group-list"
         queryTableProps={{
-          columns: GROUP_TABLE_COLUMNS,
+          columns: groupTableColumns,
           path: "user-api/group"
         }}
       />
