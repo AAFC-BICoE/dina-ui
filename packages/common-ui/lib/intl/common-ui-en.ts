3414eea496c92387c6ea40b0733cd73e12808c7b
Brandon Andre <brandon.andre@agr.gc.ca>
2026-09-01 11:09:35 -0400

    Support #38793 - Allow batch edit to append tags (#1925)
    * Support #38793 - Allow batch edit to append tags
    
    - Added set to store appended fields in the bulk edit context.
    - Added a toggle to switch between "Append" and "Replace" modes with tooltips on hover.
    - Updated the tooltip icon to just use react icon to make it look more modern.
    
    * Support #38793 - Allow batch edit to append tags
    
    - Fixed a bunch of tests.
    - Improved the accessibility of the tooltips.
    
    * Support #38793 - Allow batch edit to append tags
    
    - Fixed snapshots
    
    * Support #38793 - Allow batch edit to append tags
    
    - Fixed even more tests
    
    * Support #38793 - Allow batch edit to append tags
    
    - Fixed issue with the toggle button not allowing the dropdown to be interacted with.
    - Added logic that now configures the bulk context.
    
    * Support #38793 - Allow batch edit to append tags
    
    - Started working on the useBulkSampleSave to apply the append logic correctly.
    - Added test coverage for append and replace to ensure it's working correctly.
    
    * Support #38793 - Allow batch edit to append tags
    
    - Fixed issue with the appended fields not being passed correctly.
    
    * Support #38793 - Allow batch edit to append tags
    
    - Fixed tests
    - Fixed issue where if no tags were provided it would set an empty array which is not correct.
    
    * Support #38793 - Allow batch edit to append tags
    
    - Moved the clear and append logic to a bulk edit utils since it's the
    - Added append support to Metadata bulk editor with the tags there.
    - Added test coverage for metadata bulk editor for appending.
    
    * Support #38793 - Allow batch edit to append tags
    
    - Added internationalization
    
    * Fix SCSS syntax by closing keyframe block
    
    ---------
    
    Co-authored-by: Henrique <43481446+hdosprazeres@users.noreply.github.com>

--- packages/common-ui/lib/intl/common-ui-en.ts
+++ packages/common-ui/lib/intl/common-ui-en.ts
@@ -11,6 +11,12 @@ export const COMMON_UI_MESSAGES_ENGLISH = {
   addNew: "Add New",
   agricultureCanada: "Agriculture and Agri-Food Canada",
   areYouSure: "Are you sure?",
+  append: "Append",
+  append_tooltip:
+    "In append mode, tags entered here will be added to each record's existing tags rather than replacing them.",
+  replace: "Replace",
+  replace_tooltip:
+    "In replace mode, tags entered here will overwrite all existing tags for each record.",
   linked: "Linked Collecting Event",
   attributeLabel: "Attribute",
   autoFillGeoSuggestLabel:
